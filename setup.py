92ea6b7a7acf861dbcc8c61c50639f7925423d19
Willian Galvani <williangalvani@gmail.com>
2020-08-25 14:13:18 -0300

    create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+setup(name='companion_docker',
+      version='0.1.0',
+      description='Blue Robotics Ardusub Companion Docker System',
+      license='MIT',
+      install_requires=['docker',])
+
