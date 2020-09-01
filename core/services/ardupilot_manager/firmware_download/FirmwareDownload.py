5cef3832eaba8156e560fc42d15b70477269ba84
Patrick José Pereira <patrickelectric@gmail.com>
2020-09-01 12:16:58 -0300

    core: services: ardupilot_manager: Add FirmwareDownload functionality
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/services/ardupilot_manager/firmware_download/FirmwareDownload.py
@@ -0,0 +1,206 @@
+import enum
+import json
+import pathlib
+import random
+import string
+import tempfile
+from urllib.request import urlopen, urlretrieve
+
+from warnings import warn
+
+from packaging.version import Version
+from typing import Any, Dict, List, Optional
+
+
+class Vehicle(enum.Enum):
+    """Valid vehicle types to download"""
+
+    Sub = 1
+    Rover = 2
+    Plane = 3
+    Copter = 4
+
+
+class FirmwareDownload:
+    _manifest_remote = "https://firmware.ardupilot.org/manifest.json.gz"
+    _supported_firmware_format = "apj"
+
+    def __init__(self) -> None:
+        self._manifest: Dict[str, Any] = {}
+
+    @staticmethod
+    def _generate_random_filename(length: int = 16) -> pathlib.Path:
+        """Generate a random name for a temporary file.
+
+        Args:
+            length (int, optional): Size of the name. Defaults to 16.
+
+        Returns:
+            pathlib.Path: Path of the temporary file.
+        """
+
+        filename = "".join(random.sample(string.ascii_lowercase, length))
+        folder = pathlib.Path(tempfile.gettempdir()).absolute()
+        return pathlib.Path.joinpath(folder, filename)
+
+    @staticmethod
+    def _download(url: str) -> pathlib.Path:
+        """Download a specific file for a temporary location.
+
+        Args:
+            url (str): Url to download the file.
+
+        Returns:
+            pathlib.Path: File of the temporary file.
+        """
+
+        filename = FirmwareDownload._generate_random_filename()
+        urlretrieve(url, filename)
+        return filename
+
+    @staticmethod
+    def _validate_firmware(firmware_path: pathlib.Path) -> bool:
+        """A simple validation function for firmware files (apj)
+
+        Args:
+            firmware_path (pathlib.Path): Path of the firmware file;
+
+        Returns:
+            bool: True if valid, False if otherwise.
+        """
+        with open(firmware_path, "r") as firmware_file:
+            data = json.load(firmware_file)
+            keys = data.keys()
+            if "image_size" in keys and "image" in keys:
+                return True
+
+        return False
+
+    def _manifest_is_valid(self) -> bool:
+        """Check if internal content is valid and update it if not.
+
+        Returns:
+            bool: True if valid, False if was unable to validate.
+        """
+        return "format-version" in self._manifest or self.download_manifest()
+
+    def download_manifest(self) -> bool:
+        """Download ArduPilot manifest file
+
+        Returns:
+            bool: True if file was downloaded and validated, False if not.
+        """
+        import gzip
+
+        with urlopen(FirmwareDownload._manifest_remote) as http_response:
+            manifest_gzip = http_response.read()
+            manifest = gzip.decompress(manifest_gzip)
+            self._manifest = json.loads(manifest)
+
+        if "format-version" not in self._manifest:
+            warn("Failed to fetch content of manifest file.", RuntimeWarning)
+            return False
+
+        if self._manifest["format-version"] != "1.0.0":
+            warn("Firmware description file format changed, compatibility may be broken.", RuntimeWarning)
+
+        return True
+
+    def _find_version_item(self, **args: str) -> List[Dict[str, Any]]:
+        """Find version objects in the manifest that match the specific case of **args
+
+        The arguments should follow the same name described in the dictionary inside the manifest
+        for firmware item. Valid arguments can be:
+            mav_type, vehicletype, mav_firmware_version_minor, format, mav_firmware_version_type,
+            platform, latest and others. `-` should be replaced by `_` to use valid python arguments.
+            E.g: `self._find_version_item(vehicletype="Sub", platform="Pixhawk1", mav_firmware_version_type="4.0.1")`
+
+        Returns:
+            List[Dict[str, Any]]: A list of firmware items that match the arguments.
+        """
+        if not self._manifest and not self.download_manifest():
+            return []
+
+        found_version_item = []
+
+        # Make sure that the item matches all args value
+        for item in self._manifest["firmware"]:
+            for key in args:
+                real_key = key.replace("_", "-")
+                if real_key not in item or item[real_key] != args[key]:
+                    break
+            else:
+                found_version_item.append(item)
+
+        return found_version_item
+
+    def get_available_versions(self, vehicle: Vehicle, platform: str) -> List[str]:
+        """Get available firmware versions for the specific plataform and vehicle
+
+        Args:
+            vehicle (Vehicle): Desired vehicle.
+            platform (str): Desired platform.
+
+        Returns:
+            List[str]: List of available versions that match the specific desired configuration.
+        """
+        available_versions: List[str] = []
+
+        if not self._manifest_is_valid():
+            return available_versions
+
+        items = self._find_version_item(
+            vehicletype=vehicle.name, platform=platform, format=FirmwareDownload._supported_firmware_format
+        )
+
+        for item in items:
+            available_versions.append(item["mav-firmware-version-type"])
+
+        return available_versions
+
+    def download(self, vehicle: Vehicle, platform: str, version: str = "") -> Optional[pathlib.Path]:
+        """Download a specific firmware that matches the arguments.
+
+        Args:
+            vehicle (Vehicle): Desired vehicle.
+            platform (str): Desired platform.
+            version (str, optional): Desired version, if None provided the latest stable will be used.
+                Defaults to None.
+
+        Returns:
+            Optional[pathlib.Path]: Temporary path for the firmware file, None if unable to download or validate file.
+        """
+        versions = self.get_available_versions(vehicle, platform)
+        if version and versions and version not in versions:
+            return None
+
+        # Autodetect the latest stable version
+        if not version:
+            stable_versions = [version for version in versions if "STABLE" in version]
+            newest_version: Optional[str] = None
+            for stable_version in stable_versions:
+                semver_version = stable_version.split("-")[1]
+                if not newest_version or Version(newest_version) < Version(semver_version):
+                    newest_version = semver_version
+            if not newest_version:
+                return None
+            version = f"STABLE-{newest_version}"
+
+        items = self._find_version_item(
+            vehicletype=vehicle.name,
+            platform=platform,
+            mav_firmware_version_type=version,
+            format=FirmwareDownload._supported_firmware_format,
+        )
+
+        if len(items) != 1:
+            warn(f"Invalid number of candidates to download: {len(items)}", RuntimeWarning)
+            return None
+
+        item = items[0]
+        path = FirmwareDownload._download(item["url"])
+        if not FirmwareDownload._validate_firmware(path):
+            warn("Unable to validate firmware file.", RuntimeWarning)
+            return None
+
+        return path
---
+++ core/services/ardupilot_manager/firmware_download/test_FirmwareDownload.py
@@ -0,0 +1,36 @@
+from firmware_download.FirmwareDownload import FirmwareDownload, Vehicle
+
+
+def test_static() -> None:
+    downloaded_file = FirmwareDownload._download(FirmwareDownload._manifest_remote)
+    assert downloaded_file.exists(), "Download file does not exist."
+
+    smaller_valid_size_bytes = 180 * 1024
+    assert downloaded_file.stat().st_size > smaller_valid_size_bytes, "Download file size is not big enough."
+
+
+def test_firmware_download() -> None:
+    firmware_download = FirmwareDownload()
+    assert firmware_download.download_manifest(), "Failed to download/validate manifest file."
+
+    versions = firmware_download._find_version_item(
+        vehicletype="Sub", format="apj", mav_firmware_version_type="STABLE-4.0.1", platform="Pixhawk1"
+    )
+    assert len(versions) == 1, "Failed to find a single firmware."
+
+    versions = firmware_download._find_version_item(
+        vehicletype="Sub", mav_firmware_version_type="STABLE-4.0.1", platform="Pixhawk1"
+    )
+    assert len(versions) == 3, "Failed to find multiple versions."
+
+    available_versions = firmware_download.get_available_versions(Vehicle.Sub, "Pixhawk1")
+    assert len(available_versions) == len(set(available_versions)), "Available versions are not unique."
+
+    test_available_versions = ["STABLE-4.0.1", "STABLE-4.0.0", "OFFICIAL", "DEV", "BETA"]
+    assert len(set(available_versions)) >= len(
+        set(test_available_versions)
+    ), "Available versions are missing know versions."
+
+    assert firmware_download.download(
+        Vehicle.Sub, "Pixhawk1", "STABLE-4.0.1"
+    ), "Failed to download a valid firmware file."
