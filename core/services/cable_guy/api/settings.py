c2011c18a2f8022c478fd0a6ecb46cbb7cbe7155
Patrick José Pereira <patrickelectric@gmail.com>
2020-09-03 09:57:30 -0300

    core: cable_guy: Add corrections for pylint
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/cable_guy/api/settings.py
+++ core/services/cable_guy/api/settings.py
@@ -1,8 +1,9 @@
-import appdirs
-import os
 import json
-
+import os
+import pprint
 from typing import Any, Dict, List
+
+import appdirs
 
 
 class Settings:
@@ -42,8 +43,6 @@ class Settings:
                 self.root = data
         except Exception as exception:
             print(f"Failed to fetch data from file ({self.settings_file}): {exception}")
-            import pprint
-
             pprint.pprint(data)
 
         return True
