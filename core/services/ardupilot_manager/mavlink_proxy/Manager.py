21b0a047918b5bbd621cab710472393bded677ec
Patrick José Pereira <patrickelectric@gmail.com>
2020-09-09 14:38:24 -0300

    core: services: mavlink_proxy: Add Manager
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/services/ardupilot_manager/mavlink_proxy/Manager.py
@@ -0,0 +1,45 @@
+from typing import List, Type
+from warnings import warn
+
+# Plugins
+# pylint: disable=unused-import
+import mavlink_proxy.MAVLinkRouter
+import mavlink_proxy.MAVProxy
+from mavlink_proxy.AbstractRouter import AbstractRouter
+from mavlink_proxy.Endpoint import Endpoint
+
+
+class Manager:
+    def __init__(self) -> None:
+        self.master: Endpoint
+        if not Manager.available_interfaces():
+            raise RuntimeError("No available interface found.")
+
+        self.tool = Manager.available_interfaces()[0]()
+
+    @staticmethod
+    def available_interfaces() -> List[Type[AbstractRouter]]:
+        return AbstractRouter.available_interfaces()
+
+    def use(self, interface: AbstractRouter) -> None:
+        self.tool = interface
+
+    def add_endpoints(self, endpoints: List[Endpoint]) -> None:
+        for endpoint in endpoints:
+            if not self.tool.add_endpoint(endpoint):
+                warn(f"Endpoint {endpoint} is not valid.", RuntimeWarning)
+
+    def set_master_endpoint(self, master: Endpoint) -> None:
+        self.master = master
+
+    def start(self) -> None:
+        self.tool.start(self.master)
+
+    def command_line(self) -> str:
+        if not self.master:
+            raise RuntimeError("Master endpoint was not defined.")
+
+        return self.tool.assemble_command(self.master)
+
+    def is_running(self) -> bool:
+        return self.tool.is_running()
