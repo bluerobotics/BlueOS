719bb7d07354357d625385eb9bfc558b5b9bdda8
Patrick José Pereira <patrickelectric@gmail.com>
2020-09-21 11:48:25 -0300

    install: Use new boards script
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- install/install.sh
+++ install/install.sh
@@ -17,37 +17,10 @@ set -e
 # Check if the script is running as root
 [[ $EUID != 0 ]] && echo "Script must run as root."  && exit 1
 
-# Running in a BCM27* or BCM28, do necessary Raspberry configuration
-cat /proc/cpuinfo | grep -Eq "BCM(27|28)" && (
-    echo "Configuring board.."
-
-    # Remove any configuration related to i2c and spi/spi1 and do the necessary changes for navigator
-    echo "- Enable I2C, SPI and UART."
-    for STRING in "dtparam=i2c_arm=" "dtparam=spi=" "dtoverlay=spi1" "dtoverlay=uart1"; do
-        sudo sed -i "/$STRING/d" /boot/config.txt
-    done
-    for STRING in "dtparam=i2c_arm=on" "dtparam=spi=on" "dtoverlay=spi1-3cs" "dtoverlay=uart1"; do
-        echo "$STRING" | sudo tee -a /boot/config.txt
-    done
-
-    # Check for valid modules file to load kernel modules
-    if [ -f "/etc/modules" ]; then
-        MODULES_FILE="/etc/modules"
-    else
-        MODULES_FILE="/etc/modules-load.d/companion.conf"
-        touch "$MODULES_FILE" || true # Create if it does not exist
-    fi
-
-    echo "- Set up kernel modules."
-    # Remove any configuration or commented part related to the i2c drive
-    for STRING in "bcm2835-v4l2" "i2c-bcm2708" "i2c-dev"; do
-        sudo sed -i "/$STRING/d" "$MODULES_FILE"
-        echo "$STRING" | sudo tee -a "$MODULES_FILE"
-    done
-
-    # Remove any console serial configuration
-    echo "- Configure serial."
-    sudo sed -e 's/console=serial[0-9],[0-9]*\ //' -i /boot/cmdline.txt
+# Detect CPU and do necessary hardware configuration for each supported hardware
+CPU_INFO="$(cat /proc/cpuinfo)"
+echo $CPU_INFO | grep -Eq "BCM(27|28)" && (
+    curl -fsSL $REMOTE/install/boards/bcm_28_27.sh | bash
 )
 
 echo "Checking for blocked wifi and bluetooth."
