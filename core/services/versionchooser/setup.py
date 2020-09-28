7c62889397a36191deb01bbffffaf68b109b477d
Willian Galvani <williangalvani@gmail.com>
2020-09-28 15:16:37 -0300

    Core: Create VersionChooser

---
+++ core/services/versionchooser/setup.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import pathlib
+import ssl
+import urllib.request
+from warnings import warn
+
+from setuptools import setup
+
+ssl._create_default_https_context = ssl._create_unverified_context
+
+
+static_files = {
+    "js/axios.min.js": "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js",
+    "js/polyfill.min.js": "https://polyfill.io/v3/polyfill.min.js?features=es2015,IntersectionObserver",
+    "js/vue.js": "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.js",
+    "js/timeago.js": "https://cdnjs.cloudflare.com/ajax/libs/timeago.js/2.0.2/timeago.min.js",
+    "js/metro.js": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/js/metro.min.js",
+    "js/jquery.js": "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js",
+    "css/metro-all.css": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/css/metro-all.css",
+    "mif/metro.woff": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.woff",
+    "mif/metro.ttf": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.ttf",
+}
+
+current_folder = pathlib.Path(__file__).parent.absolute()
+static_folder = pathlib.Path.joinpath(current_folder, "static")
+
+for filename, url in static_files.items():
+    path = pathlib.Path.joinpath(static_folder, filename)
+    path.parent.mkdir(exist_ok=True)
+    try:
+        urllib.request.urlretrieve(url, path)
+    except Exception as error:
+        warn(f"unable to open url {url}, error {error}")
+
+setup(
+    name="versionchooser_service",
+    version="0.1.0",
+    description="Blue Robotics Ardusub Companion Version Chooser",
+    license="MIT",
+    install_requires=[
+        "aiohttp-jinja2 == 1.4.2",
+        "aiohttp == 3.7.3",
+        "connexion[swagger-ui, aiohttp] == 2.7.0",
+        "docker == 4.4.4",
+        "appdirs == 1.4.4",
+        "pytest-asyncio == 0.14.0",
+        "asyncmock == 0.4.2",
+        "werkzeug == 2.0.0rc4",
+        "attrs == 20.3.0",
+    ],
+)
---
+++ core/services/versionchooser/test_versionchooser.py
@@ -0,0 +1,209 @@
+import json
+from unittest import mock
+from unittest.mock import AsyncMock
+
+import pytest
+from utils.chooser import VersionChooser
+
+# All test coroutines will be treated as marked.
+pytestmark = pytest.mark.asyncio
+
+SAMPLE_JSON = """{
+    "core": {
+        "tag": "master",
+        "image": "bluerobotics/companion-core",
+        "enabled": true,
+        "webui": false,
+        "network": "host",
+        "binds": {
+            "/dev/": {
+                "bind": "/dev/",
+                "mode": "rw"
+            },
+            "/var/run/wpa_supplicant/wlan0": {
+                "bind": "/var/run/wpa_supplicant/wlan0",
+                "mode": "rw"
+            },
+            "/tmp/wpa_playground": {
+                "bind": "/tmp/wpa_playground",
+                "mode": "rw"
+            }
+        },
+        "privileged": true
+    }
+}"""
+
+SAMPLE_IMAGE = json.loads(
+    """{
+   "attrs":{
+      "date":"2021-04-09T17:51:18.065721638Z"
+   },
+   "id":"856fdf5e66c9b3697c25015556e7895c9066febb1a8ac8657a4eb41f2fc95a57"
+}"""
+)
+
+
+@pytest.mark.asyncio
+async def test_get_version() -> None:
+    """Tests if VersionChooser.get_version is reading SAMPLE_JSON properly
+
+    Interacts with:
+        - docker client (get images)
+        - Settigns file
+    """
+    client_mock = mock.MagicMock()
+    chooser = VersionChooser(client_mock)
+
+    attrs = {
+        "images.get.return_value.id": "856fdf5e66c9b3697c25015556e7895c9066febb1a8ac8657a4eb41f2fc95a57",
+        "images.get.return_value.attrs.__getitem__.return_value": {"date": "2021-04-09T17:51:18.065721638Z"},
+    }
+    client_mock.configure_mock(**attrs)
+
+    # Mock so it doesn't try to read a real file from the filesystem
+    with mock.patch("builtins.open", mock.mock_open(read_data=SAMPLE_JSON)):
+
+        response = chooser.get_version()
+        result = json.loads(response.text)
+        assert result["repository"] == "bluerobotics/companion-core"
+        assert result["tag"] == "master"
+        assert len(client_mock.mock_calls) > 0
+
+
+version = {"tag": "master", "image": "bluerobotics/companion-core", "pull": False}
+
+EXPECTED_SET_VERSION_WRITE_CALL = """{  "core": {
+    "tag": "master",
+    "image": "bluerobotics/companion-core",
+    "enabled": true,
+  '
+            '  "webui": false,
+    "network": "host",
+    "binds": {
+      "/dev/": {
+        "bind": "/dev/",
+  '
+            '      "mode": "rw"
+      },
+      "/var/run/wpa_supplicant/wlan0": {
+        "bind": "/var/run/wpa_sup'
+            'plicant/wlan0",
+        "mode": "rw"
+      },
+      "/tmp/wpa_playground": {
+        "bind": "/tmp/wp'
+            'a_playground",
+        "mode": "rw"
+      }
+    },
+    "privileged": true
+  }
+}"""
+
+
+@pytest.mark.asyncio
+@mock.patch("aiohttp.web.StreamResponse.write", new_callable=AsyncMock)
+async def test_set_version(write_mock: AsyncMock) -> None:
+    client = mock.MagicMock()
+    chooser = VersionChooser(client)
+    with mock.patch("builtins.open", mock.mock_open(read_data=SAMPLE_JSON)):
+
+        result = await chooser.set_version("bluerobotics/companion-core", "master")
+        assert await write_mock.called_once_with(EXPECTED_SET_VERSION_WRITE_CALL)
+        assert result.status == 200
+
+
+@pytest.mark.asyncio
+@mock.patch("json.load", return_value={})
+async def test_set_version_invalid_settings(json_mock: mock.MagicMock) -> None:
+    client = mock.MagicMock()
+    chooser = VersionChooser(client)
+    with mock.patch("builtins.open", mock.mock_open(read_data="{}")):
+        request_mock = AsyncMock()
+        request_mock.json = AsyncMock(return_value=version)
+        result = await chooser.set_version("bluerobotics/companion-core", "master")
+        assert result.status == 500
+        assert len(json_mock.mock_calls) > 0
+
+
+image_list = [
+    mock.MagicMock(
+        attrs={"Created": "2021-04-09T17:51:18.065721638Z", "Architecture": "amd64"},
+        id="856fdf5e66c9b3697c25015556e7895c9066febb1a8ac8657a4eb41f2fc95a57",
+        tags=["companion-core:test1"],
+    ),
+    mock.MagicMock(
+        attrs={"Created": "2021-04-09T17:51:18.065721638Z", "Architecture": "amd64"},
+        id="856fdf5e66c9b36remoteID856fdf5e66c9b36",
+        tags=["companion-core:test2"],
+    ),
+]
+
+
+@pytest.mark.asyncio
+@pytest.mark.filterwarnings("ignore: Error status")  # Suppress warning of dockerhub being unavailable
+@mock.patch("aiohttp.client.ClientSession.get")
+async def test_get_available_versions_dockerhub_unavailable(
+    get_mock: mock.MagicMock,
+) -> None:
+    get_mock.configure_mock(status=500)
+    client_mock = mock.MagicMock()
+    attrs = {"images.list.return_value": image_list}
+    client_mock.configure_mock(**attrs)
+    chooser = VersionChooser(client_mock)
+    result = await chooser.get_available_versions("bluerobotics/companion-core")
+    data = json.loads(result.text)
+    assert "local" in data
+    assert "remote" in data
+    assert data["local"][0]["tag"] == "test1"
+    assert data["local"][1]["tag"] == "test2"
+    assert len(client_mock.mock_calls) > 0
+
+
+@pytest.mark.asyncio
+async def test_get_available_versions() -> None:
+    client_mock = mock.MagicMock()
+    attrs = {"images.list.return_value": image_list}
+    client_mock.configure_mock(**attrs)
+
+    chooser = VersionChooser(client_mock)
+    result = await chooser.get_available_versions("bluerobotics/companion-core")
+    data = json.loads(result.text)
+    assert "local" in data
+    assert "remote" in data
+    assert data["local"][0]["tag"] == "test1"
+    assert data["local"][1]["tag"] == "test2"
+    assert len(client_mock.mock_calls) > 0
+
+
+@pytest.mark.asyncio
+async def test_get_version_invalid_file() -> None:
+    client = mock.MagicMock()
+    with mock.patch("builtins.open", mock.mock_open(read_data="{}")):
+        chooser = VersionChooser(client)
+        response = chooser.get_version()
+        assert response.status == 500
+
+
+@pytest.mark.asyncio
+@mock.patch("json.load")
+async def test_get_version_json_exception(json_mock: mock.MagicMock) -> None:
+    client = mock.MagicMock()
+    json_mock.side_effect = Exception()
+    with mock.patch("builtins.open", mock.mock_open(read_data="")):
+        chooser = VersionChooser(client)
+        response = chooser.get_version()
+        assert response.status == 500
+        assert len(json_mock.mock_calls) > 0
+
+
+@pytest.mark.asyncio
+@mock.patch("json.load")
+async def test_set_version_json_exception(json_mock: mock.MagicMock) -> None:
+    client = mock.MagicMock()
+    json_mock.side_effect = Exception()
+    chooser = VersionChooser(client)
+    with mock.patch("builtins.open", mock.mock_open(read_data="{}")):
+        result = await chooser.set_version("bluerobotics/companion-core", "master")
+        assert result.status == 500
+        assert len(json_mock.mock_calls) > 0
