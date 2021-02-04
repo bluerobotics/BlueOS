f72511146a462b883dc18556f53c682542597d26
Patrick José Pereira <patrickelectric@gmail.com>
2021-02-04 10:04:06 -0300

    core: services: helper: First commit
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/services/helper/main.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import http
+import urllib
+from typing import List
+from urllib.request import urlopen
+
+import bottle
+import psutil
+
+PORT = 8080
+
+
+class Helper:
+    @staticmethod
+    def check_webpage(port: int) -> bool:
+        try:
+            urlopen(f"http://0.0.0.0:{port}/", timeout=0.2)
+        except urllib.error.HTTPError as error:
+            if error.code == http.HTTPStatus.NOT_FOUND:
+                return True
+            return False
+        except Exception as error:
+            return False
+        else:
+            return True
+
+    @staticmethod
+    def get_webpage_ports() -> List[int]:
+        # Filter for TCP ports that are listen and can be accessed by external users (server in 0.0.0.0)
+        connections = iter(psutil.net_connections("tcp"))
+        connections = filter(lambda connection: connection.status == psutil.CONN_LISTEN, connections)
+        connections = filter(lambda connection: connection.laddr.ip == "0.0.0.0", connections)
+
+        # And check if there is a webpage available that is not us
+        ports = iter([connection.laddr.port for connection in connections])
+        ports = filter(lambda port: port != PORT, ports)
+        ports = filter(Helper.check_webpage, ports)
+
+        return list(ports)
+
+
+if __name__ == "__main__":
+
+    @bottle.route("/")
+    def home() -> str:
+        ports = Helper.get_webpage_ports()
+        return r"<br>".join([f'<a href="/" onclick="javascript:event.target.port={port}">{port}</a>' for port in ports])
+
+    bottle.run(host="0.0.0.0", port=PORT)
---
+++ core/services/helper/setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import os
+import ssl
+
+from setuptools import setup
+
+# Ignore ssl if it fails
+if not os.environ.get("PYTHONHTTPSVERIFY", "") and getattr(ssl, "_create_unverified_context", None):
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+setup(
+    name="helper_service",
+    version="0.1.0",
+    description="Helper information for development",
+    license="MIT",
+    install_requires=[
+        "bottle == 0.12.8",
+        "psutil == 5.7.2",
+    ],
+)
