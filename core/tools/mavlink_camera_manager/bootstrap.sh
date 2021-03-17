3966e36e292bf825aca0c5f695d92a248b638b80
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-17 12:18:24 -0300

    core: tools: Add mavlink_camera_manager
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/tools/mavlink_camera_manager/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+LOCAL_BINARY_PATH="/usr/bin/mavlink-camera-manager"
+REMOTE_BINARY_URL="https://s3.amazonaws.com/downloads.bluerobotics.com/companion-docker/tools/mavlink-camera-manager"
+
+# Download and install the camera manager under user binary folder with the correct permissions
+DEBIAN_FRONTEND=noninteractive apt --yes install wget
+wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
+chmod +x "$LOCAL_BINARY_PATH"
+DEBIAN_FRONTEND=noninteractive apt --yes purge wget
+DEBIAN_FRONTEND=noninteractive apt --yes autoremove
