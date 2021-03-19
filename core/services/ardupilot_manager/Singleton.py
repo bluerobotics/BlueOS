c2ac336d9fa22a66dac1e7392b90774aa48a2352
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-03-19 16:38:55 -0300

    ArduPilotManager: turn class into Singleton
    With the current code structure, it only makes sense to have one instance of the manager running.

---
+++ core/services/ardupilot_manager/Singleton.py
@@ -0,0 +1,10 @@
+from typing import Any, Dict
+
+
+class Singleton(type):
+    _instances: Dict[Any, Any] = {}
+
+    def __call__(cls: Any, *args: Any, **kwargs: Any) -> Any:
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
