16479d43577bb7635fe68d2beb0841b80330884a
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-03-26 19:46:41 -0300

    core: ardupilot-manager: Add dependencies for the rest api

--- core/services/ardupilot_manager/setup.py
+++ core/services/ardupilot_manager/setup.py
@@ -13,5 +13,11 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     python_requires=">=3.6",
-    install_requires=["appdirs==1.4.4", "packaging==20.4", "smbus2==0.3.0"],
+    install_requires=[
+        "appdirs==1.4.4",
+        "packaging==20.4",
+        "smbus2==0.3.0",
+        "fastapi==0.63.0",
+        "uvicorn==0.13.4",
+    ],
 )
