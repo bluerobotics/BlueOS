1d8a1992c5ed1ce893783082173d93f64f171164
Patrick José Pereira <patrickelectric@gmail.com>
2021-03-30 11:23:37 -0300

    core: tools: mavlink-camera-manager: Add x86 binary
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/mavlink_camera_manager/bootstrap.sh
+++ core/tools/mavlink_camera_manager/bootstrap.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env bash
 
 LOCAL_BINARY_PATH="/usr/bin/mavlink-camera-manager"
+
+# By default we install armv7
 REMOTE_BINARY_URL="https://github.com/patrickelectric/mavlink-camera-manager/releases/download/t3.0.1/mavlink-camera-manager-armv7"
+if [[ "$(uname -m)" == "x86_64"* ]]; then
+    REMOTE_BINARY_URL="https://github.com/patrickelectric/mavlink-camera-manager/releases/download/t3.0.1/mavlink-camera-manager-linux-desktop"
+fi
 
 # Download and install the camera manager under user binary folder with the correct permissions
 DEBIAN_FRONTEND=noninteractive apt --yes install wget
