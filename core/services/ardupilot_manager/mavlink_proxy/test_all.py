28b6a13d48ef141c4f9128ae6ab32ea8e6c3a194
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-04-14 11:42:49 -0300

    core: ardupilot-manager: Change TCPServer enumerator
    The new value is more consistent to the syntax used on UDP endpoints, and is the correct value for MavProxy TCP server endpoints, as well as pymavlink.

--- core/services/ardupilot_manager/mavlink_proxy/test_all.py
+++ core/services/ardupilot_manager/mavlink_proxy/test_all.py
@@ -28,7 +28,7 @@ def test_endpoint() -> None:
 
 
 def test_endpoint_validators() -> None:
-    Endpoint.is_mavlink_endpoint({"connection_type": "tcp", "place": "0.0.0.0", "argument": 14550})
+    Endpoint.is_mavlink_endpoint({"connection_type": "tcpin", "place": "0.0.0.0", "argument": 14550})
     Endpoint.is_mavlink_endpoint({"connection_type": "serial", "place": "/dev/autopilot", "argument": 115200})
     Endpoint.is_mavlink_endpoint({"connection_type": "file", "place": "mavlink_dump"})
     with pytest.raises(ValueError):
