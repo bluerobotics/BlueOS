983fd3d8e4dd1a732fee2d7055806ca4cca42452
Willian Galvani <williangalvani@gmail.com>
2021-04-21 11:15:03 -0300

    create .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+/*global module*/
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ['eslint:recommended', 'plugin:vue/vue3-strongly-recommended'],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  plugins: ['vue', 'html'],
+  rules: {
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    camelcase: [2, { properties: 'always' }],
+    'space-before-function-paren': ['error', 'always'],
+  },
+}
