adf00d9872347c64e7c10e8764da16f2a742a377
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-04-28 12:53:44 -0300

    core: cable-guy: Remove CORS headers

--- core/services/cable_guy/main.py
+++ core/services/cable_guy/main.py
@@ -25,15 +25,6 @@ def resource(path: str, filename: str) -> Any:
     return flask.send_from_directory(real_path, filename)
 
 
-# Flask CORS appears to be not working anymore with connexion
-# Check: https://github.com/zalando/connexion/issues/357
-def set_cors_headers_on_response(response: Any) -> Any:
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Headers"] = "X-Requested-With"
-    response.headers["Access-Control-Allow-Methods"] = "OPTIONS"
-    return response
-
-
 if __name__ == "__main__":
     if os.geteuid() != 0:
         print(
@@ -42,7 +33,6 @@ if __name__ == "__main__":
         sys.exit(1)
 
     connexion_app = connexion.FlaskApp(__name__)
-    connexion_app.app.after_request(set_cors_headers_on_response)
     connexion_app.add_url_rule("/", "index", index)
     connexion_app.add_url_rule("/static/<path:path>/<path:filename>", "resource", resource)
     connexion_app.add_api(
