b8ff34d57c70ee6ed1913607cb7cdaab4fd0b994
Patrick José Pereira <patrickelectric@gmail.com>
2021-05-04 15:36:33 -0300

    core: services: html: Update service to use fastapi and html files
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/helper/main.py
+++ core/services/helper/main.py
@@ -1,42 +1,51 @@
 #!/usr/bin/env python3
 
 import http
+import json
 import urllib
-from dataclasses import dataclass
-from typing import List
+from pathlib import Path
+from typing import Any, List
 from urllib.request import urlopen
 
-import bottle
 import psutil
+import uvicorn
 from bs4 import BeautifulSoup
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi_versioning import VersionedFastAPI, version
+from pydantic import BaseModel
+from starlette.responses import Response as StarletteResponse
 
 PORT = 80
 DOCS_CANDIDATE_URLS = ["/v1.0/ui/", "/docs"]
 
+HTML_FOLDER = Path.joinpath(Path(__file__).parent.absolute(), "html")
 
-@dataclass
-class ServiceInfo:
+
+class PrettyJSONResponse(StarletteResponse):
+    media_type = "application/json"
+
+    def render(self, content: Any) -> bytes:
+        return json.dumps(
+            content,
+            ensure_ascii=False,
+            allow_nan=False,
+            indent=2,
+            separators=(", ", ": "),
+        ).encode(self.charset)
+
+
+class ServiceInfo(BaseModel):
     valid: bool
     title: str
-    ui: str
+    documentation_url: str
     port: int
-
-    def as_html(self) -> str:
-        port_html = f"""
-        <a href="#" onclick="javascript:event.target.port={self.port}">{self.port}:</a>{self.title}
-        """
-        swagger_html = f"""
-        <a href="#" onclick="window.location.href = window.location.origin + ':' + {self.port} + '{self.ui}'">Open Api/Docs</a>
-        """
-        if self.ui:
-            return port_html + swagger_html
-        return port_html
 
 
 class Helper:
     @staticmethod
     def detect_service(port: int) -> ServiceInfo:
-        info = ServiceInfo(valid=False, title="Unknown", ui="", port=port)
+        info = ServiceInfo(valid=False, title="Unknown", documentation_url="", port=port)
 
         try:
             with urlopen(f"http://127.0.0.1:{port}/", timeout=0.2) as response:
@@ -53,13 +62,13 @@ class Helper:
         if not info.valid:
             return info
 
-        for ui_path in DOCS_CANDIDATE_URLS:
+        for documentation_path in DOCS_CANDIDATE_URLS:
             try:
-                urlopen(f"http://127.0.0.1:{port}{ui_path}", timeout=0.2)
-                info.ui = ui_path
+                urlopen(f"http://127.0.0.1:{port}{documentation_path}", timeout=0.2)
+                info.documentation_url = documentation_path
                 break
             except Exception as error:
-                pass  # any error here only means there's no ui available
+                pass  # any error here only means there's no documentation ui available
         return info
 
     @staticmethod
@@ -77,11 +86,32 @@ class Helper:
         return list(valid_services)
 
 
+fast_api_app = FastAPI(
+    title="Helper API",
+    description="Everybody's helper to find web services that are running in companion.",
+    default_response_class=PrettyJSONResponse,
+)
+
+
+@fast_api_app.get(
+    "/web_services",
+    response_model=List[ServiceInfo],
+    summary="Retrieve web services found.",
+)
+@version(1, 0)
+def web_services() -> Any:
+    """REST API endpoint to retrieve web services running."""
+    return Helper.scan_ports()
+
+
+app = VersionedFastAPI(
+    fast_api_app,
+    version="1.0.0",
+    prefix_format="/v{major}.{minor}",
+    enable_latest=True,
+)
+
+app.mount("/", StaticFiles(directory=str(HTML_FOLDER), html=True))
+
 if __name__ == "__main__":
-
-    @bottle.route("/")
-    def home() -> str:
-        services = Helper.scan_ports()
-        return r"<br>".join([service.as_html() for service in services])
-
-    bottle.run(host="0.0.0.0", port=PORT)
+    uvicorn.run(app, host="0.0.0.0", port=PORT)
--- core/services/helper/setup.py
+++ core/services/helper/setup.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 import os
+import pathlib
+import shutil
 import ssl
+import sys
+import urllib.request
 
 from setuptools import setup
 
@@ -9,14 +13,62 @@ from setuptools import setup
 if not os.environ.get("PYTHONHTTPSVERIFY", "") and getattr(ssl, "_create_unverified_context", None):
     ssl._create_default_https_context = ssl._create_unverified_context
 
+
+def populate_static_files() -> None:
+    # Ignore ssl when downloading new files
+    # This can happen since python can fail to verify certificate
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    current_folder = pathlib.Path(__file__).parent.absolute()
+    static_folder = pathlib.Path.joinpath(current_folder, "html/static")
+
+    static_files = {
+        "js": [
+            "https://unpkg.com/axios@0.19.2/dist/axios.min.js",
+            "https://cdn.metroui.org.ua/v4/js/metro.min.js",
+            "https://unpkg.com/vue@2.6.11/dist/vue.js",
+        ],
+        "css": [
+            "https://cdn.metroui.org.ua/v4/css/metro-all.min.css",
+        ],
+    }
+
+    # Delete static folder and redownload everything
+    if static_folder.exists():
+        shutil.rmtree(static_folder, ignore_errors=True)
+    static_folder.mkdir()
+
+    for path, urls in static_files.items():
+        print(f"Downloading in {path}..")
+        for url in urls:
+            print(f"File: {url}")
+            try:
+                # Create path of file
+                download_path = pathlib.Path.joinpath(static_folder, path)
+                download_path.mkdir(exist_ok=True)
+                name = url.split("/")[-1]
+                # Create path with file name to download
+                download_file_path = pathlib.Path.joinpath(download_path, name)
+                urllib.request.urlretrieve(url, str(download_file_path))
+            except Exception as error:
+                print(f"Unable to download, error: {error}")
+                sys.exit(1)
+
+
+populate_static_files()
+
 setup(
     name="helper_service",
     version="0.1.0",
     description="Helper information for development",
     license="MIT",
     install_requires=[
-        "bottle == 0.12.19",
-        "psutil == 5.7.2",
+        "aiofiles == 0.6.0",
         "beautifulsoup4 == 4.9.3",
+        "fastapi == 0.63.0",
+        "fastapi-versioning == 0.9.0",
+        "psutil == 5.7.2",
+        "starlette == 0.13.6",
+        "uvicorn == 0.13.4",
     ],
 )
