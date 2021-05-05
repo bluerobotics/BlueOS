ab0c304d10febd8adb3fc2379450a6fd797799c7
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-05-05 14:10:13 -0300

    core: wifi: Move WiFi service to FastAPI

--- core/services/wifi/main.py
+++ core/services/wifi/main.py
@@ -2,11 +2,22 @@
 
 import json
 import os
-from typing import Any
+import sys
+from typing import Any, List
 
-import bottle
+import uvicorn
+from fastapi import FastAPI, HTTPException, status
+from fastapi_versioning import VersionedFastAPI, version
+from starlette.responses import Response as StarletteResponse
 
-from WifiManager import WifiManager
+from WifiManager import (
+    SavedWifiNetwork,
+    ScannedWifiNetwork,
+    WifiCredentials,
+    WifiManager,
+)
+
+wifi_manager = WifiManager()
 
 BARE_HTML_TEMPLATE = """
 <html lang="en">
@@ -19,51 +30,71 @@ BARE_HTML_TEMPLATE = """
 </html>
 """
 
+
+class PrettyJSONResponse(StarletteResponse):
+    media_type = "application/json"
+
+    def render(self, content: Any) -> bytes:
+        return json.dumps(
+            content,
+            ensure_ascii=False,
+            allow_nan=False,
+            indent=2,
+            separators=(", ", ": "),
+        ).encode(self.charset)
+
+
+app = FastAPI(
+    title="WiFi Manager API",
+    description="WiFi Manager is responsible for managing WiFi connections on Companion.",
+    default_response_class=PrettyJSONResponse,
+)
+
+
+@app.get("/status", summary="Retrieve status of wifi manager.")
+@version(1, 0)
+def network_status() -> Any:
+    return wifi_manager.status()
+
+
+@app.get("/scan", response_model=List[ScannedWifiNetwork], summary="Retrieve available wifi networks.")
+@version(1, 0)
+def scan() -> Any:
+    try:
+        return wifi_manager.get_wifi_available()
+    except ValueError as error:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error)) from error
+
+
+@app.get("/saved", response_model=List[SavedWifiNetwork], summary="Retrieve saved wifi networks.")
+@version(1, 0)
+def saved() -> Any:
+    try:
+        return wifi_manager.get_saved_wifi_network()
+    except ValueError as error:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error)) from error
+
+
+@app.post("/connect", summary="Retrieve ethernet interfaces.")
+@version(1, 0)
+def connect(credentials: WifiCredentials) -> Any:
+    wifi_manager.set_wifi_password(credentials)
+    return wifi_manager.status()
+
+
+app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
+
+
 if __name__ == "__main__":
-    wifi_manager = WifiManager()
+    if os.geteuid() != 0:
+        print("You need root privileges to run this script.\nPlease try again, this time using **sudo**. Exiting.")
+        sys.exit(1)
+
     WLAN0_SOCKET = "/var/run/wpa_supplicant/wlan0"
-    REST_API_PREFIX = "/service/network/wifi"
     if not os.path.exists(WLAN0_SOCKET):
         print("Connecting via internet socket.")
         wifi_manager.connect(("0.0.0.0", 6664))
     else:
         wifi_manager.connect(WLAN0_SOCKET)
 
-    def to_pretty_json(data: Any) -> bytes:
-        bottle.response.content_type = "application/json; charset=UTF-8"
-        return json.dumps(data, sort_keys=True, indent=4, ensure_ascii=False).encode("utf-8")
-
-    @bottle.route("/")
-    def home() -> str:
-        body = r"<br>".join([f'<a href="{route.rule}">{route.rule}</a>' for route in bottle.app().routes])
-        page = BARE_HTML_TEMPLATE.format(title="Wifi Manager", body=body)
-        return page
-
-    @bottle.route(REST_API_PREFIX + "/status")
-    def status() -> bytes:
-        return to_pretty_json(wifi_manager.status())
-
-    @bottle.route(REST_API_PREFIX + "/scan")
-    def scan() -> bytes:
-        return to_pretty_json(wifi_manager.get_wifi_available())
-
-    @bottle.route(REST_API_PREFIX + "/saved")
-    def saved() -> bytes:
-        return to_pretty_json(wifi_manager.get_saved_wifi_network())
-
-    @bottle.post(REST_API_PREFIX + "/connect")
-    def connect() -> bytes:
-        # curl -H "Content-Type: application/json"
-        #   --data '{ "ssid": "Patrick", "password": "Password" }'
-        #   --request POST http://0.0.0.0:8080/network/wifi/connect
-        data = json.loads(bottle.request.body.read().decode("utf-8"))
-
-        if not "ssid" in data or data["ssid"].strip() == "":
-            return "No ssid provided".encode("utf-8")
-
-        ssid = data["ssid"]
-        password = data["password"] if "password" in data else ""
-        wifi_manager.set_wifi_password(ssid, password)
-        return to_pretty_json(wifi_manager.status())
-
-    bottle.run(host="0.0.0.0", port=9000)
+    uvicorn.run(app, host="0.0.0.0", port=9000)
