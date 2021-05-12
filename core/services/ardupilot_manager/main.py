3ebcf24f3130354923a6a2590a6e5b440ec53254
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-05-12 14:49:04 -0300

    core: ardupilot-manager: Add versioning to REST API

--- core/services/ardupilot_manager/main.py
+++ core/services/ardupilot_manager/main.py
@@ -4,6 +4,7 @@ from typing import Any, Dict, List, Set
 
 import uvicorn
 from fastapi import Body, FastAPI, Response, status
+from fastapi_versioning import VersionedFastAPI, version
 from starlette.responses import Response as StarletteResponse
 
 from ArduPilotManager import ArduPilotManager
@@ -28,11 +29,13 @@ autopilot = ArduPilotManager()
 
 
 @app.get("/endpoints", response_model=List[Dict[str, Any]])
+@version(1, 0)
 def get_available_endpoints() -> Any:
     return [endpoint.asdict() for endpoint in autopilot.get_endpoints()]
 
 
 @app.post("/endpoints", status_code=status.HTTP_201_CREATED)
+@version(1, 0)
 def create_endpoints(response: Response, endpoints: Set[Endpoint] = Body(...)) -> Any:
     if not autopilot.add_new_endpoints(endpoints):
         response.status_code = status.HTTP_409_CONFLICT
@@ -40,11 +43,14 @@ def create_endpoints(response: Response, endpoints: Set[Endpoint] = Body(...)) -
 
 
 @app.delete("/endpoints", status_code=status.HTTP_204_NO_CONTENT)
+@version(1, 0)
 def remove_endpoints(response: Response, endpoints: Set[Endpoint] = Body(...)) -> Any:
     if not autopilot.remove_endpoints(endpoints):
         response.status_code = status.HTTP_409_CONFLICT
         return {"message": "One or more endpoints does not exist or is protected. No changes were made."}
 
+
+app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
 
 if __name__ == "__main__":
     autopilot.run()
