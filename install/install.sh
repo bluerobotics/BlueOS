206b56d246013978c869549a32f4c135c0a4ce16
Patrick José Pereira <patrickelectric@gmail.com>
2021-05-19 11:25:34 -0300

    install: Add information about computer access webpage and ssh
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- install/install.sh
+++ install/install.sh
@@ -88,5 +88,8 @@ echo "Starting network configuration."
 curl -fsSL $ROOT/install/network/avahi.sh | bash
 
 echo "Installation finished successfully."
+echo "You can access after the reboot:"
+echo "- The computer webpage: http://companion.local"
+echo "- The ssh client: pi@companion.local"
 echo "System will reboot in 10 seconds."
 sleep 10 && reboot
