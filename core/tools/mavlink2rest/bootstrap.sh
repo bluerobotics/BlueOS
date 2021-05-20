2fc388d1646e3fc4984de742d5faa81df61e33f9
Patrick José Pereira <patrickelectric@gmail.com>
2021-05-20 16:44:58 -0300

    core: tools: Add mavlink2rest
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/tools/mavlink2rest/bootstrap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+LOCAL_BINARY_PATH="/usr/bin/mavlink2rest"
+
+VERSION=t0.11.0
+
+# By default we install armv7
+REMOTE_BINARY_URL="https://github.com/patrickelectric/mavlink2rest/releases/download/${VERSION}/mavlink2rest-armv7-unknown-linux-musleabihf"
+if [[ "$(uname -m)" == "x86_64"* ]]; then
+    REMOTE_BINARY_URL="https://github.com/patrickelectric/mavlink2rest/releases/download/${VERSION}/mavlink2rest-x86_64-unknown-linux-musl"
+fi
+
+# Download and install mavlink2rest under user binary folder with the correct permissions
+wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
+chmod +x "$LOCAL_BINARY_PATH"
