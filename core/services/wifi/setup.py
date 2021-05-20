25cec096d3109b0da1fae83b8aa3fe64e550c388
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-05-20 14:03:37 -0300

    core: wifi: Add web interface

--- core/services/wifi/setup.py
+++ core/services/wifi/setup.py
@@ -1,15 +1,40 @@
 #!/usr/bin/env python3
 
-import os
+import pathlib
 import ssl
+import urllib.request
+from warnings import warn
 
-from setuptools import setup
+import setuptools
 
-# Ignore ssl if it fails
-if not os.environ.get("PYTHONHTTPSVERIFY", "") and getattr(ssl, "_create_unverified_context", None):
-    ssl._create_default_https_context = ssl._create_unverified_context
+ssl._create_default_https_context = ssl._create_unverified_context
 
-setup(
+
+static_files = {
+    "js/axios.min.js": "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js",
+    "js/polyfill.min.js": "https://polyfill.io/v3/polyfill.min.js?features=es2015,IntersectionObserver",
+    "js/vue.js": "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.js",
+    "js/metro.js": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/js/metro.min.js",
+    "css/metro-all.css": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/css/metro-all.css",
+    "mif/metro.woff": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.woff",
+    "mif/metro.ttf": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.ttf",
+}
+
+current_folder = pathlib.Path(__file__).parent.absolute()
+frontend_folder = pathlib.Path.joinpath(current_folder, "frontend")
+static_folder = pathlib.Path.joinpath(frontend_folder, "static")
+static_folder.mkdir(exist_ok=True)
+
+for filename, url in static_files.items():
+    path = pathlib.Path.joinpath(static_folder, filename)
+    path.parent.mkdir(exist_ok=True)
+    try:
+        urllib.request.urlretrieve(url, path)
+    except Exception as error:
+        warn(f"unable to open url {url}, error {error}")
+
+
+setuptools.setup(
     name="wifi_service",
     version="0.1.0",
     description="Wifi manager for wpa_supplicant",
