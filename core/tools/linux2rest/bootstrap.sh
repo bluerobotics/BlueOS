72a5ac6ae7ebd1808cd443338a1cfe49cc1fa2f2
Willian Galvani <williangalvani@gmail.com>
2021-05-21 08:38:08 -0300

    core: tools: linux2rest: dont install wget (it is already in the image)

--- core/tools/linux2rest/bootstrap.sh
+++ core/tools/linux2rest/bootstrap.sh
@@ -10,10 +10,6 @@ if [[ "$(uname -m)" == "x86_64"* ]]; then
 fi
 
 # Download and install necessary libraries for linux2rest
-DEBIAN_FRONTEND=noninteractive apt --yes install wget libudev-dev
+DEBIAN_FRONTEND=noninteractive apt --yes install libudev-dev
 wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
 chmod +x "$LOCAL_BINARY_PATH"
-# Remove necessary stuff to install binary, creating a small docker layer
-## Some libraries are still necessary
-DEBIAN_FRONTEND=noninteractive apt --yes purge wget
-DEBIAN_FRONTEND=noninteractive apt --yes autoremove
