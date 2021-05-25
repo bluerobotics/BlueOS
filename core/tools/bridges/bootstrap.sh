edf2452cafcfa497431551256baefef99932f027
Willian Galvani <williangalvani@gmail.com>
2021-05-25 19:54:37 +0100

    core: tools: bridges: don't (un)install wget
    wget is one of the basic tools installed in companion-base.
    it shouldn't be uninstalled or reinstalled in core.

--- core/tools/bridges/bootstrap.sh
+++ core/tools/bridges/bootstrap.sh
@@ -9,10 +9,5 @@ if [[ "$(uname -m)" == "x86_64"* ]]; then
     REMOTE_BINARY_URL="https://github.com/patrickelectric/bridges/releases/download/${VERSION}/bridges-x86_64-unknown-linux-musl"
 fi
 
-DEBIAN_FRONTEND=noninteractive apt --yes install wget
 wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
 chmod +x "$LOCAL_BINARY_PATH"
-# Remove necessary stuff to install binary, creating a small docker layer
-## Some libraries are still necessary
-DEBIAN_FRONTEND=noninteractive apt --yes purge wget
-DEBIAN_FRONTEND=noninteractive apt --yes autoremove
