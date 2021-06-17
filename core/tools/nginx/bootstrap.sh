7ba19eb279bd8654812d0880a8fffc1bfa08c9ba
Willian Galvani <williangalvani@gmail.com>
2021-06-17 17:21:18 -0300

    core: tools: create nginx's bootstrap.sh

---
+++ core/tools/nginx/bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Exit if something goes wrong
+set -e
+
+# Nginx works as a reverse proxy to merge all services into one server
+echo "Installing nginx."
+apt install --no-install-recommends --no-install-suggests -y nginx
