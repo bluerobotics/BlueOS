48eca0f314a5b9eb9a8f49b6ada90147a35f19fd
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-06-19 20:24:15 -0300

    eslint: Add typescript rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,11 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['eslint:recommended', 'plugin:vue/vue3-strongly-recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-strongly-recommended',
+    '@vue/typescript/recommended',
+  ],
   parserOptions: {
     ecmaVersion: 12,
   },
