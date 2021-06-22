badc77b972790edce7a57cd9b0e0c6fed1a24488
Patrick José Pereira <patrickelectric@gmail.com>
2021-06-22 10:46:19 -0300

    core: nginx: Add apt update before install
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/nginx/bootstrap.sh
+++ core/tools/nginx/bootstrap.sh
@@ -5,4 +5,5 @@ set -e
 
 # Nginx works as a reverse proxy to merge all services into one server
 echo "Installing nginx."
+apt update
 apt install --no-install-recommends --no-install-suggests -y nginx
