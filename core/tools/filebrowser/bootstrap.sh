c38dc6db3ae28059b2879d1db6070aa8bdc15bbe
Patrick José Pereira <patrickelectric@gmail.com>
2021-06-24 10:44:14 -0300

    core: tools: Add filebrowser
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/tools/filebrowser/bootstrap.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+LOCAL_BINARY_PATH="/usr/bin/filebrowser"
+VERSION=v2.15.0
+
+# By default we install armv7
+REMOTE_BINARY_URL="https://github.com/filebrowser/filebrowser/releases/download/${VERSION}/linux-armv7-filebrowser.tar.gz"
+if [[ "$(uname -m)" == "x86_64"* ]]; then
+    REMOTE_BINARY_URL="https://github.com/filebrowser/filebrowser/releases/download/${VERSION}/linux-amd64-filebrowser.tar.gz"
+fi
+
+wget "$REMOTE_BINARY_URL" -O - | tar xz -C /usr/bin/
+chmod +x "$LOCAL_BINARY_PATH"
+
+# Create configuration file
+DATABASE_PATH="$HOME/.config/filebrowser/filebrowser.db"
+mkdir -p $(dirname "$DATABASE_PATH")
+filebrowser config init --address=0.0.0.0 --port=7777 --auth.method=noauth --log=stdout --root=/ --database="$DATABASE_PATH"
+filebrowser users add pi raspberry --database="$DATABASE_PATH"
