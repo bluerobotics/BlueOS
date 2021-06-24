1d48f312a3e33eddeb0d0a7ead113e8e75e4e68d
Patrick José Pereira <patrickelectric@gmail.com>
2021-06-24 17:28:32 -0300

    core: libs: commonwealth: Fix calling setup.py from different path
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/libs/commonwealth/setup.py
+++ core/libs/commonwealth/setup.py
@@ -1,6 +1,11 @@
+import os
 import pathlib
 
 import setuptools
+
+# Force current path to be used as reference for Python
+## Fix problems related to calling setup.py from different paths
+os.chdir(os.path.abspath(os.path.dirname(__file__)))
 
 with open(pathlib.Path(__file__).parent.joinpath("README.md"), "r") as readme:
     long_description = readme.read()
