a107e16d1e7938d03f7987b5a0470e95f5fb805d
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-07-14 16:04:03 -0300

    core: ardupilot-manager: Add FirmwareUploader
    - Add Ardupilot firmware uploader tool
    - Add FirmwareUploader class to interface with the uploader tool

--- core/services/ardupilot_manager/exceptions.py
+++ core/services/ardupilot_manager/exceptions.py
@@ -1,3 +1,15 @@
 """
 Ardupilot-manager exception classes.
 """
+
+
+class FirmwareUploadFail(RuntimeError):
+    """Firmware upload operation failed."""
+
+
+class UploadToolNotFound(RuntimeError):
+    """Firmware upload tool not found."""
+
+
+class InvalidUploadTool(ValueError):
+    """Firmware upload tool cannot be validated."""
