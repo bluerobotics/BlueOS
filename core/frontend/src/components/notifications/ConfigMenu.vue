400384bf263fd4b9a9dd6663437072a51e716f35
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-11 13:50:08 -0300

    core: frontend: Create NotificationManager component and subs
    This set of components is responsible for listing notifications, allow dismissing them and choosing custom settings around them.

---
+++ core/frontend/src/components/notifications/ConfigMenu.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-card
+    elevation="1"
+    width="250"
+  >
+    <v-list>
+      <v-list-item>
+        <v-switch
+          :value="showOld"
+          color="blue lighten-1"
+          @change="emitShowOldChange"
+        />
+        <div>Show old messages</div>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NotificationsConfigMenu',
+  props: {
+    showOld: {
+      type: Boolean,
+    },
+  },
+  methods: {
+    emitShowOldChange(value: boolean): void {
+      this.$emit('update:show-old', value)
+    },
+  },
+})
+</script>
+
+<style>
+</style>
---
+++ core/frontend/src/components/notifications/TrayButton.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    nudge-left="500"
+    nudge-bottom="30"
+  >
+    <template
+      #activator="{on, attrs}"
+    >
+      <v-card
+        elevation="0"
+        color="transparent"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-badge
+          :content="number_of_notifications"
+          :value="number_of_notifications"
+          color="red"
+          overlap
+        >
+          <v-icon color="white">
+            mdi-bell
+          </v-icon>
+        </v-badge>
+      </v-card>
+    </template>
+    <notification-manager />
+  </v-menu>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { getModule } from 'vuex-module-decorators'
+
+import NotificationStore from '@/store/notifications'
+
+import NotificationManager from './NotificationManager.vue'
+
+const notification_store: NotificationStore = getModule(NotificationStore)
+
+export default Vue.extend({
+  name: 'NotificationTrayButton',
+  components: {
+    NotificationManager,
+  },
+  computed: {
+    number_of_notifications(): number {
+      return notification_store.active_cumulated_notifications.length
+    },
+  },
+})
+</script>
+
+<style>
+</style>
