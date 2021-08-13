6b95217fe0e72b90196ec755deeed24065b0f401
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-13 11:20:26 -0300

    core: frontend: Add WifiManager and WifiUpdater
    WifiManager is the component responsible for managing the wifi adapter state.
    It allows:
    - showing available networks (hidden or not) and it's properties
    - connecting to new networks (hidden or not)
    - connect to existing networks (without pass)
    - forgetting knows networks
    - forcing password change
    - disconnecting from current network
    - showing wifi adapter properties (including local IP, security type, signal level and others)
    
    WifiUpdater is instantiated by WifiManager and its responsible for constantly fetching available wifi networks, saved networks and adapter status.

---
+++ core/frontend/src/components/wifi/ConnectionDialog.vue
@@ -0,0 +1,222 @@
+<template>
+  <v-dialog
+    width="300"
+    :value="show"
+    @input="showDialog"
+  >
+    <v-card>
+      <v-card-title>
+        {{ real_ssid }}
+
+        <v-spacer />
+
+        <v-btn
+          icon
+          @click="toggleInfoShow()"
+        >
+          <v-icon>{{ show_more_info_icon }}</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-expand-transition>
+        <div v-show="show_more_info">
+          <v-card-text>
+            <v-simple-table
+              dense
+            >
+              <template #default>
+                <tbody>
+                  <tr
+                    v-for="(value, name) in network"
+                    :key="name"
+                  >
+                    <td>{{ name }}</td>
+                    <td>{{ value }}</td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+          </v-card-text>
+        </div>
+      </v-expand-transition>
+
+      <v-card-actions class="d-flex flex-column">
+        <v-container>
+          <v-row>
+            <v-text-field
+              v-if="is_hidden"
+              v-model="inputed_ssid"
+              label="SSID"
+              hide-details="auto"
+              class="pa-2"
+            />
+          </v-row>
+
+          <v-row>
+            <v-card
+              v-if="!show_password_input_box"
+              elevation="0"
+              @click="toggleForcePassword()"
+            >
+              <v-card-text>
+                Force new password
+              </v-card-text>
+            </v-card>
+            <password-input
+              v-if="show_password_input_box"
+              v-model="password"
+              @submit="connectToWifiNetwork"
+            />
+          </v-row>
+
+          <v-row
+            class="d-flex justify-space-around py-4"
+          >
+            <v-btn
+              v-if="network.saved"
+              dark
+              color="red lighten-1"
+              depressed
+              @click="removeSavedWifiNetwork"
+            >
+              Forget
+            </v-btn>
+
+            <v-btn
+              dark
+              color="blue darken-1"
+              depressed
+              @click="connectToWifiNetwork"
+            >
+              Connect
+            </v-btn>
+          </v-row>
+        </v-container>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import Vue, { PropType } from 'vue'
+import { getModule } from 'vuex-module-decorators'
+
+import NotificationsStore from '@/store/notifications'
+import WifiStore from '@/store/wifi'
+import { wifi_service } from '@/types/frontend_services'
+import { LiveNotification, NotificationLevel } from '@/types/notifications'
+import { Network, NetworkCredentials } from '@/types/wifi.d'
+
+import PasswordInput from '../common/PasswordInput.vue'
+
+const notification_store: NotificationsStore = getModule(NotificationsStore)
+const wifi_store: WifiStore = getModule(WifiStore)
+
+export default Vue.extend({
+  name: 'ConnectionDialog',
+  components: {
+    PasswordInput,
+  },
+  model: {
+    prop: 'show',
+    event: 'change',
+  },
+  props: {
+    network: {
+      type: Object as PropType<Network>,
+      required: true,
+    },
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data() {
+    return {
+      password: '',
+      force_password: false,
+      inputed_ssid: 'HIDDEN SSID',
+      show_more_info: false,
+    }
+  },
+  computed: {
+    show_more_info_icon(): string {
+      return this.show_more_info ? 'mdi-chevron-up' : 'mdi-information'
+    },
+    real_ssid(): string {
+      return this.is_hidden ? this.inputed_ssid : this.network.ssid
+    },
+    is_hidden(): boolean {
+      return this.network.ssid === null || this.network.ssid === ''
+    },
+    show_password_input_box(): boolean {
+      if (this.force_password) {
+        return true
+      }
+      if (this.network.saved || !this.network.locked) {
+        return false
+      }
+      return true
+    },
+  },
+  methods: {
+    toggleInfoShow(): void {
+      this.show_more_info = !this.show_more_info
+    },
+    toggleForcePassword(): void {
+      this.force_password = !this.force_password
+    },
+    async connectToWifiNetwork(): Promise<void> {
+      const credentials: NetworkCredentials = { ssid: this.real_ssid, password: this.password }
+      await axios({
+        method: 'post',
+        url: `${wifi_store.API_URL}/connect`,
+        timeout: 2000,
+        data: credentials,
+        params: { hidden: this.is_hidden },
+      })
+        .then(() => {
+          wifi_store.setNetworkStatus(null)
+        })
+        .catch((error) => {
+          notification_store.pushNotification(new LiveNotification(
+            NotificationLevel.Error,
+            wifi_service,
+            'WIFI_CONNECT_FAIL',
+            `Could not connect to wifi network: ${error.message}.`,
+          ))
+        })
+        .finally(() => {
+          this.password = ''
+          this.showDialog(false)
+        })
+    },
+    async removeSavedWifiNetwork(): Promise<void> {
+      await axios({
+        method: 'post',
+        url: `${wifi_store.API_URL}/remove`,
+        timeout: 10000,
+        params: { ssid: this.network.ssid },
+      })
+        .catch((error) => {
+          notification_store.pushNotification(new LiveNotification(
+            NotificationLevel.Error,
+            wifi_service,
+            'WIFI_FORGET_FAIL',
+            `Could not remove saved wifi network: ${error.message}.`,
+          ))
+        })
+        .finally(() => {
+          this.showDialog(false)
+        })
+    },
+    showDialog(state: boolean) {
+      this.$emit('change', state)
+    },
+  },
+})
+</script>
+
+<style>
+</style>
---
+++ core/frontend/src/components/wifi/WifiManager.vue
@@ -0,0 +1,124 @@
+<template>
+  <v-card
+    elevation="1"
+    width="500"
+  >
+    <v-container>
+      <v-container v-if="current_network">
+        <network-card
+          class="connected-network"
+          :network="current_network"
+          @click="openDisconnectionDialog"
+        />
+      </v-container>
+
+      <v-container v-if="are_connectable_networks_available">
+        <network-card
+          v-for="(network, key) in connectable_networks"
+          :key="key"
+          class="available-network"
+          :network="network"
+          @click="openConnectionDialog"
+        />
+      </v-container>
+      <v-container v-else>
+        <spinning-logo />
+      </v-container>
+    </v-container>
+    <wifi-updater />
+
+    <connection-dialog
+      v-if="selected_network"
+      v-model="show_connection_dialog"
+      :network="selected_network"
+    />
+
+    <disconnection-dialog
+      v-if="current_network"
+      v-model="show_disconnection_dialog"
+      :network="current_network"
+      :status="wifi_status"
+    />
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { getModule } from 'vuex-module-decorators'
+
+import WifiStore from '@/store/wifi'
+import { Network, WifiStatus } from '@/types/wifi.d'
+
+import SpinningLogo from '../common/SpinningLogo.vue'
+import ConnectionDialog from './ConnectionDialog.vue'
+import DisconnectionDialog from './DisconnectionDialog.vue'
+import NetworkCard from './NetworkCard.vue'
+import WifiUpdater from './WifiUpdater.vue'
+
+const wifi_store: WifiStore = getModule(WifiStore)
+
+export default Vue.extend({
+  name: 'WifiManager',
+  components: {
+    NetworkCard,
+    SpinningLogo,
+    ConnectionDialog,
+    DisconnectionDialog,
+    WifiUpdater,
+  },
+  data() {
+    return {
+      selected_network: null as Network | null,
+      show_connection_dialog: false,
+      show_disconnection_dialog: false,
+    }
+  },
+  computed: {
+    wifi_status(): WifiStatus | null {
+      return wifi_store.network_status
+    },
+    current_network(): Network | null {
+      return wifi_store.current_network
+    },
+    connectable_networks(): Network[] {
+      let showable_networks = wifi_store.available_networks
+      const { current_network } = wifi_store
+      if (current_network) {
+        showable_networks = showable_networks.filter((network: Network) => network.ssid !== current_network.ssid)
+      }
+      return showable_networks.sort((a: Network, b: Network) => b.signal - a.signal)
+    },
+    are_connectable_networks_available(): boolean {
+      return this.connectable_networks.length !== 0
+    },
+  },
+  methods: {
+    openConnectionDialog(network: Network): void {
+      this.selected_network = network
+      this.show_connection_dialog = true
+    },
+    openDisconnectionDialog(): void {
+      this.show_disconnection_dialog = true
+    },
+  },
+})
+</script>
+
+<style>
+  .connected-network {
+      background-color: #2799D2
+  }
+
+  .connected-network:hover {
+      cursor: pointer;
+  }
+
+  .available-network {
+      background-color: #f8f8f8;
+  }
+
+  .available-network:hover {
+      cursor: pointer;
+      background-color: #c5c5c5;
+  }
+</style>
