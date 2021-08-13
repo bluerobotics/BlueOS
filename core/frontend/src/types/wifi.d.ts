4d81ecc552e31e3b3256c733033404214596c3a8
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-13 11:20:26 -0300

    core: frontend: Add wifi type definitions

---
+++ core/frontend/src/types/wifi.d.ts
@@ -0,0 +1,43 @@
+export interface Network {
+    ssid: string
+    signal: number
+    locked: boolean
+    saved: boolean
+}
+
+export interface WifiStatus {
+    bssid: string
+    freq: number
+    ssid: string
+    id: number
+    mode: string
+    wifi_generation: number
+    pairwise_cipher: string
+    group_cipher: string
+    key_mgmt: string
+    wpa_state: string
+    ip_address: string
+    p2p_device_address: string
+    address: string
+    uuid: string
+}
+
+export interface WPANetwork {
+    ssid: string
+    bssid: string
+    flags: string
+    frequency: number
+    signallevel: number
+}
+
+export interface SavedNetwork {
+    networkid: number
+    ssid: string
+    bssid: string
+    flags: string
+}
+
+export interface NetworkCredentials {
+    ssid: string
+    password: string
+}
