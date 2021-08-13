c8f4576d5899220dc2b99eb4591f3eb7d27caedd
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-13 11:20:26 -0300

    core: frontend: Add WifiTrayMenu component
    This component is a small icon positioned at the top-right corner of the frontend, allowing the user to open the WifiManager.

---
+++ core/frontend/src/components/wifi/WifiTrayMenu.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-menu
+    :close-on-content-click="false"
+    nudge-left="500"
+    nudge-bottom="25"
+  >
+    <template
+      #activator="{ on, attrs }"
+    >
+      <v-card
+        class="px-1"
+        elevation="0"
+        color="transparent"
+        v-bind="attrs"
+        v-on="on"
+      >
+        <v-icon color="white">
+          mdi-wifi
+        </v-icon>
+      </v-card>
+    </template>
+    <wifi-manager />
+  </v-menu>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import WifiManager from './WifiManager.vue'
+
+export default Vue.extend({
+  name: 'WifiTrayMenu',
+  components: {
+    WifiManager,
+  },
+})
+</script>
+
+<style>
+</style>
