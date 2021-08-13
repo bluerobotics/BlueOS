09b05afa3cc803aa29709f742cc331629b23ff5c
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-13 11:20:26 -0300

    core: frontend: Add PasswordInput component
    This is a common component to be used for password input.
    It allows showing/hiding the input text with a button and emmiting a submit by pressing the Enter key.

---
+++ core/frontend/src/components/common/PasswordInput.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-text-field
+    :value="value"
+    :append-icon="password_show_icon"
+    :type="input_field_type"
+    label="Password"
+    hide-details="auto"
+    class="pa-2"
+    @input="emitInput"
+    @keyup.enter="emitSubmit"
+    @click:append="togglePasswordShow"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'PasswordInput',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      show_password: false,
+    }
+  },
+  computed: {
+    password_show_icon(): string {
+      return this.show_password ? 'mdi-eye' : 'mdi-eye-off'
+    },
+    input_field_type(): string {
+      return this.show_password ? 'text' : 'password'
+    },
+  },
+  methods: {
+    togglePasswordShow() {
+      this.show_password = !this.show_password
+    },
+    emitInput(value: string) {
+      this.$emit('input', value)
+    },
+    emitSubmit() {
+      this.$emit('submit')
+    },
+  },
+})
+</script>
+
+<style>
+</style>
