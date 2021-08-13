a748eeb10d7111ec769847017cac1261cd8971bb
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-13 11:20:26 -0300

    core: frontend: Add SpinningLogo component
    Blue Robotics spinning logo, to be used for loading states.

---
+++ core/frontend/src/components/common/SpinningLogo.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-img
+    class="lds-dual-ring"
+    src="../../assets/logo.svg"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'SpinningLogo',
+})
+</script>
+
+<style>
+  .lds-dual-ring {
+    display: block;
+    width: 30%;
+    height: 30%;
+    margin: 5% auto;
+    animation-name: rotation360;
+    animation-duration: 2s;
+    animation-iteration-count: infinite;
+  }
+  @keyframes rotation360 {
+    0% {
+        transform: rotate(0deg);
+    }
+    100% {
+        transform: rotate(360deg);
+    }
+  }
+</style>
