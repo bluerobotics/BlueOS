68fe06509e5132f7771f4af352db75449da0a60c
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-19 13:48:10 -0300

    core: ardupilot-manager: Make default GCS endpoint an UDP server

--- core/services/ardupilot_manager/ArduPilotManager.py
+++ core/services/ardupilot_manager/ArduPilotManager.py
@@ -145,7 +145,12 @@ class ArduPilotManager(metaclass=Singleton):
             self.add_new_endpoints(
                 {
                     Endpoint(
-                        "GCS Link", self.settings.app_name, EndpointType.UDPClient, "192.168.2.1", 14550, protected=True
+                        "GCS Link",
+                        self.settings.app_name,
+                        EndpointType.UDPServer,
+                        "0.0.0.0",
+                        14550,
+                        protected=True,
                     )
                 }
             )
