3b3cae269613844c25dac859a614c4466611e2df
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-25 11:40:17 -0300

    core: frontend: Declare ethernet-manager service

--- core/frontend/src/types/frontend_services.ts
+++ core/frontend/src/types/frontend_services.ts
@@ -1,7 +1,5 @@
 import { Service } from './common'
 
-// There will be more services here, so a default export makes no sense.
-// eslint-disable-next-line import/prefer-default-export
 export const service_scanner_service: Service = {
   name: 'Service Scanner',
   description: 'Actual scanner for running services.',
@@ -12,6 +10,13 @@ export const service_scanner_service: Service = {
 export const wifi_service: Service = {
   name: 'Wifi Manager',
   description: 'Service responsible for managing wifi configuration on Companion.',
+  company: 'Blue Robotics',
+  version: '0.1.0',
+}
+
+export const ethernet_service: Service = {
+  name: 'Ethernet Manager',
+  description: 'Responsible for management of ethernet adapters configuration.',
   company: 'Blue Robotics',
   version: '0.1.0',
 }
