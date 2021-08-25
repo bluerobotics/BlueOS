a6d8d2389b61d7ee796f8d336ce86f0283a928d7
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-25 10:46:19 -0300

    core: ardupilot-manager: Refactor FirmwareDownload to use Exceptions
    Raise exceptions with proper messages instead of returning a silent None. This passes more information toward when errors happen and removes the necessity of returning Optionals.

--- core/services/ardupilot_manager/exceptions.py
+++ core/services/ardupilot_manager/exceptions.py
@@ -1,3 +1,23 @@
 """
 Ardupilot-manager exception classes.
 """
+
+
+class FirmwareDownloadFail(RuntimeError):
+    """Firmware download operation failed."""
+
+
+class NoVersionAvailable(ValueError):
+    """No firmware versions available for specified configuration."""
+
+
+class InvalidManifest(ValueError):
+    """Ardupilot manifest file cannot be validated."""
+
+
+class ManifestUnavailable(RuntimeError):
+    """Ardupilot manifest file unavailable."""
+
+
+class MoreThanOneCandidate(ValueError):
+    """More than one firmware candidate found for specified configuration."""
