c21176cca9229be2e18008ca332f1e747ffdd23e
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-08-25 11:40:16 -0300

    core: frontend: Add ethernet store

---
+++ core/frontend/src/store/ethernet.ts
@@ -0,0 +1,29 @@
+import { Module, Mutation, VuexModule } from 'vuex-module-decorators'
+
+import store from '@/store'
+import { EthernetInterface } from '@/types/ethernet'
+
+@Module({
+  dynamic: true,
+  store,
+  name: 'ethernet_store',
+})
+
+export default class EthernetStore extends VuexModule {
+  API_URL = '/cable-guy/v1.0'
+
+  available_interfaces: EthernetInterface[] = []
+
+  updating_interfaces = true
+
+  @Mutation
+  setUpdatingInterfaces(updating: boolean): void {
+    this.updating_interfaces = updating
+  }
+
+  @Mutation
+  setInterfaces(ethernet_interfaces: EthernetInterface[]): void {
+    this.available_interfaces = ethernet_interfaces
+    this.updating_interfaces = false
+  }
+}
