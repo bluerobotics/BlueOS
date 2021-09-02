141d7a0d5c41ad952600bc31c3118254a354fecf
Willian Galvani <williangalvani@gmail.com>
2021-09-02 10:13:29 -0300

    core: tools: tlogviewer: bump version to v0.9.2
    This fixed the issue https://github.com/ArduPilot/UAVLogViewer/issues/302
    and a minor one with asset paths in dev/build modes

--- core/tools/logviewer/bootstrap.sh
+++ core/tools/logviewer/bootstrap.sh
@@ -2,7 +2,7 @@
 
 DESTINATION_PATH="/home/pi/tools/logviewer"
 
-VERSION=v0.9.1
+VERSION=v0.9.2
 
 REMOTE_URL="https://github.com/Ardupilot/UAVLogViewer/releases/download/${VERSION}/logviewer.tar.gz"
 
