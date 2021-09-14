ea8627bee29255b868dc951b9d5c3964efcc615d
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-14 14:04:07 -0300

    core: frontend: Add autopilot endpoints service page

---
+++ core/frontend/src/views/EndpointView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <endpoint-manager />
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import EndpointManager from '@/components/autopilot/EndpointManager.vue'
+
+export default Vue.extend({
+  name: 'EndpointView',
+  components: {
+    EndpointManager,
+  },
+})
+</script>
