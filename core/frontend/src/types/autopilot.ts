63db348370510789983a7dce2b87b2dce05be32c
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-14 14:04:07 -0300

    core: frontend: Add autopilot endpoint-related types

--- core/frontend/src/types/autopilot.ts
+++ core/frontend/src/types/autopilot.ts
@@ -9,3 +9,31 @@ export enum Vehicle {
     Plane = 'Plane',
     Copter = 'Copter',
 }
+
+export enum EndpointType {
+    udpin = 'udpin',
+    udpout = 'udpout',
+    tcpin = 'tcpin',
+    tcpout = 'tcpout',
+    serial = 'serial',
+}
+
+export function userFriendlyEndpointType(type: EndpointType): string {
+  switch (type) {
+    case EndpointType.udpin: return 'UDP Server'
+    case EndpointType.udpout: return 'UDP Client'
+    case EndpointType.tcpin: return 'TCP Server'
+    case EndpointType.tcpout: return 'TCP Client'
+    case EndpointType.serial: return 'Serial'
+    default: return 'Undefined type'
+  }
+}
+export interface AutopilotEndpoint {
+    name: string
+    owner: string
+    connection_type: EndpointType
+    place: string
+    argument: number
+    persistent: boolean
+    protected: boolean
+}
