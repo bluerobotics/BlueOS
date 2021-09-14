d6ef0a4a22cd842fd7382f77a402b58277f7cb5d
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-14 14:04:07 -0300

    core: frontend: Extend autopilot store with endpoints logic

--- core/frontend/src/store/autopilot_manager.ts
+++ core/frontend/src/store/autopilot_manager.ts
@@ -1,6 +1,7 @@
-import { Module, VuexModule } from 'vuex-module-decorators'
+import { Module, Mutation, VuexModule } from 'vuex-module-decorators'
 
 import store from '@/store'
+import { AutopilotEndpoint } from '@/types/autopilot'
 
 @Module({
   dynamic: true,
@@ -10,4 +11,19 @@ import store from '@/store'
 
 export default class AutopilotManagerStore extends VuexModule {
   API_URL = '/ardupilot-manager/v1.0'
+
+  available_endpoints: AutopilotEndpoint[] = []
+
+  updating_endpoints = true
+
+  @Mutation
+  setUpdatingEndpoints(updating: boolean): void {
+    this.updating_endpoints = updating
+  }
+
+  @Mutation
+  setAvailableEndpoints(available_endpoints: AutopilotEndpoint[]): void {
+    this.available_endpoints = available_endpoints
+    this.updating_endpoints = false
+  }
 }
