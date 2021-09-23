8a0639eb91d5034ca288936cd93225b2f04dff5c
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-23 14:49:55 -0300

    core: frontend: Periodically fetch current autopilot platform

--- core/frontend/src/components/autopilot/AutopilotManagerUpdater.vue
+++ core/frontend/src/components/autopilot/AutopilotManagerUpdater.vue
@@ -9,6 +9,7 @@ import { getModule } from 'vuex-module-decorators'
 
 import AutopilotManagerStore from '@/store/autopilot_manager'
 import NotificationStore from '@/store/notifications'
+import { Platform } from '@/types/autopilot'
 import { autopilot_manager_service } from '@/types/frontend_services'
 import { LiveNotification, NotificationLevel } from '@/types/notifications'
 import { callPeriodically } from '@/utils/helper_functions'
@@ -27,6 +28,7 @@ export default Vue.extend({
   name: 'AutopilotManagerUpdater',
   async mounted() {
     await callPeriodically(this.fetchAvailableEndpoints, 5000)
+    await callPeriodically(this.fetchCurrentPlatform, 5000)
   },
   methods: {
     async fetchAvailableEndpoints(): Promise<void> {
@@ -47,6 +49,25 @@ export default Vue.extend({
             `Could not fetch available MAVLink endpoints: ${error.message}`,
           ))
           autopilot_manager_store.setAvailableEndpoints([])
+        })
+    },
+    async fetchCurrentPlatform(): Promise<void> {
+      await axios({
+        method: 'get',
+        url: `${autopilot_manager_store.API_URL}/platform`,
+        timeout: 10000,
+      })
+        .then((response) => {
+          autopilot_manager_store.setCurrentPlatform(response.data)
+        })
+        .catch((error) => {
+          notification_store.pushNotification(new LiveNotification(
+            NotificationLevel.Error,
+            autopilot_manager_service,
+            'AUTOPILOT_PLATFORM_FETCH_FAIL',
+            `Could not fetch current Autopilot platform: ${error.message}`,
+          ))
+          autopilot_manager_store.setCurrentPlatform(Platform.Undefined)
         })
     },
   },
