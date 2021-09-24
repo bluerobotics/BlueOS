aee7c6b065efd3168a6a15789e0bb410177a09dd
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-24 09:34:50 -0300

    core: frontend: Add video-manager view

---
+++ core/frontend/src/views/VideoManagerView.vue
@@ -0,0 +1,16 @@
+<template>
+  <video-manager />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import VideoManager from '@/components/video-manager/VideoManager.vue'
+
+export default Vue.extend({
+  name: 'VideoManagerView',
+  components: {
+    VideoManager,
+  },
+})
+</script>
