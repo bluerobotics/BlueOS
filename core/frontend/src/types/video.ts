8070f9380f8279203c54399a8844ac783bfa7f20
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-24 09:34:50 -0300

    core: frontend: Add video-manager types

---
+++ core/frontend/src/types/video.ts
@@ -0,0 +1,150 @@
+export interface FrameInterval {
+  denominator: number
+  numerator: number
+}
+
+export interface Size {
+  height: number
+  width: number
+  intervals: FrameInterval[]
+}
+
+export enum VideoEncodeType {
+  H265 = 'H265',
+  H264 = 'H264',
+  MJPG = 'MJPG',
+  YUYV = 'YUYV',
+}
+
+export interface Format {
+  encode: VideoEncodeType
+  sizes: Size[]
+}
+
+export interface ControlOption {
+  name: string
+  value: number
+}
+
+export interface MenuConfigType {
+  default: number
+  value: number
+  options: ControlOption
+}
+
+export interface MenuConfig {
+  Menu: MenuConfigType
+}
+
+export interface BoolConfigType {
+  default: number
+  value: number
+}
+
+export interface BoolConfig {
+  Bool: BoolConfigType
+}
+
+export interface SliderConfigType {
+  default: number
+  max: number
+  min: number
+  step: number
+  value: number
+}
+
+export interface SliderConfig {
+  Slider: SliderConfigType
+}
+
+export interface Control {
+  configuration: MenuConfig | BoolConfig | SliderConfig
+  cpp_type: string
+  id: number
+  name: string
+}
+
+export interface Menu extends Control {
+  configuration: MenuConfig
+}
+
+export interface Bool extends Control {
+  configuration: BoolConfig
+}
+
+export interface Slider extends Control {
+  configuration: SliderConfig
+}
+
+export interface Device {
+  name: string
+  source: string
+  formats: Format[]
+  controls: Control[]
+}
+
+export interface CaptureConfiguration {
+  encode: string
+  height: number
+  width: number
+  frame_interval: FrameInterval
+}
+
+export interface StreamInformation {
+  endpoints: string[]
+  configuration: CaptureConfiguration
+}
+
+export interface CreatedStream {
+  name: string
+  source: string
+  stream_information: StreamInformation
+}
+
+export interface UsbBus {
+  interface: string
+  usb_hub: number
+  usb_port: number
+}
+
+export interface UsbBusType {
+  Usb: UsbBus
+}
+
+export interface IspType {
+  Isp: string
+}
+
+export interface VideoSourceLocalType {
+  name: string
+  device_path: string
+  type: UsbBusType | IspType
+}
+
+export interface VideoSourceLocal {
+  Local: VideoSourceLocalType
+}
+
+export interface VideoSourceFake {
+  Fake: string
+}
+
+export interface VideoSourceGstType {
+  name: string
+  source: VideoSourceFake
+}
+
+export interface VideoSourceGst {
+  Gst: VideoSourceGstType
+}
+
+export interface VideoAndStreamInformation {
+  name: string
+  stream_information: StreamInformation
+  video_source: VideoSourceLocal | VideoSourceGst
+}
+
+export interface StreamStatus {
+  running: boolean
+  video_and_stream: VideoAndStreamInformation
+}
