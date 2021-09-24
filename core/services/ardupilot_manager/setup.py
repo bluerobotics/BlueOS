283f5a7524a62750455f461896b6a7686bdafada
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-24 15:59:51 -0300

    core: ardupilot-manager: Make static files download more flexible

--- core/services/ardupilot_manager/setup.py
+++ core/services/ardupilot_manager/setup.py
@@ -4,6 +4,7 @@ import pathlib
 import ssl
 import sys
 import urllib.request
+from collections import namedtuple
 from warnings import warn
 
 import setuptools
@@ -11,28 +12,35 @@ import setuptools
 ssl._create_default_https_context = ssl._create_unverified_context
 
 
-static_files = {
-    "js/axios.min.js": "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js",
-    "js/polyfill.min.js": "https://polyfill.io/v3/polyfill.min.js?features=es2015,IntersectionObserver",
-    "js/vue.js": "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.js",
-    "js/metro.js": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/js/metro.min.js",
-    "css/metro-all.css": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/css/metro-all.css",
-    "mif/metro.woff": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.woff",
-    "mif/metro.ttf": "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.ttf",
-}
-
 current_folder = pathlib.Path(__file__).parent.absolute()
 frontend_folder = pathlib.Path.joinpath(current_folder, "frontend")
 static_folder = pathlib.Path.joinpath(frontend_folder, "static")
-static_folder.mkdir(exist_ok=True)
 
-for filename, url in static_files.items():
-    path = pathlib.Path.joinpath(static_folder, filename)
-    path.parent.mkdir(exist_ok=True)
+StaticFile = namedtuple("StaticFile", "parent filename url")
+
+static_files = [
+    StaticFile(static_folder, "js/axios.min.js", "https://cdnjs.cloudflare.com/ajax/libs/axios/0.19.2/axios.min.js"),
+    StaticFile(
+        static_folder,
+        "js/polyfill.min.js",
+        "https://polyfill.io/v3/polyfill.min.js?features=es2015,IntersectionObserver",
+    ),
+    StaticFile(static_folder, "js/vue.js", "https://cdnjs.cloudflare.com/ajax/libs/vue/2.6.12/vue.js"),
+    StaticFile(static_folder, "js/metro.js", "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/js/metro.min.js"),
+    StaticFile(
+        static_folder, "css/metro-all.css", "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/css/metro-all.css"
+    ),
+    StaticFile(static_folder, "mif/metro.woff", "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.woff"),
+    StaticFile(static_folder, "mif/metro.ttf", "https://cdnjs.cloudflare.com/ajax/libs/metro/4.4.3/mif/metro.ttf"),
+]
+
+for file in static_files:
+    path = pathlib.Path.joinpath(file.parent, file.filename)
+    path.parent.mkdir(parents=True, exist_ok=True)
     try:
-        urllib.request.urlretrieve(url, path)
+        urllib.request.urlretrieve(file.url, path)
     except Exception as error:
-        warn(f"unable to open url {url}, error {error}")
+        warn(f"unable to open url {file.url}, error {error}")
         sys.exit(1)
 
 
