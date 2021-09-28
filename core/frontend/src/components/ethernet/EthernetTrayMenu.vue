8ed5964f20af8a7be50f13288afa3acf7cb936f0
Patrick José Pereira <patrickelectric@gmail.com>
2021-09-28 19:26:28 -0300

    core: frontend: EthernetTrayMenu: Update icon based on interface connection state
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/ethernet/EthernetTrayMenu.vue
+++ core/frontend/src/components/ethernet/EthernetTrayMenu.vue
@@ -17,7 +17,7 @@
           class="px-1"
           color="white"
         >
-          mdi-connection
+          {{ interface_connected_icon }}
         </v-icon>
       </v-card>
     </template>
@@ -27,13 +27,25 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import { getModule } from 'vuex-module-decorators'
+
+import EthernetStore from '@/store/ethernet'
 
 import EthernetManager from './EthernetManager.vue'
+
+const ethernet_store: EthernetStore = getModule(EthernetStore)
 
 export default Vue.extend({
   name: 'EthernetTrayMenu',
   components: {
     EthernetManager,
+  },
+  computed: {
+    interface_connected_icon(): string {
+      const connected_interfaces = ethernet_store.available_interfaces
+        .filter((ethernet_interface) => ethernet_interface.info && ethernet_interface.info.connected)
+      return connected_interfaces.length !== 0 ? 'mdi-lan-connect' : 'mdi-lan-disconnect'
+    },
   },
 })
 </script>
