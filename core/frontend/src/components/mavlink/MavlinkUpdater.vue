2cb0b56c218b1146b940668508de2286853d9591
Patrick José Pereira <patrickelectric@gmail.com>
2021-09-29 09:17:45 -0300

    core: frontend: Add MavlinkUpdater
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/components/mavlink/MavlinkUpdater.vue
@@ -0,0 +1,20 @@
+<template>
+  <span />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import { getModule } from 'vuex-module-decorators'
+
+import MavlinkStore from '@/store/mavlink'
+
+const mavlink_store: MavlinkStore = getModule(MavlinkStore)
+
+export default Vue.extend({
+  name: 'MavlinkUpdater',
+  mounted() {
+    mavlink_store.setMessageRefreshRate({ messageName: 'HEARTBEAT', refreshRate: 1 })
+    mavlink_store.setMessageRefreshRate({ messageName: 'SYS_STATUS', refreshRate: 3 })
+  },
+})
+</script>
