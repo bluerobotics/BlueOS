c0cdf06cefdb1767a4f234d20b7461ec4d135273
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-09-29 19:14:49 -0300

    core: frontend: Add Bridget components

---
+++ core/frontend/src/components/bridges/BridgeCard.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-card
+    width="100%"
+    class="available-bridge pa-0 my-4"
+  >
+    <v-card-text class="pa-4">
+      <v-container class="pa-0">
+        <v-card class="elevation-0 pa-0">
+          <v-card-text
+            class="d-flex align-center pa-0 justify-center"
+          >
+            <span class="text-subtitle-1 mr-2">
+              {{ bridge.serial_path }}:{{ bridge.baud }}
+            </span>
+            <v-icon>mdi-link</v-icon>
+            <span class="text-subtitle-1 ml-2">
+              {{ bridge.ip }}:{{ bridge.udp_port }}
+            </span>
+          </v-card-text>
+        </v-card>
+      </v-container>
+      <v-fab-transition>
+        <v-btn
+          color="pink"
+          fab
+          dark
+          small
+          absolute
+          bottom
+          right
+          @click="removeBridge"
+        >
+          <v-icon>mdi-minus</v-icon>
+        </v-btn>
+      </v-fab-transition>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import Vue, { PropType } from 'vue'
+import { getModule } from 'vuex-module-decorators'
+
+import BridgetStore from '@/store/bridget'
+import NotificationStore from '@/store/notifications'
+import { Bridge } from '@/types/bridges'
+import { bridget_service } from '@/types/frontend_services'
+import { LiveNotification, NotificationLevel } from '@/types/notifications'
+
+const notification_store: NotificationStore = getModule(NotificationStore)
+const bridget_store: BridgetStore = getModule(BridgetStore)
+
+export default Vue.extend({
+  name: 'BridgeCard',
+  props: {
+    bridge: {
+      type: Object as PropType<Bridge>,
+      required: true,
+    },
+  },
+  methods: {
+    async removeBridge(): Promise<void> {
+      bridget_store.setUpdatingBridges(true)
+      await axios({
+        method: 'delete',
+        url: `${bridget_store.API_URL}/bridges`,
+        timeout: 10000,
+        data: this.bridge,
+      })
+        .catch((error) => {
+          notification_store.pushNotification(new LiveNotification(
+            NotificationLevel.Error,
+            bridget_service,
+            'BRIDGE_DELETE_FAIL',
+            `Could not remove bridge: ${error.message}.`,
+          ))
+        })
+    },
+  },
+})
+</script>
