d57294a96e8321d72d954721538595fc787cf111
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-10-13 15:01:47 -0300

    core: frontend: Create store for the NMEA injector service

---
+++ core/frontend/src/store/nmea-injector.ts
@@ -0,0 +1,29 @@
+import { Module, Mutation, VuexModule } from 'vuex-module-decorators'
+
+import store from '@/store'
+import { NMEASocket } from '@/types/nmea-injector'
+
+@Module({
+  dynamic: true,
+  store,
+  name: 'nmea_injector_store',
+})
+
+export default class NMEAInjectorStore extends VuexModule {
+  API_URL = '/nmea-injector/v1.0'
+
+  available_nmea_sockets: NMEASocket[] = []
+
+  updating_nmea_sockets = true
+
+  @Mutation
+  setUpdatingNMEASockets(updating: boolean): void {
+    this.updating_nmea_sockets = updating
+  }
+
+  @Mutation
+  setAvailableNMEASockets(available_nmea_sockets: NMEASocket[]): void {
+    this.available_nmea_sockets = available_nmea_sockets
+    this.updating_nmea_sockets = false
+  }
+}
