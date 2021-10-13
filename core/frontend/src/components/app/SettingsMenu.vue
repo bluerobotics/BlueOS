18a84233d46ed1e54985eb2990e0bc2fde5ab071
Patrick José Pereira <patrickelectric@gmail.com>
2021-10-13 13:35:19 -0300

    core: frontend: Add SettingsMenu
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/components/app/SettingsMenu.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-container
+    class="d-flex justify-center"
+  >
+    <v-btn
+      class="mr-2"
+      icon
+      large
+      elevation="2"
+      @click="showDialog(true)"
+    >
+      <v-icon>mdi-cog</v-icon>
+    </v-btn>
+    <v-dialog
+      width="320"
+      :value="show_dialog"
+      @input="showDialog"
+    >
+      <v-card>
+        <v-card-title class="align-center">
+          Settings
+        </v-card-title>
+
+        <v-divider />
+
+        <v-container class="pa-8">
+          <v-row
+            justify="center"
+          >
+            <v-btn
+              @click="reset_settings"
+            >
+              <v-icon left>
+                mdi-trash-can
+              </v-icon>
+              Reset Settings
+            </v-btn>
+          </v-row>
+        </v-container>
+      </v-card>
+    </v-dialog>
+    <v-dialog
+      width="380"
+      :value="show_reset_dialog"
+      @input="show_reset_dialog = false"
+    >
+      <v-card>
+        <v-container class="pa-8">
+          Restart the system to finish the settings reset.
+        </v-container>
+      </v-card>
+    </v-dialog>
+  </v-container>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import Vue from 'vue'
+import { getModule } from 'vuex-module-decorators'
+
+import NotificationStore from '@/store/notifications'
+import { commander_service } from '@/types/frontend_services'
+import { LiveNotification, NotificationLevel } from '@/types/notifications'
+
+const notification_store: NotificationStore = getModule(NotificationStore)
+
+const API_URL = '/commander/v1.0'
+
+export default Vue.extend({
+  name: 'PowerMenu',
+  components: {
+  },
+  data() {
+    return {
+      show_dialog: false,
+      show_reset_dialog: false,
+    }
+  },
+  computed: {
+  },
+  methods: {
+    async reset_settings(): Promise<void> {
+      await axios({
+        url: `${API_URL}/settings/reset`,
+        method: 'post',
+        params: {
+          i_know_what_i_am_doing: true,
+        },
+        timeout: 2000,
+      }).then(() => {
+        this.show_reset_dialog = true
+      }).catch((error) => {
+        const detail_message = 'data' in error.response
+            && 'message' in error.response.data ? error.response.data.message : 'No details available.'
+        notification_store.pushNotification(new LiveNotification(
+          NotificationLevel.Error,
+          commander_service,
+          'RESET_SETTINGS_FAIL',
+          `Failed to commit operation: ${error.message}, ${detail_message}`
+          ,
+        ))
+      })
+    },
+    showDialog(state: boolean): void {
+      this.show_dialog = state
+    },
+  },
+})
+</script>
