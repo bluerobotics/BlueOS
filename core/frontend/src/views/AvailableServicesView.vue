15998b11035ea3e5d2c886964cc29dad9fda4e2c
Patrick José Pereira <patrickelectric@gmail.com>
2021-10-15 08:53:45 -0300

    core: frontend: Add AvailableServicesView
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/views/AvailableServicesView.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <available-services-table />
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import AvailableServicesTable from '@/components/scanner/availableServicesTable.vue'
+
+export default Vue.extend({
+  name: 'AvailableServicesView',
+  components: {
+    AvailableServicesTable,
+  },
+})
+</script>
