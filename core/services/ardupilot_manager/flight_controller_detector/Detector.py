ddb364d4bcdff98749ec3edb76a8a6d286c9bdce
Patrick José Pereira <patrickelectric@gmail.com>
2021-10-20 17:34:04 -0300

    core: services: ardupilot_manager: Detector: Split Navigator in NavigatorR3 and NavigatorR4
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/ardupilot_manager/flight_controller_detector/Detector.py
+++ core/services/ardupilot_manager/flight_controller_detector/Detector.py
@@ -8,7 +8,8 @@ from smbus2 import SMBus
 
 class FlightControllerType(IntEnum):
     Serial = 1
-    Navigator = 2
+    NavigatorR3 = 2
+    NavigatorR4 = 3
 
 
 class Detector:
@@ -22,8 +23,8 @@ class Detector:
         return os.geteuid() == 0
 
     @staticmethod
-    def detect_navigator() -> Tuple[bool, str]:
-        """Check if navigator is connected using the sensors on the I²C BUS
+    def detect_navigator_r3() -> Tuple[bool, str]:
+        """Check if navigator R3 is connected using the sensors on the I²C BUS
 
         Returns:
             (bool, str): True if a navigator is connected, false otherwise.
@@ -40,7 +41,36 @@ class Detector:
 
             return (True, "")
         except Exception as error:
-            logger.info("Navigator not detected on I2C bus.")
+            logger.info("Navigator R3 not detected on I2C bus.")
+            logger.debug(error)
+            return (False, "")
+
+    @staticmethod
+    def detect_navigator_r4() -> Tuple[bool, str]:
+        """Check if navigator R4 is connected using the sensors on the I²C BUS
+
+        Returns:
+            (bool, str): True if a navigator is connected, false otherwise.
+                String is always empty
+        """
+        try:
+            bus = SMBus(1)
+            ADS1115_address = 0x48
+            bus.read_byte_data(ADS1115_address, 0)
+
+            AK09915_address = 0x0C
+            bus.read_byte_data(AK09915_address, 0)
+
+            BME280_address = 0x76
+            bus.read_byte_data(BME280_address, 0)
+
+            bus = SMBus(4)
+            PCA9685_address = 0x40
+            bus.read_byte_data(PCA9685_address, 0)
+
+            return (True, "")
+        except Exception as error:
+            logger.info("Navigator R4 not detected on I2C bus.")
             logger.debug(error)
             return (False, "")
 
@@ -65,9 +95,14 @@ class Detector:
         """
         available = []
         if Detector._is_root():
-            result, path = Detector.detect_navigator()
+            # We should detect R4 first since it shares some sensors as R3
+            result, path = Detector.detect_navigator_r4()
             if result:
-                available.append((FlightControllerType.Navigator, path))
+                available.append((FlightControllerType.NavigatorR4, path))
+            else:
+                result, path = Detector.detect_navigator_r3()
+                if result:
+                    available.append((FlightControllerType.NavigatorR3, path))
 
         result, path = Detector.detect_serial_flight_controller()
         if result:
