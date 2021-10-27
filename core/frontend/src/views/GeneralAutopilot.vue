294a45a85c63b862af55542e7bdee4bf9200fa57
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-10-27 13:19:34 -0300

    core: frontend: Add platform toggle button

--- core/frontend/src/views/GeneralAutopilot.vue
+++ core/frontend/src/views/GeneralAutopilot.vue
@@ -7,6 +7,13 @@
     <v-card-actions>
       <v-spacer />
       <v-btn
+        color="blue lighten-3"
+        :disabled="restarting"
+        @click="togglePlatform"
+      >
+        {{ toggle_button_text }}
+      </v-btn>
+      <v-btn
         color="red lighten-3"
         :disabled="restarting"
         @click="restart_autopilot"
@@ -32,6 +39,15 @@ export default Vue.extend({
     current_platform(): Platform {
       return autopilot.current_platform
     },
+    running_sitl(): boolean {
+      return [Platform.SITL_X86, Platform.SITL_ARM].includes(this.current_platform)
+    },
+    platform_undefined(): boolean {
+      return this.current_platform === Platform.Undefined
+    },
+    toggle_button_text(): string {
+      return this.platform_undefined || !this.running_sitl ? 'Use SITL' : 'Use board'
+    },
     restarting(): boolean {
       return autopilot.restarting
     },
@@ -42,11 +58,28 @@ export default Vue.extend({
       await back_axios({
         method: 'post',
         url: `${autopilot.API_URL}/restart`,
+        params: { use_sitl: !this.running_sitl },
         timeout: 10000,
       })
         .catch((error) => {
           const message = `Could not restart autopilot: ${error.message}`
           notifications.pushError({ service: autopilot_service, type: 'AUTOPILOT_RESTART_FAIL', message })
+        })
+        .finally(() => {
+          autopilot.setRestarting(false)
+        })
+    },
+    async togglePlatform(): Promise<void> {
+      autopilot.setRestarting(true)
+      await back_axios({
+        method: 'post',
+        url: `${autopilot.API_URL}/platform`,
+        params: { use_sitl: !this.running_sitl },
+        timeout: 10000,
+      })
+        .catch((error) => {
+          const { message } = error
+          notifications.pushError({ service: autopilot_service, type: 'AUTOPILOT_PLATFORM_TOGGLE_FAIL', message })
         })
         .finally(() => {
           autopilot.setRestarting(false)
