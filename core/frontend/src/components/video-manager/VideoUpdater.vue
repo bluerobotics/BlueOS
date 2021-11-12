5b0c4a1fab6e59af6898a8481b9cbb96550daf2e
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-11-12 14:42:44 -0300

    core: frontend: Move video fetch methods to store

--- core/frontend/src/components/video-manager/VideoUpdater.vue
+++ core/frontend/src/components/video-manager/VideoUpdater.vue
@@ -14,42 +14,8 @@ import { callPeriodically } from '@/utils/helper_functions'
 export default Vue.extend({
   name: 'VideoUpdater',
   async mounted() {
-    await callPeriodically(this.fetchDevices, 5000)
-    await callPeriodically(this.fetchStreams, 5000)
-  },
-  methods: {
-    async fetchDevices(): Promise<void> {
-      await back_axios({
-        method: 'get',
-        url: `${video.API_URL}/v4l`,
-        timeout: 10000,
-      })
-        .then((response) => {
-          video.setAvailableDevices(response.data)
-        })
-        .catch((error) => {
-          video.setAvailableDevices([])
-          if (error === backend_offline_error) { return }
-          const message = `Could not fetch video devices: ${error.message}`
-          notifications.pushError({ service: video_manager_service, type: 'VIDEO_DEVICES_FETCH_FAIL', message })
-        })
-    },
-    async fetchStreams(): Promise<void> {
-      await back_axios({
-        method: 'get',
-        url: `${video.API_URL}/streams`,
-        timeout: 10000,
-      })
-        .then((response) => {
-          video.setAvailableStreams(response.data)
-        })
-        .catch((error) => {
-          video.setAvailableStreams([])
-          if (error === backend_offline_error) { return }
-          const message = `Could not fetch video streams: ${error.message}`
-          notifications.pushError({ service: video_manager_service, type: 'VIDEO_STREAMS_FETCH_FAIL', message })
-        })
-    },
+    await callPeriodically(video.fetchDevices, 5000)
+    await callPeriodically(video.fetchStreams, 5000)
   },
 })
 </script>
