312184538f5943aead3cd99dfe2295fd4dd03f0c
Patrick José Pereira <patrickelectric@gmail.com>
2021-11-16 15:04:20 -0300

    core: frontend: views: Use new iframe component
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/views/FileBrowserView.vue
+++ core/frontend/src/views/FileBrowserView.vue
@@ -1,17 +1,19 @@
 <template>
-  <iframe
-    loading="lazy"
-    :src="service_path"
-    height="100%"
-    width="100%"
+  <Iframe
+    :source="service_path"
   />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import Iframe from '@/components/utils/iframe.vue'
+
 export default Vue.extend({
   name: 'FileBrowserView',
+  components: {
+    Iframe,
+  },
   data() {
     return {
       service_path: '/file-browser',
