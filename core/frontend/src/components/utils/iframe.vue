81bbdf9ff51fb18038fdc326fbdf61facdadc4c0
Patrick José Pereira <patrickelectric@gmail.com>
2021-11-16 15:03:57 -0300

    core: frontend: utils: Add iframe component
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/components/utils/iframe.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-sheet
+    width="100%"
+    height="100%"
+  >
+    <spinning-logo
+      v-if="!iframe_loaded"
+      size="15%"
+    />
+    <iframe
+      v-show="iframe_loaded"
+      loading="lazy"
+      :src="source"
+      height="100%"
+      width="100%"
+      @load="loadFinished"
+    />
+  </v-sheet>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import SpinningLogo from '../common/SpinningLogo.vue'
+
+export default Vue.extend({
+  name: 'Iframe',
+  components: {
+    SpinningLogo,
+  },
+  props: {
+    source: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      iframe_loaded: false,
+    }
+  },
+  methods: {
+    loadFinished(): void {
+      this.iframe_loaded = true
+    },
+  },
+})
+</script>
