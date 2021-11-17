dd8270fb78ca39b5bd877232c9a1712a4d3c79fc
Patrick José Pereira <patrickelectric@gmail.com>
2021-11-17 16:59:58 -0300

    core: services: ping: Add libraries for REST API
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/ping/setup.py
+++ core/services/ping/setup.py
@@ -17,7 +17,11 @@ setup(
     install_requires=[
         "bluerobotics-ping == 0.1.2",
         "bridges == 0.1.0",
+        "fastapi == 0.63.0",
+        "fastapi-versioning == 0.9.1",
         "loguru == 0.5.3",
         "pyserial == 3.5",
+        "starlette == 0.13.6",
+        "uvicorn == 0.13.4",
     ],
 )
