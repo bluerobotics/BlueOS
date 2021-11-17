be69e79246ef8f83b55ba83341030ff0016ad3a4
Patrick José Pereira <patrickelectric@gmail.com>
2021-11-17 16:59:02 -0300

    core: services: ping: Add REST API
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/ping/main.py
+++ core/services/ping/main.py
@@ -2,16 +2,58 @@
 import argparse
 import asyncio
 import logging
+from typing import Any, List
+
+from commonwealth.utils.apis import PrettyJSONResponse
+from commonwealth.utils.logs import InterceptHandler
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi_versioning import VersionedFastAPI, version
+from loguru import logger
+from uvicorn import Config, Server
 
 from pingmanager import PingManager
 from pingprober import PingProber
 from portwatcher import PortWatcher
+from typedefs import PingDeviceDescriptorModel
+
+app = FastAPI(
+    title="Ping Manager API",
+    description="Ping Manager is responsible for managing Ping devices connected to Companion.",
+    default_response_class=PrettyJSONResponse,
+    debug=True,
+)
+logger.info("Starting Ping Service.")
+
+# TODO: move to singleton
+ping_manager = PingManager()
 
 
-async def main() -> None:
-    logging.info("Starting Ping Service")
+@app.get("/sensors", response_model=List[PingDeviceDescriptorModel], summary="Current sensors detected.")
+@version(1, 0)
+def get_sensors() -> Any:
+    devices = ping_manager.devices()
+    logger.debug(f"Sensors available: {devices}")
+    return [PingDeviceDescriptorModel.from_descriptor(device) for device in ping_manager.devices()]
+
+
+app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
+
+
+@app.get("/")
+async def root() -> Any:
+    html_content = """
+    <html>
+        <head>
+            <title>Ping Service</title>
+        </head>
+    </html>
+    """
+    return HTMLResponse(content=html_content, status_code=200)
+
+
+async def sensor_manager() -> None:
     ping_prober = PingProber()
-    ping_manager = PingManager()
     port_watcher = PortWatcher(probe_callback=ping_prober.probe)
     port_watcher.set_port_post_callback(ping_manager.stop_driver_at_port)
 
@@ -22,12 +64,15 @@ async def main() -> None:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Ping Service for Bluerobotics Companion")
-    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
-    parser.add_argument("--logtofile", help="log output to a file instead")
-
     args = parser.parse_args()
 
-    level = logging.DEBUG if args.verbose else logging.INFO
+    logging.basicConfig(handlers=[InterceptHandler()], level=0)
 
-    logging.basicConfig(filename=args.logtofile, level=level)
-    asyncio.run(main())
+    loop = asyncio.new_event_loop()
+
+    # Running uvicorn with log disabled so loguru can handle it
+    config = Config(app=app, loop=loop, host="0.0.0.0", port=9110, log_config=None)
+    server = Server(config)
+
+    loop.create_task(sensor_manager())
+    loop.run_until_complete(server.serve())
