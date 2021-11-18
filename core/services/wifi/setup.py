8e73f0e444500df333ab37eba532561700d0db66
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2021-11-18 15:53:56 -0300

    core: wifi: Add detailed logging to main functions

--- core/services/wifi/setup.py
+++ core/services/wifi/setup.py
@@ -48,5 +48,7 @@ setuptools.setup(
         "aiofiles == 0.6.0",
         "fastapi-versioning == 0.9.1",
         "commonwealth == 0.1.0",
+        "tabulate == 0.8.9",
+        "types-tabulate == 0.8.3",
     ],
 )
