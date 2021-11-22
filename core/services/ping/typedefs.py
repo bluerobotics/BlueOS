4f9088170bffc8c0df01ae06fb1fa5179d085718
Patrick José Pereira <patrickelectric@gmail.com>
2021-11-22 11:23:32 -0300

    core: services: ping: Add typedefs
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/services/ping/typedefs.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+
+from pingutils import PingDeviceDescriptor
+
+
+# TODO: This is a ugly workaround to have SysFS working for us
+# Issue: https://github.com/tiangolo/fastapi/issues/4189
+# pylint: disable=too-many-instance-attributes
+@dataclass
+class PingDeviceDescriptorModel:
+    ping_type: str
+    device_id: int
+    device_model: int
+    device_revision: int
+    firmware_version_major: int
+    firmware_version_minor: int
+    firmware_version_patch: int
+    port: str
+
+    @staticmethod
+    def from_descriptor(descriptor: PingDeviceDescriptor) -> "PingDeviceDescriptorModel":
+        return PingDeviceDescriptorModel(
+            ping_type=str(descriptor.ping_type),
+            device_id=descriptor.device_id,
+            device_model=descriptor.device_model,
+            device_revision=descriptor.device_revision,
+            firmware_version_major=descriptor.firmware_version_major,
+            firmware_version_minor=descriptor.firmware_version_minor,
+            firmware_version_patch=descriptor.firmware_version_patch,
+            port=descriptor.port.device,
+        )
