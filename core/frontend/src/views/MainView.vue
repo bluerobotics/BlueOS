6f1ad7013b2c30277d090439e646db121eda74ac
Willian Galvani <williangalvani@gmail.com>
2022-01-06 12:46:24 -0300

    core: freontend: mainview: remove warning about no logner providing 192.168.2.1 endpoint

--- core/frontend/src/views/MainView.vue
+++ core/frontend/src/views/MainView.vue
@@ -1,21 +1,5 @@
 <template>
   <v-container>
-    <v-bottom-sheet
-      :value="true"
-      inset
-    >
-      <v-card>
-        <v-card-title>Mavlink connection change</v-card-title>
-        <v-card-text>
-          New Companion doesn't provide a fixed UDP Client endpoint on 192.168.2.1 anymore. For new
-          connections Companion now provides an UDP Server locally on port 14550.
-
-          For connecting QGroundControl, for example, please go to Comm Links and create an
-          UDP endpoint pointing to companion.local or Companion's IP address (e.g.:192.168.2.2), on port 14550.
-          <v-card-text />
-        </v-card-text>
-      </v-card>
-    </v-bottom-sheet>
     <v-card>
       <v-card-title><p>Welcome to companion <strong>B.E.T.A test</strong>!</p></v-card-title>
       <v-card-text>
