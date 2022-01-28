eab9c5c3fe36f875f6925ca8f3e08ea43bdca54f
Patrick José Pereira <patrickelectric@gmail.com>
2022-01-28 15:05:24 -0300

    core: frontend: components: app: Add ErrorMessage
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/components/app/ErrorMessage.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-snackbar
+    v-model="show"
+    timeout="-1"
+  >
+    {{ message }}
+
+    <template #action="{ attrs }">
+      <v-btn
+        color="pink"
+        text
+        v-bind="attrs"
+        @click="show = false"
+      >
+        Close
+      </v-btn>
+    </template>
+  </v-snackbar>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import error_message_manager from '@/libs/error-message'
+
+export default Vue.extend({
+  name: 'ErrorMessage',
+  data() {
+    return {
+      message: '',
+      show: false,
+    }
+  },
+  mounted() {
+    error_message_manager.addCallback((message) => {
+      this.message = message
+      this.show = true
+    })
+  },
+})
+</script>
