c0aa902d4c5c5106c87172a4965093f6f64c1c84
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-01-31 12:23:29 -0300

    core: ardupilot-manager: Move endpoint-handling methods to mavlink-manager

--- core/services/ardupilot_manager/mavlink_proxy/Manager.py
+++ core/services/ardupilot_manager/mavlink_proxy/Manager.py
@@ -8,6 +8,7 @@ from loguru import logger
 # pylint: disable=unused-import
 import mavlink_proxy.MAVLinkRouter
 import mavlink_proxy.MAVProxy
+from exceptions import EndpointCreationFail, EndpointDeleteFail, EndpointUpdateFail
 from mavlink_proxy.AbstractRouter import AbstractRouter
 from mavlink_proxy.Endpoint import Endpoint
 
@@ -43,13 +44,49 @@ class Manager:
     def remove_endpoint(self, endpoint: Endpoint) -> None:
         self.tool.remove_endpoint(endpoint)
 
-    def add_endpoints(self, endpoints: Set[Endpoint]) -> None:
-        for endpoint in endpoints:
-            self.add_endpoint(endpoint)
+    def add_endpoints(self, new_endpoints: Set[Endpoint]) -> None:
+        for endpoint in new_endpoints:
+            try:
+                self.add_endpoint(endpoint)
+            except Exception as error:
+                self._reset_endpoints()
+                raise EndpointCreationFail(
+                    f"Failed adding endpoint '{endpoint.name}'. Endpoints reset to previous state."
+                ) from error
 
-    def remove_endpoints(self, endpoints: Set[Endpoint]) -> None:
-        for endpoint in endpoints:
-            self.remove_endpoint(endpoint)
+    def remove_endpoints(self, endpoints_to_remove: Set[Endpoint]) -> None:
+        protected_endpoints = set(filter(lambda endpoint: endpoint.protected, endpoints_to_remove))
+        if protected_endpoints:
+            raise ValueError(f"Endpoints {[e.name for e in protected_endpoints]} are protected. Aborting operation.")
+
+        for endpoint in endpoints_to_remove:
+            try:
+                self.remove_endpoint(endpoint)
+            except Exception as error:
+                self._reset_endpoints()
+                raise EndpointDeleteFail(
+                    f"Failed removing endpoint '{endpoint.name}'. Endpoints reset to previous state."
+                ) from error
+
+    def update_endpoints(self, endpoints_to_update: Set[Endpoint]) -> None:
+        old_endpoints = self.endpoints()
+
+        protected_endpoints = set(filter(lambda endpoint: endpoint.protected, endpoints_to_update))
+        if protected_endpoints:
+            raise ValueError(f"Endpoints {[e.name for e in protected_endpoints]} are protected. Aborting operation.")
+
+        for updated_endpoint in endpoints_to_update:
+            old_endpoint = next((e for e in old_endpoints if e.name == updated_endpoint.name), None)
+            try:
+                if not old_endpoint:
+                    raise ValueError(f"Endpoint '{updated_endpoint.name}' does not exist.")
+                self.remove_endpoint(old_endpoint)
+                self.add_endpoint(updated_endpoint)
+            except Exception as error:
+                self._reset_endpoints()
+                raise EndpointUpdateFail(
+                    f"Failed updating endpoint '{updated_endpoint.name}'. Endpoints reset to previous state."
+                ) from error
 
     def endpoints(self) -> Set[Endpoint]:
         return self.tool.endpoints()
@@ -58,7 +95,7 @@ class Manager:
         """Remove all output endpoints."""
         self.tool.clear_endpoints()
 
-    def reset_endpoints(self) -> None:
+    def _reset_endpoints(self) -> None:
         self.clear_endpoints()
         self.add_endpoints(self._last_valid_endpoints)
 
