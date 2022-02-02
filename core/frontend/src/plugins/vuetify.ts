84f2681b0f81249ac1ba2cc17ac5ab270f882581
Patrick José Pereira <patrickelectric@gmail.com>
2022-02-02 08:27:26 -0300

    core: frontend: plugins: vuetify: Integrate simple-icons with vuetify icon system
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/plugins/vuetify.ts
+++ core/frontend/src/plugins/vuetify.ts
@@ -1,12 +1,27 @@
 import '@mdi/font/css/materialdesignicons.css'
 
+import simple_icons from 'simple-icons'
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 
 Vue.use(Vuetify)
 
-export default new Vuetify({
+const vuetify = new Vuetify({
   icons: {
     iconfont: 'mdi',
   },
 })
+
+// Add simple-icons on vuetify
+// Use `$si-${name}` to access it
+// E.g: <v-icon> $si-discourse </v-icon>
+const icons = Object.entries(simple_icons).map(([key]) => ({
+  name: key,
+  simple_icon: simple_icons.Get(key),
+}))
+
+for (const icon of icons) {
+  vuetify.framework.icons.values[`si-${icon.simple_icon.slug}`] = `${icon.simple_icon.path}`
+}
+
+export default vuetify
