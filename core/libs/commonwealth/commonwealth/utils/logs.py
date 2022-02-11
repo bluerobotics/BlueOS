80430b4c57dd130cb565dedecfaf75a46e1d175d
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-02-11 09:35:03 -0300

    core: commonwealth: Allow retrieving default log path for service

--- core/libs/commonwealth/commonwealth/utils/logs.py
+++ core/libs/commonwealth/commonwealth/utils/logs.py
@@ -1,8 +1,11 @@
 import logging
+from datetime import datetime
 from logging import LogRecord
+from pathlib import Path
 from types import FrameType
 from typing import Optional, Union
 
+import appdirs
 from loguru import logger
 
 
@@ -23,3 +26,23 @@ class InterceptHandler(logging.Handler):
             depth += 1
 
         logger.opt(depth=depth, exception=record.exc_info).log(level, record.getMessage())
+
+
+def get_new_log_path(service_name: str) -> Path:
+    """Get default Path to a new log for a given service."""
+    # Prevent problematic service names
+    if service_name == "":
+        raise ValueError("Service name cannot be empty")
+    if "/" in service_name:
+        raise ValueError("Service name cannot contain forward slash character ('/').")
+    if "." in service_name:
+        raise ValueError("Service name cannot contain extension-separation character ('.').")
+
+    # Create folder for service logs if it doesn't exist yet
+    default_log_folder = Path(appdirs.user_log_dir())
+    service_log_folder = default_log_folder.joinpath(service_name)
+    service_log_folder.mkdir(parents=True, exist_ok=True)
+
+    # Returned log path are service-specific and store datetime information
+    datetime_now = datetime.now().strftime("%m-%d-%Y_%H:%M:%S")
+    return service_log_folder.joinpath(f"logfile_{datetime_now}.log")
