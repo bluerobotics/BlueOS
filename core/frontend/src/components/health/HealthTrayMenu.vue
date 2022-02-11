555b14bd0a7be4b818a99f4c9662d6667e20ff58
Willian Galvani <williangalvani@gmail.com>
2022-02-10 23:22:58 -0300

    core: frontend: add navbar warnings for vcgencmd flags and disk usage

--- core/frontend/src/components/health/HealthTrayMenu.vue
+++ core/frontend/src/components/health/HealthTrayMenu.vue
@@ -14,6 +14,38 @@
         v-on="on"
       >
         <v-icon
+          v-if="disk_usage_high"
+          class="px-1 blinking white-shadow"
+          color="red"
+          :title="`High disk usage high! (${disk_usage_percent.toFixed(1)}%) please clean up the disk`"
+        >
+          mdi-content-save-alert
+        </v-icon>
+        <v-icon
+          v-if="cpu_temperature_limit_reached"
+          class="px-1 blinking white-shadow"
+          color="red"
+          :title="`CPU temperature too high (${cpu_temperature} ºC), please cool down your vehicle!`"
+        >
+          mdi-thermometer
+        </v-icon>
+        <v-icon
+          v-if="cpu_throttled"
+          class="px-1 blinking white-shadow"
+          color="red"
+          :title="`CPU is throttled. Performance may be affected. Please check your power supply and cooling!`"
+        >
+          mdi-gauge-empty
+        </v-icon>
+        <v-icon
+          v-if="cpu_undervoltage"
+          class="px-1 blinking white-shadow"
+          color="red"
+          :title="`CPU has reported low voltage. Please check your power supply!`"
+        >
+          mdi-lightning-bolt
+        </v-icon>
+        <v-icon
           v-if="heartbeat_age() < 3000"
           class="px-1"
           :color="`rgba(255,255,255,${0.4 + (1000-heartbeat_age())/1000}`"
@@ -63,6 +95,7 @@ import Vue from 'vue'
 
 import mavlink from '@/store/mavlink'
 import system_information from '@/store/system-information'
+import { RaspberryEventType } from '@/types/system-information/platform'
 import mavlink_store_get from '@/utils/mavlink'
 
 export default Vue.extend({
@@ -73,6 +106,25 @@ export default Vue.extend({
       const main_sensor = temperature_sensors?.find((sensor) => sensor.name === 'CPU')
       return main_sensor ? main_sensor.temperature.toFixed(1) : 'Loading..'
     },
+    cpu_throttled() : boolean {
+      const events = system_information.platform?.raspberry?.events
+      const throttling = events?.occurring?.find((event) => [
+        RaspberryEventType.FrequencyCapping,
+        RaspberryEventType.Throttling,
+      ].includes(event.type))
+      return throttling !== undefined
+    },
+    cpu_undervoltage() : boolean {
+      const events = system_information.platform?.raspberry?.events
+      console.log(events)
+      const undervoltage = events?.occurring?.find((event) => event.type === RaspberryEventType.UnderVoltage)
+      return undervoltage !== undefined
+    },
+    cpu_temperature_limit_reached() : boolean {
+      const events = system_information.platform?.raspberry?.events
+      const temperature_limit = events?.occurring?.find((event) => event.type === RaspberryEventType.TemperatureLimit)
+      return temperature_limit !== undefined
+    },
     battery_voltage(): string {
       const voltage_microvolts = mavlink_store_get(mavlink, 'SYS_STATUS.messageData.voltage_battery') as number
       return (voltage_microvolts as number / 1000).toFixed(2)
@@ -81,6 +133,14 @@ export default Vue.extend({
     battery_current(): string {
       const current_centiampere = mavlink_store_get(mavlink, 'SYS_STATUS.messageData.current_battery') as number
       return (current_centiampere as number / 100).toFixed(2)
+    },
+    disk_usage_percent(): number {
+      const disks = system_information.system?.disk
+      const main_disk = disks?.find((sensor) => sensor.mount_point === '/')
+      return main_disk ? main_disk.available_space_B / main_disk.total_space_B / 0.01 : 0
+    },
+    disk_usage_high(): boolean {
+      return this.disk_usage_percent > 85
     },
   },
   methods: {
@@ -96,6 +156,15 @@ export default Vue.extend({
 </script>
 
 <style>
+.blinking {
+  animation: blinker 1s linear infinite;
+}
+@keyframes blinker {
+  50% {
+    opacity: 0.5;
+  }
+}
+
 .white-shadow {
   text-shadow: 0 0 3px #FFF;
 }
