608d3d29acd02160c800bab87e052db0f72a40e0
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-02-14 14:04:00 -0300

    core: frontend: Allow tooltips as vue-directives

--- core/frontend/src/main.ts
+++ core/frontend/src/main.ts
@@ -1,6 +1,8 @@
 import Vue from 'vue'
+import VueTooltipDirective from 'vue-tooltip-directive'
 
 import App from './App.vue'
+import DefaultTooltip from './components/common/DefaultTooltip.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
@@ -11,3 +13,7 @@ new Vue({
   vuetify,
   render: (h) => h(App),
 }).$mount('#app')
+
+Vue.use(VueTooltipDirective, {
+  component: DefaultTooltip,
+})
