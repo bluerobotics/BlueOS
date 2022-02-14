9ffc26509c65d9472234903040c3e816418013ed
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-02-14 14:04:00 -0300

    core: frontend: Allow tooltips as vue-directives

---
+++ core/frontend/src/components/common/DefaultTooltip.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-tooltip
+    :top="position === 'top'"
+    :right="position === 'right'"
+    :bottom="position === 'bottom'"
+    :left="position === 'left'"
+    :activator="activator"
+  >
+    <span>{{ content }}</span>
+  </v-tooltip>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'DefaultTooltip',
+  props: {
+    activator: { required: true }, // eslint-disable
+    content: { type: String, required: true },
+    position: { type: String, required: false, default: 'bottom' },
+  },
+})
+</script>
--- core/frontend/src/types/shims-general.ts
+++ core/frontend/src/types/shims-general.ts
@@ -2,3 +2,5 @@ declare module '*.svg' {
   const value: string
   export default value
 }
+
+declare module 'vue-tooltip-directive'
