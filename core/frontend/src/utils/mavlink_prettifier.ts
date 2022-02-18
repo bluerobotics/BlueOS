5ac6b4542bfa6579e369fd3b88ff815a860fbe74
Patrick José Pereira <patrickelectric@gmail.com>
2022-02-18 06:10:48 -0300

    core: frontend: utils: mavlink_prettier: Use message to add prefix
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/utils/mavlink_prettifier.ts
+++ core/frontend/src/utils/mavlink_prettifier.ts
@@ -2,34 +2,34 @@ import { Dictionary } from '@/types/common'
 
 const formatters = {
   AHRS2(message: any) {
-    return `AHRS2 - Roll: ${message.roll.toFixed(2)} rad, Pitch: `
+    return `Roll: ${message.roll.toFixed(2)} rad, Pitch: `
     + `${message.pitch.toFixed(2)} rad, Yaw: ${message.yaw.toFixed(2)} rad`
   },
   ATTITUDE(message: any): string {
-    return `ATTITUDE - Roll: ${message.roll.toFixed(2)} rad, Pitch: `
+    return `Roll: ${message.roll.toFixed(2)} rad, Pitch: `
     + `${message.pitch.toFixed(2)} rad, Yaw: ${message.yaw.toFixed(2)} rad`
   },
   BATTERY_STATUS(message: any) {
-    return `BATTERY_STATUS - ${message.voltages[0] / 1000} V ${message.current_consumed} mAh consumed`
+    return `${message.voltages[0] / 1000} V ${message.current_consumed} mAh consumed`
   },
   COMMAND_ACK(message: any) {
-    return `COMMAND_ACK - ${message.command.type.replace('MAV_CMD_', '')}`
+    return `${message.command.type.replace('MAV_CMD_', '')}`
       + ` - ${message.result.type.replace('MAV_RESULT_', '')}`
   },
   GLOBAL_POSITION_INT(message: any) {
-    return `GLOBAL_POSITION_INT - Lat: ${message.lat.toFixed(5)} Lon: ${message.lon.toFixed(5)}`
+    return `Lat: ${message.lat.toFixed(5)} Lon: ${message.lon.toFixed(5)}`
   },
   HEARTBEAT(message: any) {
-    return `HEATBEAT - ${message.mavtype.type}`
+    return `${message.mavtype.type}`
   },
   NAMED_VALUE_FLOAT(message: any) {
-    return `NAMED_VALUE_FLOAT - ${message.name.join('')} = ${message.value.toFixed(2)}`
+    return `${message.name.join('')} = ${message.value.toFixed(2)}`
   },
   STATUSTEXT(message: any): string {
     return `${message.severity.type.replace('MAV_SEVERITY_', '')}: ${message.text.join('')}`
   },
   SYS_STATUS(message: any) {
-    return `SYS_STATUS - Batt: (${message.current_battery / 100} A, ${message.voltage_battery / 1000} V)`
+    return `Batt: (${message.current_battery / 100} A, ${message.voltage_battery / 1000} V)`
   },
 } as Dictionary<(message: any) => string>
 
@@ -39,7 +39,7 @@ export default function prettify(message: any): string {
   }
   const message_type = message.type as string
   if (message_type in formatters) {
-    return formatters[message_type](message)
+    return `${message_type} - ${formatters[message_type](message)}`
   }
   return message_type
 }
