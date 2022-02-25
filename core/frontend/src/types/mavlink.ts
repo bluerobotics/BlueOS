da870703b2fa316b9e057026589a55e2229ec5bb
Patrick José Pereira <patrickelectric@gmail.com>
2022-02-25 15:43:02 -0300

    core: frontend: types: Use new mavlink2rest websocket struct
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/types/mavlink.ts
+++ core/frontend/src/types/mavlink.ts
@@ -1,6 +1,18 @@
+import { JSONValue } from '@/types/common'
+
+export interface MavlinkHeader {
+    system_id: number,
+    component_id: number,
+    sequence: number
+}
+export interface MavlinkData {
+    header: MavlinkHeader,
+    message: JSONValue
+}
+
 export interface MavlinkMessage {
     messageName: string
-    messageData: {[key: string]: {value: string|number}}
+    messageData: MavlinkData
     requestedMessageRate: number
     timestamp: Date
 }
