cf618d4f8a8f6b396e07dff739856f0871360248
Patrick José Pereira <patrickelectric@gmail.com>
2022-03-15 13:25:48 -0300

    bootstrap: Update project to work with BlueOS
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- bootstrap/bootstrap/setup.py
+++ bootstrap/bootstrap/setup.py
@@ -3,9 +3,9 @@
 from setuptools import setup
 
 setup(
-    name="companion_bootstrap",
+    name="blueos_bootstrap",
     version="0.0.1",
-    description="Blue Robotics Ardusub Companion Docker System Bootstrap",
+    description="Blue Robotics Ardusub BlueOS Docker System Bootstrap",
     license="MIT",
     install_requires=["docker == 5.0.0", "six == 1.15.0", "requests == 2.26.0"],
 )
