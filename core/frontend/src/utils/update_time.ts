3d1e0938c6fbf5835d59b14bf4e5ee1eed5c1304
Patrick José Pereira <patrickelectric@gmail.com>
2022-03-23 14:41:31 -0300

    core: frontend: Move UpdateTimeLibrary component to library
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/utils/update_time.ts
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+
+import notifications from '@/store/notifications'
+import { update_time_service } from '@/types/frontend_services'
+import back_axios from '@/utils/api'
+
+export default async function run() : Promise<void> {
+  await back_axios({
+    url: '/commander/v1.0/set_time',
+    method: 'post',
+    params: {
+      unix_time_seconds: Math.round(new Date().getTime() / 1000),
+      i_know_what_i_am_doing: true,
+    },
+    timeout: 10000,
+  })
+    .catch((error) => {
+      // Connection lost/timeout, normal when we are turnning off/rebooting
+      if (error.code === 'ECONNABORTED') {
+        run()
+        return
+      }
+
+      const message = `Failed to commit operation: ${error.message}`
+      notifications.pushError({ service: update_time_service, type: 'UPDATE_TIME_FAIL', message })
+    })
+}
