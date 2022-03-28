3881efdab6f6cbaf9bf7b90ef2880c88ef5029dd
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-03-28 14:43:06 -0300

    frontend: ethernet: Update error messages to use new backend pattern

--- core/frontend/src/components/ethernet/InterfaceCard.vue
+++ core/frontend/src/components/ethernet/InterfaceCard.vue
@@ -108,7 +108,7 @@ export default Vue.extend({
         params: { interface_name: this.adapter.name, ip_address: ip },
       })
         .catch((error) => {
-          const message = `Could not delete address '${ip}' on '${this.adapter.name}': ${error.message}.`
+          const message = error.response.data.detail ?? error.message
           notifications.pushError({ service: ethernet_service, type: 'ETHERNET_ADDRESS_DELETE_FAIL', message })
         })
     },
