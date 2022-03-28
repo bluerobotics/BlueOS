999fa22178030e050716c7b7ef9ea0ee89c461e4
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-03-28 14:43:03 -0300

    core: commander: Use global exception handler

--- core/services/commander/main.py
+++ core/services/commander/main.py
@@ -9,8 +9,9 @@ from typing import Any
 
 import appdirs
 import uvicorn
+from commonwealth.utils.apis import GenericErrorHandlingRoute
 from commonwealth.utils.logs import InterceptHandler, get_new_log_path
-from fastapi import FastAPI, HTTPException, Response, status
+from fastapi import FastAPI, HTTPException, status
 from fastapi.responses import HTMLResponse
 from fastapi_versioning import VersionedFastAPI, version
 from loguru import logger
@@ -24,6 +25,7 @@ app = FastAPI(
     title="Commander API",
     description="Commander is a BlueOS service responsible to abstract simple commands to the frontend.",
 )
+app.router.route_class = GenericErrorHandlingRoute
 logger.info("Starting Commander!")
 
 
@@ -68,29 +70,22 @@ def check_what_i_am_doing(i_know_what_i_am_doing: bool = False) -> None:
 
 @app.post("/command/host", status_code=status.HTTP_200_OK)
 @version(1, 0)
-async def command_host(response: Response, command: str, i_know_what_i_am_doing: bool = False) -> Any:
+async def command_host(command: str, i_know_what_i_am_doing: bool = False) -> Any:
     check_what_i_am_doing(i_know_what_i_am_doing)
     logger.debug(f"Running command: {command}")
-
-    try:
-        output = run_command(command, False)
-        logger.debug(f"Output: {output}")
-        response.status_code = status.HTTP_200_OK
-        message = {
-            "stdout": f"{output.stdout!r}",
-            "stderr": f"{output.stderr!r}",
-            "return_code": output.returncode,
-        }
-        return message
-    except Exception as error:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        logger.exception(error)
-        return {"message": f"{error}"}
+    output = run_command(command, False)
+    logger.debug(f"Output: {output}")
+    message = {
+        "stdout": f"{output.stdout!r}",
+        "stderr": f"{output.stderr!r}",
+        "return_code": output.returncode,
+    }
+    return message
 
 
 @app.post("/set_time", status_code=status.HTTP_200_OK)
 @version(1, 0)
-async def set_time(response: Response, unix_time_seconds: int, i_know_what_i_am_doing: bool = False) -> Any:
+async def set_time(unix_time_seconds: int, i_know_what_i_am_doing: bool = False) -> Any:
     unix_time_seconds_now = int(time.time())
     if abs(unix_time_seconds_now - unix_time_seconds) < 5 * 60:
         return {
@@ -100,53 +95,38 @@ async def set_time(response: Response, unix_time_seconds: int, i_know_what_i_am_
 
     # It's necessary to stop ntp sync before setting time
     command = f"sudo timedatectl set-ntp false; sudo date -s '@{unix_time_seconds}'; sudo timedatectl set-ntp true"
-    return await command_host(response, command, i_know_what_i_am_doing)
+    return await command_host(command, i_know_what_i_am_doing)
 
 
 # TODO: Update commander to work with openapi modules and improve modularity and code organization
 @app.post("/shutdown", status_code=status.HTTP_200_OK)
 @version(1, 0)
-async def shutdown(response: Response, shutdown_type: ShutdownType, i_know_what_i_am_doing: bool = False) -> Any:
+async def shutdown(shutdown_type: ShutdownType, i_know_what_i_am_doing: bool = False) -> Any:
     check_what_i_am_doing(i_know_what_i_am_doing)
-
-    try:
-        hold_time_seconds = 5
-        if shutdown_type == ShutdownType.REBOOT:
-            output = run_command(f"(sleep {hold_time_seconds}; sudo reboot)&")
-            logger.debug(f"reboot: {output}")
-        elif shutdown_type == ShutdownType.POWEROFF:
-            output = run_command(f"(sleep {hold_time_seconds}; sudo shutdown --poweroff -h now)&")
-            logger.debug(f"shutdown: {output}")
-
-        return HTMLResponse(status_code=200)
-    except Exception as error:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        logger.exception(error)
-        return {"message": f"{error}"}
+    hold_time_seconds = 5
+    if shutdown_type == ShutdownType.REBOOT:
+        output = run_command(f"(sleep {hold_time_seconds}; sudo reboot)&")
+        logger.debug(f"reboot: {output}")
+    elif shutdown_type == ShutdownType.POWEROFF:
+        output = run_command(f"(sleep {hold_time_seconds}; sudo shutdown --poweroff -h now)&")
+        logger.debug(f"shutdown: {output}")
 
 
 @app.post("/settings/reset", status_code=status.HTTP_200_OK)
 @version(1, 0)
-async def reset_settings(response: Response, i_know_what_i_am_doing: bool = False) -> Any:
+async def reset_settings(i_know_what_i_am_doing: bool = False) -> Any:
     check_what_i_am_doing(i_know_what_i_am_doing)
 
-    try:
-        user_config_dir = Path(appdirs.user_config_dir())
-        for item in user_config_dir.glob("*"):
-            try:
-                if item.is_file():
-                    item.unlink()
-                if item.is_dir():
-                    # Delete folder and its contents
-                    shutil.rmtree(item)
-            except Exception as exception:
-                logger.warning(f"Failed to delete: {item}, {exception}")
-
-        return HTMLResponse(status_code=200)
-    except Exception as error:
-        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-        logger.exception(error)
-        return {"message": f"Could not reset user configuration, {error}"}
+    user_config_dir = Path(appdirs.user_config_dir())
+    for item in user_config_dir.glob("*"):
+        try:
+            if item.is_file():
+                item.unlink()
+            if item.is_dir():
+                # Delete folder and its contents
+                shutil.rmtree(item)
+        except Exception as exception:
+            logger.warning(f"Failed to delete: {item}, {exception}")
 
 
 app = VersionedFastAPI(app, version="1.0.0", prefix_format="/v{major}.{minor}", enable_latest=True)
