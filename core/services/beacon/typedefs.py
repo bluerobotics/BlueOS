1f77f5d4c17555afac0781be8a2271e5507e82d6
Willian Galvani <williangalvani@gmail.com>
2022-04-04 13:47:53 -0300

    core: beacon: add typedefs

---
+++ core/services/beacon/typedefs.py
@@ -0,0 +1,33 @@
+from enum import Enum
+
+from pydantic import BaseModel
+
+
+class InterfaceType(str, Enum):
+    WIRED = "WIRED"
+    WIFI = "WIFI"
+    UNKNOWN = "UNKNOWN"
+
+    @staticmethod
+    def guess_from_name(name: str) -> "InterfaceType":
+        if name.startswith("wl"):
+            return InterfaceType.WIFI
+        if name.startswith("en"):
+            return InterfaceType.WIRED
+        if name.startswith("eth"):
+            return InterfaceType.WIRED
+        return InterfaceType.UNKNOWN
+
+
+class MdnsEntry(BaseModel):
+    ip: str
+    hostname: str
+    fullname: str
+    interface: str
+    interface_type: InterfaceType
+    service_type: str
+
+
+class IpInfo(BaseModel):
+    client_ip: str
+    interface_ip: str
