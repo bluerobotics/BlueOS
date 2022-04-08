f4ed878a003fd1b4fd21d205fb37dd15400496ce
Patrick José Pereira <patrickelectric@gmail.com>
2022-04-08 12:58:46 -0300

    core: frontent: store: notifications: Remove some functions
    Notifier will do the job now
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/store/notifications.ts
+++ core/frontend/src/store/notifications.ts
@@ -1,10 +1,10 @@
 import {
-  Action, getModule, Module, Mutation, VuexModule,
+  getModule, Module, Mutation, VuexModule,
 } from 'vuex-module-decorators'
 
 import store from '@/store'
 import {
-  CumulatedNotification, LiveNotification, Notification, NotificationContent, NotificationLevel,
+  CumulatedNotification, Notification, NotificationLevel,
 } from '@/types/notifications'
 
 @Module({
@@ -57,31 +57,6 @@ class NotificationsStore extends VuexModule {
       return
     }
     this.cumulated_notifications.push({ notification, count: 1 })
-  }
-
-  @Action
-  async pushSuccess(notif: NotificationContent): Promise<void> {
-    this.pushNotification(new LiveNotification(NotificationLevel.Success, notif.service, notif.type, notif.message))
-  }
-
-  @Action
-  async pushError(notif: NotificationContent): Promise<void> {
-    this.pushNotification(new LiveNotification(NotificationLevel.Error, notif.service, notif.type, notif.message))
-  }
-
-  @Action
-  async pushInfo(notif: NotificationContent): Promise<void> {
-    this.pushNotification(new LiveNotification(NotificationLevel.Info, notif.service, notif.type, notif.message))
-  }
-
-  @Action
-  async pushWarning(notif: NotificationContent): Promise<void> {
-    this.pushNotification(new LiveNotification(NotificationLevel.Warning, notif.service, notif.type, notif.message))
-  }
-
-  @Action
-  async pushCritical(notif: NotificationContent): Promise<void> {
-    this.pushNotification(new LiveNotification(NotificationLevel.Critical, notif.service, notif.type, notif.message))
   }
 
   @Mutation
