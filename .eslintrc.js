f92469328affed6cf9dc712d105b66813f9e0e5d
Willian Galvani <williangalvani@gmail.com>
2022-04-08 12:22:35 -0300

    core: frontend: add require-await rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -50,6 +50,7 @@ module.exports = {
     'no-param-reassign': 'off',
     'no-shadow': 'off',
     'no-useless-constructor': 'off',
+    'require-await': 'error',
     semi: ['error', 'never'],
     'simple-import-sort/exports': 'error',
     'simple-import-sort/imports': 'error',
