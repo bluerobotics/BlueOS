8a77e280368a913378ad68c66e4f0baab96dc739
Patrick José Pereira <patrickelectric@gmail.com>
2022-04-14 15:44:23 -0300

    core: frontend: types: helper: Add ServiceMetadata
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/types/helper.ts
+++ core/frontend/src/types/helper.ts
@@ -1,7 +1,18 @@
+export interface ServiceMetadata {
+    name: string
+    description: string
+    icon: string
+    company: string
+    version: string
+    webpage: string
+    api: string
+}
+
 export interface Service {
     valid: boolean
     title: string
     documentation_url: string
     versions: Array<string>
     port: number
+    metadata?: ServiceMetadata
 }
