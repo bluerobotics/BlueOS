12743548de7749656a0e69bd7ba021bbeca2a08e
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-04-15 19:16:50 -0300

    frontend: nmea-injector: Remove card about non-persistency

--- core/frontend/src/components/nmea-injector/NMEAInjector.vue
+++ core/frontend/src/components/nmea-injector/NMEAInjector.vue
@@ -35,10 +35,6 @@
       <p class="text-h6">
         No NMEA sockets available.
       </p>
-      <p class="text-subtitle-1">
-        Remember that for the time, NMEA sockets are not persistent along system reboots, so
-        if you have restarted your system you need to re-create your sockets.
-      </p>
     </v-container>
 
     <v-fab-transition>
