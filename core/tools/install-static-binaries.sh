6d03e56ae7dccaf0dc6094cb201230c6fa537b6b
Patrick José Pereira <patrickelectric@gmail.com>
2022-04-20 09:28:56 -0300

    core: tools: Add install scripts for static binaries and system tools
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/tools/install-static-binaries.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# Script to install tools that are simple static binaries
+
+TOOLS=(
+    bridges
+    linux2rest
+    mavlink2rest
+    mavlink_camera_manager
+    mavlink_router
+    ttyd
+)
+
+parallel --halt now,fail=1 '/home/pi/tools/{}/bootstrap.sh' ::: "${TOOLS[@]}"
