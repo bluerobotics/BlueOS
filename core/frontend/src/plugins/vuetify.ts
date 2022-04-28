956f9e3096670d42a70caef0323e87e89130145a
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-04-28 15:10:46 -0300

    frontend: vuetify: Use defined color sets

--- core/frontend/src/plugins/vuetify.ts
+++ core/frontend/src/plugins/vuetify.ts
@@ -4,11 +4,56 @@ import simple_icons from 'simple-icons'
 import Vue from 'vue'
 import Vuetify from 'vuetify/lib/framework'
 
+import * as BR_COLORS from '@/assets/colors/blue_robotics'
+import * as DEFAULT_COLORS from '@/assets/colors/default'
+
 Vue.use(Vuetify)
 
 const vuetify = new Vuetify({
   icons: {
     iconfont: 'mdi',
+  },
+  theme: {
+    themes: {
+      light: {
+        // Default colors used by BlueOS
+        primary: DEFAULT_COLORS.PRIMARY,
+        secondary: DEFAULT_COLORS.SECONDARY,
+        accent: DEFAULT_COLORS.ACCENT,
+        success: DEFAULT_COLORS.SUCCESS,
+        error: DEFAULT_COLORS.ERROR,
+        info: DEFAULT_COLORS.INFO,
+        warning: DEFAULT_COLORS.WARNING,
+        critical: DEFAULT_COLORS.CRITICAL,
+
+        // Colors from Blue Robotics color palette
+        br_blue: BR_COLORS.BR_BLUE,
+        mariner_blue: BR_COLORS.MARINER_BLUE,
+        blue_whale: BR_COLORS.BLUE_WHALE,
+        tether_yellow: BR_COLORS.TETHER_YELLOW,
+        tuna: BR_COLORS.TUNA,
+        oyster: BR_COLORS.OYSTER,
+      },
+      dark: {
+        // Default colors used by BlueOS
+        primary: DEFAULT_COLORS.PRIMARY,
+        secondary: DEFAULT_COLORS.SECONDARY,
+        accent: DEFAULT_COLORS.ACCENT,
+        success: DEFAULT_COLORS.SUCCESS,
+        error: DEFAULT_COLORS.ERROR,
+        info: DEFAULT_COLORS.INFO,
+        warning: DEFAULT_COLORS.WARNING,
+        critical: DEFAULT_COLORS.CRITICAL,
+
+        // Colors from Blue Robotics color palette
+        br_blue: BR_COLORS.BR_BLUE,
+        mariner_blue: BR_COLORS.MARINER_BLUE,
+        blue_whale: BR_COLORS.BLUE_WHALE,
+        tether_yellow: BR_COLORS.TETHER_YELLOW,
+        tuna: BR_COLORS.TUNA,
+        oyster: BR_COLORS.OYSTER,
+      },
+    },
   },
 })
 
