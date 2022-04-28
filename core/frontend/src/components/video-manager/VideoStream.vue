b2d43dd71ba56b8fc62309ffbf83395a2c49be0d
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-04-28 15:10:46 -0300

    frontend: video-manager: Make it mobile-friendly

--- core/frontend/src/components/video-manager/VideoStream.vue
+++ core/frontend/src/components/video-manager/VideoStream.vue
@@ -1,62 +1,70 @@
 <template>
   <v-card
-    width="100%"
-    class="pa-0 my-4"
+    style="max-width: 93%; margin: auto;"
   >
-    <v-container>
-      <v-row align="center">
-        <v-col cols="5">
-          <v-card-title>{{ stream.video_and_stream.name }}</v-card-title>
-          <v-card-subtitle>{{ settings_summary }}</v-card-subtitle>
-          <p class="pl-4">
-            Status: {{ stream.running ? 'running' : 'Not running' }}
-          </p>
-        </v-col>
-        <v-col cols="5">
-          <v-simple-table
-            dense
-            class="text-center"
-          >
-            <template #default>
-              <tbody>
-                <tr>
-                  <td>{{ stream.video_and_stream.stream_information.configuration.encode }}</td>
-                </tr>
-                <tr
-                  v-for="(endpoint, index) in stream.video_and_stream.stream_information.endpoints"
-                  :key="index"
-                >
-                  <td>
-                    {{ endpoint }}
-                  </td>
-                </tr>
-                <tr>
-                  <td>{{ source_path }}</td>
-                </tr>
-              </tbody>
-            </template>
-          </v-simple-table>
-        </v-col>
-        <v-col cols="2">
-          <v-btn
-            class="ma-2 elevation-1"
-            color="primary"
-            icon
-            @click="openStreamEditDialog"
-          >
-            <v-icon>mdi-pencil</v-icon>
-          </v-btn>
-          <v-btn
-            class="ma-2 elevation-1"
-            color="error"
-            icon
-            @click="deleteStream"
-          >
-            <v-icon>mdi-delete</v-icon>
-          </v-btn>
-        </v-col>
-      </v-row>
+    <v-container
+      class="d-flex align-center elevation-0 py-6 px-4"
+      :class="{ 'flex-column': $vuetify.breakpoint.xs }"
+    >
+      <div
+        class="d-flex flex-column justify-space-between"
+        :class="{ 'align-center': $vuetify.breakpoint.xs }"
+      >
+        <p class="text-h6 text-no-wrap ma-0">
+          {{ stream.video_and_stream.name }}
+        </p>
+        <p class="text-subtitle-2 text-no-wrap text--secondary ma-0">
+          {{ settings_summary }}
+        </p>
+        <p class="mb-2 mt-6">
+          Status: {{ stream.running ? 'running' : 'Not running' }}
+        </p>
+      </div>
+      <v-simple-table
+        dense
+        class="text-center"
+        style="margin: auto;"
+      >
+        <template #default>
+          <tbody>
+            <tr>
+              <td>{{ stream.video_and_stream.stream_information.configuration.encode }}</td>
+            </tr>
+            <tr
+              v-for="(endpoint, index) in stream.video_and_stream.stream_information.endpoints"
+              :key="index"
+            >
+              <td>
+                {{ endpoint }}
+              </td>
+            </tr>
+            <tr>
+              <td>{{ source_path }}</td>
+            </tr>
+          </tbody>
+        </template>
+      </v-simple-table>
     </v-container>
+    <v-btn
+      class="stream-edit-btn elevation-1"
+      color="primary"
+      dark
+      fab
+      small
+      @click="openStreamEditDialog"
+    >
+      <v-icon>mdi-pencil</v-icon>
+    </v-btn>
+    <v-btn
+      class="stream-remove-btn elevation-1"
+      color="error"
+      dark
+      fab
+      small
+      @click="deleteStream"
+    >
+      <v-icon>mdi-delete</v-icon>
+    </v-btn>
     <video-stream-creation-dialog
       v-model="show_stream_edit_dialog"
       :device="device"
@@ -139,3 +147,16 @@ export default Vue.extend({
   },
 })
 </script>
+
+<style scoped>
+.stream-edit-btn {
+  position: absolute;
+  top: 15%;
+  right: -20px;
+}
+.stream-remove-btn {
+  position: absolute;
+  bottom: 15%;
+  right: -20px;
+}
+</style>
