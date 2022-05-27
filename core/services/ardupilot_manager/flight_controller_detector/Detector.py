e75a38ef3547c248a136d78601114777c099e4f2
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-05-27 15:38:51 -0300

    ardupilot-manager: Allow detection of serial bootloaders

--- core/services/ardupilot_manager/flight_controller_detector/Detector.py
+++ core/services/ardupilot_manager/flight_controller_detector/Detector.py
@@ -5,7 +5,8 @@ from loguru import logger
 from serial.tools.list_ports_linux import SysFS, comports
 from smbus2 import SMBus
 
-from typedefs import FlightController, Platform
+from flight_controller_detector.board_identification import identifiers
+from typedefs import FlightController, FlightControllerFlags, Platform
 
 
 class Detector:
@@ -45,13 +46,16 @@ class Detector:
         return None
 
     @staticmethod
+    def is_serial_bootloader(port: SysFS) -> bool:
+        return port.product is not None and "BL" in port.product
+
+    @staticmethod
     def detect_serial_platform(port: SysFS) -> Optional[Platform]:
-        if port.product in ["Pixhawk1", "PX4 FMU v2.x"]:
-            return Platform.Pixhawk1
-        if port.product in ["Pixhawk4", "PX4 FMU v5.x"]:
-            return Platform.Pixhawk4
-        if port.manufacturer in ["ArduPilot", "3D Robotics"] and (port.product and not "BL" in port.product):
-            return Platform.GenericSerial
+        for identifier in identifiers:
+            port_attr = getattr(port, identifier.attribute)
+            if port_attr is not None and identifier.id_value in port_attr:
+                return identifier.platform
+
         return None
 
     @staticmethod
@@ -67,7 +71,7 @@ class Detector:
             # usb_device_path property will be the same for two serial connections using the same USB port
             if port.usb_device_path not in [device.usb_device_path for device in unique_serial_devices]:
                 unique_serial_devices.append(port)
-        return [
+        boards = [
             FlightController(
                 name=port.product or port.name,
                 manufacturer=port.manufacturer,
@@ -77,6 +81,11 @@ class Detector:
             for port in unique_serial_devices
             if Detector.detect_serial_platform(port) is not None
         ]
+        for port in unique_serial_devices:
+            for board in boards:
+                if board.path == port.device and Detector.is_serial_bootloader(port):
+                    board.flags.append(FlightControllerFlags.is_bootloader)
+        return boards
 
     @staticmethod
     def detect_sitl() -> FlightController:
@@ -85,6 +94,9 @@ class Detector:
     @classmethod
     def detect(cls, include_sitl: bool = True) -> List[FlightController]:
         """Return a list of available flight controllers
+
+        Arguments:
+            include_sitl {bool} -- To include or not SITL controllers in the returned list
 
         Returns:
             List[FlightController]: List of available flight controllers
