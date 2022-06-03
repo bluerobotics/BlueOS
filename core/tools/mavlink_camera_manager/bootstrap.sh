c5b1e5f0769724eeca8e5816daceb76b48484d1b
Patrick José Pereira <patrickelectric@gmail.com>
2022-06-03 10:26:38 -0300

    core: tools: mavlink-camera-manager: Update to t3.4.0
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/mavlink_camera_manager/bootstrap.sh
+++ core/tools/mavlink_camera_manager/bootstrap.sh
@@ -4,15 +4,22 @@
 set -e
 
 LOCAL_BINARY_PATH="/usr/bin/mavlink-camera-manager"
-
-VERSION=t3.3.2
+ARTIFACT_PREFIX="mavlink-camera-manager"
+VERSION=t3.4.0
 
 # By default we install armv7
-REMOTE_BINARY_URL="https://github.com/bluerobotics/mavlink-camera-manager/releases/download/${VERSION}/mavlink-camera-manager-armv7"
+REMOTE_BINARY_URL="https://github.com/bluerobotics/mavlink-camera-manager/releases/download/${VERSION}/${ARTIFACT_PREFIX}-armv7.zip"
 if [[ "$(uname -m)" == "x86_64"* ]]; then
-    REMOTE_BINARY_URL="https://github.com/bluerobotics/mavlink-camera-manager/releases/download/${VERSION}/mavlink-camera-manager-linux-desktop"
+    REMOTE_BINARY_URL="https://github.com/bluerobotics/mavlink-camera-manager/releases/download/${VERSION}/${ARTIFACT_PREFIX}-linux-desktop.zip"
 fi
 
 # Download and install the camera manager under user binary folder with the correct permissions
-wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
+ARTIFACT_PREFIX="mavlink-camera-manager"
+wget "$REMOTE_BINARY_URL" -O "${ARTIFACT_PREFIX}.zip"
+unzip "${ARTIFACT_PREFIX}.zip" -d "${ARTIFACT_PREFIX}"
+# Binary
+cp "${ARTIFACT_PREFIX}/${ARTIFACT_PREFIX}"* "$LOCAL_BINARY_PATH"
 chmod +x "$LOCAL_BINARY_PATH"
+# WWW folder
+mkdir -p /opt/blueos/mavlink-camera-manager
+cp -r "${ARTIFACT_PREFIX}/www" /opt/blueos/mavlink-camera-manager
