c2e0d2a084f8bfb6bec6c046134b94fc441a4075
Patrick José Pereira <patrickelectric@gmail.com>
2022-07-15 11:09:38 -0300

    core: frontend: types: system-information: Add serial
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/types/system-information/serial.ts
@@ -0,0 +1,32 @@
+import { JSONValue } from '@/types/common'
+
+export interface UsbPortInfo {
+    /// Vendor ID
+    vid: number,
+    /// Product ID
+    pid: number,
+    /// Serial number (arbitrary string)
+    serial_number: string | null,
+    /// Manufacturer (arbitrary string)
+    manufacturer: string | null,
+    /// Product name (arbitrary string)
+    product: string | null,
+}
+
+export type SerialPortType = null | string | UsbPortInfo;
+
+export interface SerialPortInfo {
+    // The short name of the serial port
+    name: string,
+    // The long name of the serial port
+    by_path: string | null,
+    // Time since the device was created,
+    by_path_created_ms_ago: number | null
+    // Udev information from the device
+    udev_properties: JSONValue | null,
+}
+
+/** Base structure that provides serial port information */
+export interface Serial {
+    ports: SerialPortInfo[],
+}
