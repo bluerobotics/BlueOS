89e4bb637bd2eaddcd9c9a21ce6c47fbc3b7ae8d
Patrick José Pereira <patrickelectric@gmail.com>
2022-07-15 10:06:28 -0300

    core: frontend: types: bridges: Add BridgeWithSerialInfo
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/types/bridges.ts
+++ core/frontend/src/types/bridges.ts
@@ -1,8 +1,14 @@
 import { Baudrate } from '@/types/common'
+import { SerialPortInfo } from '@/types/system-information/serial'
 
 export interface Bridge {
   serial_path: string
   baud: Baudrate
   ip: string
   udp_port: number
+}
+
+export interface BridgeWithSerialInfo {
+  bridge: Bridge
+  serial_info: SerialPortInfo | undefined
 }
