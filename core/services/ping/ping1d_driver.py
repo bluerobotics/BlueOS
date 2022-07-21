17ee38e8d219b8f86abc5dc529f841f4f6b60178
Willian Galvani <williangalvani@gmail.com>
2022-07-21 18:54:27 -0300

    Core: Ping: add ping1d mavlink driver

--- core/services/ping/ping1d_driver.py
+++ core/services/ping/ping1d_driver.py
@@ -1,4 +1,4 @@
-from ping1d_mavlink import drive
+from ping1d_mavlink import Ping1DMavlinkDriver
 from pingdriver import PingDriver
 from pingutils import PingDeviceDescriptor
 
@@ -7,7 +7,8 @@ class Ping1DDriver(PingDriver):
     def __init__(self, ping: PingDeviceDescriptor, port: int) -> None:
         super().__init__(ping, port)
         self.port = port
+        self.mavlink_driver = Ping1DMavlinkDriver()
 
     async def start(self) -> None:
         await super().start()
-        await drive(self.port)
+        await self.mavlink_driver.drive(self.port)
--- core/services/ping/ping1d_mavlink.py
+++ core/services/ping/ping1d_mavlink.py
@@ -22,116 +22,116 @@ from brping import (
 )
 from commonwealth.mavlink_comm.MavlinkComm import MavlinkMessenger
 
-mavlink2rest = MavlinkMessenger()
+class Ping1DMavlinkDriver():
+    mavlink2rest = MavlinkMessenger()
+
+    def distance_message(self, time_boot_ms: int, distance: int, device_id: int) -> Dict[str, Any]:
+        return {
+            "type": "DISTANCE_SENSOR",
+            "time_boot_ms": time_boot_ms,
+            "min_distance": 20,
+            "max_distance": 5000,
+            "current_distance": int(distance),
+            "mavtype": {"type": "MAV_DISTANCE_SENSOR_LASER"},
+            "id": device_id,
+            "orientation": {"type": "MAV_SENSOR_ROTATION_PITCH_270"},
+            "covariance": 0,
+            "horizontal_fov": 0,
+            "vertical_fov": 0,
+            "quaternion": [0, 0, 0, 0],
+            "signal_quality": 0,
+        }
 
 
-def distance_message(time_boot_ms: int, distance: int, device_id: int) -> Dict[str, Any]:
-    return {
-        "type": "DISTANCE_SENSOR",
-        "time_boot_ms": time_boot_ms,
-        "min_distance": 20,
-        "max_distance": 5000,
-        "current_distance": int(distance),
-        "mavtype": {"type": "MAV_DISTANCE_SENSOR_LASER"},
-        "id": device_id,
-        "orientation": {"type": "MAV_SENSOR_ROTATION_PITCH_270"},
-        "covariance": 0,
-        "horizontal_fov": 0,
-        "vertical_fov": 0,
-        "quaternion": [0, 0, 0, 0],
-        "signal_quality": 0,
-    }
+    # pylint: disable=too-many-locals
+    # pylint: disable=too-many-statements
+    async def drive(self, port: int) -> None:
+        """Main function"""
+        ## The time that this script was started
+        tboot = time.time()
 
+        ## Parser to decode incoming PingMessage
+        ping_parser = PingParser()
 
-# pylint: disable=too-many-locals
-# pylint: disable=too-many-statements
-async def drive(port: int) -> None:
-    """Main function"""
-    ## The time that this script was started
-    tboot = time.time()
+        ## Messages that have the current distance measurement in the payload
+        distance_messages = [PING1D_DISTANCE, PING1D_DISTANCE_SIMPLE, PING1D_PROFILE]
 
-    ## Parser to decode incoming PingMessage
-    ping_parser = PingParser()
+        ## The minimum interval time for distance updates to the autopilot
+        ping_interval_ms = 0.2
 
-    ## Messages that have the current distance measurement in the payload
-    distance_messages = [PING1D_DISTANCE, PING1D_DISTANCE_SIMPLE, PING1D_PROFILE]
+        ## The last time a distance measurement was received
+        last_distance_measurement_time = 0.0
 
-    ## The minimum interval time for distance updates to the autopilot
-    ping_interval_ms = 0.2
+        ## The last time a distance measurement was requested
+        last_ping_request_time = 0.0
 
-    ## The last time a distance measurement was received
-    last_distance_measurement_time = 0.0
+        pingserver = ("127.0.0.1", port)
 
-    ## The last time a distance measurement was requested
-    last_ping_request_time = 0.0
+        ## Send distance_sensor message to autopilot
+        async def send_distance_data(distance: int, deviceid: int, confidence: int) -> None:
+            # print("sending distance %d confidence %d" % (distance, confidence))
+            if confidence < 0.5:
+                distance = 0
+            print(f"sendind {distance}")
+            await self.mavlink2rest.send_mavlink_message(
+                distance_message(int((time.time() - tboot) * 1000), int(distance / 10), deviceid)
+            )
 
-    pingserver = ("127.0.0.1", port)
+        ## Send a request for distance_simple message to ping device
+        async def send_ping1d_request() -> None:
+            print("requesting new data")
+            data = PingMessage()
+            data.request_id = PING1D_DISTANCE_SIMPLE
+            data.src_device_id = 0
+            data.pack_msg_data()
+            ping1d_io.sendall(data.msg_data)
 
-    ## Send distance_sensor message to autopilot
-    async def send_distance_data(distance: int, deviceid: int, confidence: int) -> None:
-        # print("sending distance %d confidence %d" % (distance, confidence))
-        if confidence < 0.5:
-            distance = 0
-        print(f"sendind {distance}")
-        await mavlink2rest.send_mavlink_message(
-            distance_message(int((time.time() - tboot) * 1000), int(distance / 10), deviceid)
-        )
+        ping1d_io = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        ping1d_io.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        ping1d_io.setblocking(False)
+        ping1d_io.connect(pingserver)
 
-    ## Send a request for distance_simple message to ping device
-    async def send_ping1d_request() -> None:
-        print("requesting new data")
+        # set the ping interval once at startup
+        # the ping interval may change if another client to the pingproxy requests it
         data = PingMessage()
-        data.request_id = PING1D_DISTANCE_SIMPLE
+        data.request_id = PING1D_SET_PING_INTERVAL
         data.src_device_id = 0
+        data.ping_interval = int(ping_interval_ms * 1000)
         data.pack_msg_data()
         ping1d_io.sendall(data.msg_data)
 
-    ping1d_io = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    ping1d_io.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    ping1d_io.setblocking(False)
-    ping1d_io.connect(pingserver)
+        while True:
+            await asyncio.sleep(0.001)
+            tnow = time.time()
 
-    # set the ping interval once at startup
-    # the ping interval may change if another client to the pingproxy requests it
-    data = PingMessage()
-    data.request_id = PING1D_SET_PING_INTERVAL
-    data.src_device_id = 0
-    data.ping_interval = int(ping_interval_ms * 1000)
-    data.pack_msg_data()
-    ping1d_io.sendall(data.msg_data)
+            # request data from ping device
+            if tnow > last_distance_measurement_time + ping_interval_ms:
+                if tnow > last_ping_request_time + ping_interval_ms:
+                    last_ping_request_time = time.time()
+                    await send_ping1d_request()
 
-    while True:
-        await asyncio.sleep(0.001)
-        tnow = time.time()
+            if tnow > last_distance_measurement_time + ping_interval_ms * 10:
+                print("attempting reconnection...")
+                ping1d_io.connect(pingserver)
 
-        # request data from ping device
-        if tnow > last_distance_measurement_time + ping_interval_ms:
-            if tnow > last_ping_request_time + ping_interval_ms:
-                last_ping_request_time = time.time()
-                await send_ping1d_request()
-
-        if tnow > last_distance_measurement_time + ping_interval_ms * 10:
-            print("attempting reconnection...")
-            ping1d_io.connect(pingserver)
-
-        # read data in from ping device
-        try:
-            data, _ = ping1d_io.recvfrom(4096)
-        except socket.error as exception:
-            # check if it's waiting for data
-            if exception.errno != errno.EAGAIN:
-                raise exception
-            continue
-
-        # decode data from ping device, forward to autopilot
-        for byte in data:
+            # read data in from ping device
             try:
-                if ping_parser.parse_byte(byte) == PingParser.NEW_MESSAGE:
-                    if ping_parser.rx_msg.message_id in distance_messages:
-                        last_distance_measurement_time = time.time()
-                        distance = ping_parser.rx_msg.distance
-                        deviceid = ping_parser.rx_msg.src_device_id
-                        confidence = ping_parser.rx_msg.confidence
-                        await send_distance_data(distance, deviceid, confidence)
-            except Exception as error:
-                print(error)
+                data, _ = ping1d_io.recvfrom(4096)
+            except socket.error as exception:
+                # check if it's waiting for data
+                if exception.errno != errno.EAGAIN:
+                    raise exception
+                continue
+
+            # decode data from ping device, forward to autopilot
+            for byte in data:
+                try:
+                    if ping_parser.parse_byte(byte) == PingParser.NEW_MESSAGE:
+                        if ping_parser.rx_msg.message_id in distance_messages:
+                            last_distance_measurement_time = time.time()
+                            distance = ping_parser.rx_msg.distance
+                            deviceid = ping_parser.rx_msg.src_device_id
+                            confidence = ping_parser.rx_msg.confidence
+                            await send_distance_data(distance, deviceid, confidence)
+                except Exception as error:
+                    print(error)
