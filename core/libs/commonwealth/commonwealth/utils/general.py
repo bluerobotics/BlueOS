a4dbb695699e0a614ea850eae61d3f35ad653b4b
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-07-22 18:57:16 -0300

    commonwealth: Add function to get device-id

--- core/libs/commonwealth/commonwealth/utils/general.py
+++ core/libs/commonwealth/commonwealth/utils/general.py
@@ -1,5 +1,23 @@
 import os
 
+from loguru import logger
+
 
 def is_running_as_root() -> bool:
     return os.geteuid() == 0
+
+
+def device_id() -> str:
+    try:
+        with open("/sys/firmware/devicetree/base/serial-number", "r", encoding="utf-8") as f:
+            return f.read()
+    except Exception as error:
+        logger.exception(f"Could not get device's serial-number. {error}")
+
+    try:
+        with open("/etc/machine-id", "r", encoding="utf-8") as f:
+            return "".join(f.read().split())
+    except Exception as error:
+        logger.exception(f"Could not get device's machine-id. {error}")
+
+    raise ValueError("Could not get device id.")
