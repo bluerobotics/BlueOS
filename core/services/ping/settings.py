23266dc750cd5ab94e9a6f9c15ee9fccc33e655e
Willian Galvani <williangalvani@gmail.com>
2022-08-02 17:43:15 -0300

    create settings.py for ping service

---
+++ core/services/ping/settings.py
@@ -0,0 +1,38 @@
+from typing import Any, Dict
+
+import pykson  # type: ignore
+from commonwealth.settings import settings
+from serial.tools.list_ports_linux import SysFS
+
+class Ping1dSettingsSpecV1(pykson.JsonObject):
+    port = pykson.StringField()
+    mavlink_enabled = pykson.BooleanField()
+
+    def __str__(self):
+        return f"{self.port} - {self.mavlink_enabled}"
+
+    @staticmethod
+    def new(port: str, enabled: bool):
+        return Ping1dSettingsSpecV1(
+            port=port,
+            mavlink_enabled=enabled
+        )
+
+class SettingsV1(settings.BaseSettings):
+    VERSION = 1
+    ping1d_specs = pykson.ObjectListField(Ping1dSettingsSpecV1)
+    # no settings for ping360 as of V1
+
+    def __init__(self, *args: str, **kwargs: int) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.VERSION = SettingsV1.VERSION
+
+    def migrate(self, data: Dict[str, Any]) -> None:
+        if data["VERSION"] == SettingsV1.VERSION:
+            return
+
+        if data["VERSION"] < SettingsV1.VERSION:
+            super().migrate(data)
+
+        data["VERSION"] = SettingsV1.VERSION
