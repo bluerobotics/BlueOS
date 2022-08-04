62357d58adf050b695f85260c9b3830326f98148
Willian Galvani <williangalvani@gmail.com>
2022-08-04 13:41:16 -0300

    core: frontend: create Ping types definitions

---
+++ core/frontend/src/types/ping.ts
@@ -0,0 +1,22 @@
+export enum PingType {
+  Ping1D = 'Ping1D',
+  Ping360 = 'Ping360'
+}
+
+export interface DriverStatus {
+  udp_port: number | undefined
+  mavlink_driver_enabled: boolean
+}
+
+export interface PingDevice {
+  ping_type: PingType,
+  device_id: number,
+  device_model: number,
+  device_revision: number,
+  firmware_version_major: number,
+  firmware_version_minor: number,
+  firmware_version_patch: number,
+  port: string,
+  driver_status: DriverStatus,
+  ethernet_discovery_info: string
+}
