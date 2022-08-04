9fa698bc730ff865bb650eae8663622401175e84
Rafael Araujo Lehmkuhl <rafael.lehmkuhl93@gmail.com>
2022-08-04 18:13:25 -0300

    beacon: Warn users about usage of hotspot connection
    Make sure they know this should not be their main connection, the same way we advertise it for client-wifi connections.

--- core/services/beacon/typedefs.py
+++ core/services/beacon/typedefs.py
@@ -6,12 +6,15 @@ from pydantic import BaseModel
 class InterfaceType(str, Enum):
     WIRED = "WIRED"
     WIFI = "WIFI"
+    HOTSPOT = "HOTSPOT"
     UNKNOWN = "UNKNOWN"
 
     @staticmethod
     def guess_from_name(name: str) -> "InterfaceType":
         if name.startswith("wl"):
             return InterfaceType.WIFI
+        if name.startswith("uap"):
+            return InterfaceType.HOTSPOT
         if name.startswith("en"):
             return InterfaceType.WIRED
         if name.startswith("eth"):
