5ef2bae35faad9d5812d8f7c6b7626f108d96beb
Patrick José Pereira <patrickelectric@gmail.com>
2022-08-04 15:16:56 -0300

    core: tools: mavlink_router: Update to use latest version available
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/mavlink_router/bootstrap.sh
+++ core/tools/mavlink_router/bootstrap.sh
@@ -4,13 +4,13 @@
 set -e
 
 LOCAL_BINARY_PATH="/usr/bin/mavlink-routerd"
-VERSION="companion-core-development-0.1"
+VERSION="blueos-0.1"
 GITHUB_REMOTE="https://github.com/patrickelectric/mavlink-router"
 
 # By default we install armv6
-REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-libc-armv6"
+REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-glibc-armhf"
 if [[ "$(uname -m)" == "x86_64"* ]]; then
-    REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-libc-x86-64"
+    REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-musl-x86-64"
 fi
 
 wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
