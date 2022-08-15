b6f3af6dfbce2636f7908477059173bc4c7e9aa1
Willian Galvani <williangalvani@gmail.com>
2022-08-15 14:38:44 -0300

    core: frontend: allow importing png/jpg images

--- core/frontend/src/types/shims-general.ts
+++ core/frontend/src/types/shims-general.ts
@@ -3,6 +3,16 @@ declare module '*.svg' {
   export default value
 }
 
+declare module '*.jpg' {
+  const value: string
+  export default value
+}
+
+declare module '*.png' {
+  const value: string
+  export default value
+}
+
 declare module 'vue-tooltip-directive'
 
 declare module '@/assets/colors/default'
