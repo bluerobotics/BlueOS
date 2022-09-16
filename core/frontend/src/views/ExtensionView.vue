ffd4f6a473cb8c2a9689bfddc1f9038fee6b0d40
Willian Galvani <williangalvani@gmail.com>
2022-09-16 11:54:11 -0300

    Create ExtensionView, show extensions in Iframe

---
+++ core/frontend/src/views/ExtensionView.vue
@@ -0,0 +1,23 @@
+<template>
+  <Iframe
+    :source="service_path"
+  />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import Iframe from '@/components/utils/iframe.vue'
+
+export default Vue.extend({
+  name: 'ExtensionView',
+  components: {
+    Iframe,
+  },
+  data() {
+    return {
+      service_path: `${window.location.protocol}//${window.location.hostname}:${this.$route.params.port}`,
+    }
+  },
+})
+</script>
