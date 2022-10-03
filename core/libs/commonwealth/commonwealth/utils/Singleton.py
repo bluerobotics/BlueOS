d162c066bd4614108d04af27b35ffcdb79329647
ES-Alexander <sandman.esalexander@gmail.com>
2022-10-03 12:08:41 +1100

    commonwealth: singleton: use parameterless super

--- core/libs/commonwealth/commonwealth/utils/Singleton.py
+++ core/libs/commonwealth/commonwealth/utils/Singleton.py
@@ -6,5 +6,5 @@ class Singleton(type):
 
     def __call__(cls: Any, *args: Any, **kwargs: Any) -> Any:
         if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+            cls._instances[cls] = super().__call__(*args, **kwargs)
         return cls._instances[cls]
