9d1544b3d150bf679eb4fce0ab99df6642fa2256
Willian Galvani <williangalvani@gmail.com>
2022-10-17 00:10:50 -0300

    core: frontend: Add a progress bar to docker pull operations
    Co-authored-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/utils/pull_tracker.ts
+++ core/frontend/src/utils/pull_tracker.ts
@@ -3,9 +3,14 @@ import { Dictionary } from '@/types/common'
 class PullTracker {
     private layer_status: Dictionary<string> = {}
 
+    // We need to assume a size for the missing layers, we are assuming 100mb for now.
+    private guesstimated_size = 100 * 1024 * 1024
+
     private layers: string[] = []
 
     private layer_progress: Dictionary<string> = {}
+
+    private layer_progress_detail: Dictionary<{total: number, current: number}> = {}
 
     private left_over_data = ''
 
@@ -15,8 +20,61 @@ class PullTracker {
 
     overall_status = ''
 
+    download_percentage = 0
+
+    extraction_percentage = 0
+
     constructor(ready_callback: () => void) {
       this.onready = ready_callback
+    }
+
+    updateSimplifiedProgress(): void {
+      let download_total = 0
+      let download_current = 0
+      let extraction_total = 0
+      let extraction_current = 0
+      for (const id of Object.keys(this.layer_status)) {
+        if (this.layer_status[id] === 'Already exists') {
+          continue
+        }
+        switch (this.layer_status[id]) {
+          case 'Waiting':
+            download_total += this.guesstimated_size
+            extraction_total += this.guesstimated_size
+            break
+
+          case 'Downloading':
+            download_total += this.layer_progress_detail[id].total
+            download_current += this.layer_progress_detail[id].current
+            extraction_total += this.layer_progress_detail[id].total
+            break
+
+          case 'Pull complete':
+            download_total += this.layer_progress_detail[id].total
+            download_current += this.layer_progress_detail[id].total
+            extraction_total += this.layer_progress_detail[id].total
+            extraction_current += this.layer_progress_detail[id].total
+            break
+
+          case 'Download complete':
+            download_total += this.layer_progress_detail[id]?.total ?? 0
+            download_current += this.layer_progress_detail[id]?.total ?? 0
+            extraction_total += this.layer_progress_detail[id]?.total ?? 0
+            break
+
+          case 'Extracting':
+            download_total += this.layer_progress_detail[id].total
+            download_current += this.layer_progress_detail[id].total
+            extraction_total += this.layer_progress_detail[id].total
+            extraction_current += this.layer_progress_detail[id].current
+            break
+          default:
+            // some data is not consumed
+            break
+        }
+      }
+      this.download_percentage = download_current / download_total / 0.01
+      this.extraction_percentage = extraction_current / extraction_total / 0.01
     }
 
     digestNewData(progressEvent: any): void {
@@ -42,6 +100,9 @@ class PullTracker {
             if ('status' in data) {
               this.layer_status[id] = data.status
             }
+            if (data?.progressDetail?.total !== undefined) {
+              this.layer_progress_detail[id] = data.progressDetail
+            }
           } else {
             this.overall_status = data.status
             // Axios returns the promise too early (before the pull is done)
@@ -63,6 +124,8 @@ class PullTracker {
         + `  ${this.layer_progress[image] || ''}\n`
       })
       this.pull_output = `${this.pull_output}${this.overall_status}\n`
+
+      this.updateSimplifiedProgress()
     }
 }
 export default PullTracker
