c6c5b6d85f1b2537cec5ca98147d4a7d1dd4f16d
Patrick José Pereira <patrickelectric@gmail.com>
2022-10-21 16:35:03 -0300

    install: boards: bcm*: Enable cgroup for memory and cpu
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- install/boards/bcm_28xx.sh
+++ install/boards/bcm_28xx.sh
@@ -2,6 +2,8 @@
 
 echo "Configuring BCM28XX board (Raspberry Pi zero, 1, 2, 3).."
 
+CMDLINE_FILE=/boot/cmdline.txt
+
 # Remove any configuration related to i2c and spi/spi1 and do the necessary changes for navigator
 echo "- Enable I2C, SPI and UART."
 for STRING in "dtparam=i2c_arm=" "dtparam=spi=" "dtoverlay=spi1" "dtoverlay=uart1"; do
@@ -28,4 +30,11 @@ done
 
 # Remove any console serial configuration
 echo "- Configure serial."
-sudo sed -e 's/console=serial[0-9],[0-9]*\ //' -i /boot/cmdline.txt
+sudo sed -e 's/console=serial[0-9],[0-9]*\ //' -i $CMDLINE_FILE
+
+# Set cgroup, necessary for docker access to memory information
+echo "- Enable cgroup with memory and cpu"
+grep -q cgroup $CMDLINE_FILE || (
+    truncate --size -1 $CMDLINE_FILE
+    echo " cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" >> $CMDLINE_FILE
+)
