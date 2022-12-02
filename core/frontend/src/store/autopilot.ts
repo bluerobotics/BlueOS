73a9b5102e2ee3986233a34e29c15b23876402ae
Patrick José Pereira <patrickelectric@gmail.com>
2022-12-02 13:32:20 -0300

    core: frontend: store: autopilot: Track when parameter metadata loads
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/store/autopilot.ts
+++ core/frontend/src/store/autopilot.ts
@@ -28,11 +28,14 @@ class AutopilotStore extends VuexModule {
 
   finished_loading = false
 
+  metadata_loaded = false
+
   @Mutation
   reset(): void {
     this.parameters = []
     parameterFetcher.reset()
     this.finished_loading = false
+    this.metadata_loaded = false
     this.parameters_loaded = 0
     this.parameters_total = 0
   }
@@ -43,15 +46,21 @@ class AutopilotStore extends VuexModule {
   }
 
   @Mutation
+  setMetadataLoaded(loaded: boolean): void {
+    this.metadata_loaded = loaded
+    this.finished_loading = this.parameters_loaded >= this.parameters_total && this.metadata_loaded
+  }
+
+  @Mutation
   setLoadedParametersCount(count: number): void {
     this.parameters_loaded = count
-    this.finished_loading = this.parameters_loaded >= this.parameters_total
+    this.finished_loading = this.parameters_loaded >= this.parameters_total && this.metadata_loaded
   }
 
   @Mutation
   setTotalParametersCount(count: number): void {
     this.parameters_total = count
-    this.finished_loading = this.parameters_loaded >= this.parameters_total
+    this.finished_loading = this.parameters_loaded >= this.parameters_total && this.metadata_loaded
   }
 }
 
