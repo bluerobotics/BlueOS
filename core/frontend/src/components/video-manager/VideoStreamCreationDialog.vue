ebf363e8bcb86f8b54a1c8f6b1d1086d5c9dfdb7
Patrick José Pereira <patrickelectric@gmail.com>
2023-01-12 17:27:34 -0300

    core: frontend: VideoStreamCreationDialog: Move from 'Encode' to 'Encoding'
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/video-manager/VideoStreamCreationDialog.vue
+++ core/frontend/src/components/video-manager/VideoStreamCreationDialog.vue
@@ -24,7 +24,7 @@
           <v-select
             v-model="selected_encode"
             :items="available_encodes"
-            label="Encode"
+            label="Encoding"
             required
           />
           <v-select
