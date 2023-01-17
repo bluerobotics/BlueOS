4ba889136b0c52009e3610b7ac2c094a5946ec27
Patrick José Pereira <patrickelectric@gmail.com>
2023-01-16 21:46:09 -0300

    core: frontend: speedtest: Graph: Remove controls
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/speedtest/Graph.vue
+++ core/frontend/src/components/speedtest/Graph.vue
@@ -28,6 +28,9 @@ export default Vue.extend({
           animations: {
             enabled: false,
           },
+          toolbar: {
+            show: false,
+          },
         },
         animate: false,
         xaxis: {
