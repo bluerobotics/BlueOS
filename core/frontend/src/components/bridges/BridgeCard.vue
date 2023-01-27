723b05fd2bbefbe05ff4272c4cb6842b0fcc22af
Willian Galvani <williangalvani@gmail.com>
2023-01-27 12:30:25 -0300

    core: frontend: rename bridge_serial_ports to make linter happy

--- core/frontend/src/components/bridges/BridgeCard.vue
+++ core/frontend/src/components/bridges/BridgeCard.vue
@@ -6,7 +6,7 @@
     <v-card-text class="pa-4">
       <v-tooltip
         top
-        :disabled="!(bridge_serial_info.serial_info && bridge_serial_info.serial_info.by_path)"
+        :disabled="!(bridgeSerialInfo.serial_info && bridgeSerialInfo.serial_info.by_path)"
       >
         <template #activator="{ on, attrs }">
           <v-container
@@ -19,26 +19,26 @@
                 class="d-flex align-center pa-0 justify-center"
               >
                 <span class="text-subtitle-1 mr-2">
-                  {{ get_display_name(bridge_serial_info) }}:{{ bridge_serial_info.bridge.baud }}
+                  {{ get_display_name(bridgeSerialInfo) }}:{{ bridgeSerialInfo.bridge.baud }}
                 </span>
                 <v-icon>mdi-link</v-icon>
                 <span class="text-subtitle-1 ml-2">
-                  {{ bridge_serial_info.bridge.ip }}:{{ bridge_serial_info.bridge.udp_port }}
+                  {{ bridgeSerialInfo.bridge.ip }}:{{ bridgeSerialInfo.bridge.udp_port }}
                 </span>
               </v-card-text>
             </v-card>
           </v-container>
         </template>
-        <span v-if="bridge_serial_info.serial_info">
+        <span v-if="bridgeSerialInfo.serial_info">
           <div class="d-flex flex-column">
             <p class="subtitle-1 text-center ma-0">
-              Path: {{ bridge_serial_info.serial_info.by_path }}
+              Path: {{ bridgeSerialInfo.serial_info.by_path }}
             </p>
             <p class="subtitle-1 text-center ma-0">
               Info:
-              {{ bridge_serial_info.serial_info.udev_properties["ID_VENDOR"] }}
+              {{ bridgeSerialInfo.serial_info.udev_properties["ID_VENDOR"] }}
               /
-              {{ bridge_serial_info.serial_info.udev_properties["ID_MODEL"] }}
+              {{ bridgeSerialInfo.serial_info.udev_properties["ID_MODEL"] }}
             </p>
           </div></span>
       </v-tooltip>
@@ -73,14 +73,14 @@ const notifier = new Notifier(bridget_service)
 export default Vue.extend({
   name: 'BridgeCard',
   props: {
-    bridge_serial_info: {
+    bridgeSerialInfo: {
       type: Object as PropType<BridgeWithSerialInfo>,
       required: true,
     },
   },
   methods: {
-    get_display_name(bridge_serial_info: BridgeWithSerialInfo): string {
-      return bridge_serial_info.serial_info?.name ?? bridge_serial_info.bridge.serial_path
+    get_display_name(bridgeSerialInfo: BridgeWithSerialInfo): string {
+      return bridgeSerialInfo.serial_info?.name ?? bridgeSerialInfo.bridge.serial_path
     },
     async removeBridge(): Promise<void> {
       bridget.setUpdatingBridges(true)
@@ -88,7 +88,7 @@ export default Vue.extend({
         method: 'delete',
         url: `${bridget.API_URL}/bridges`,
         timeout: 10000,
-        data: this.bridge_serial_info.bridge,
+        data: this.bridgeSerialInfo.bridge,
       })
         .catch((error) => {
           notifier.pushBackError('BRIDGE_DELETE_FAIL', error)
