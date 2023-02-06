4e54ca21386ed9f48f4d00c3a76ddf39d3a8596f
Patrick José Pereira <patrickelectric@gmail.com>
2023-02-06 12:48:42 -0300

    core: frontend: components: Add new ThemeTrayMenu
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/components/app/ThemeTrayMenu.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-icon
+    height="100%"
+    color="white"
+    title="Change theme between dark and light mode."
+    @click="toggleDarkTheme"
+  >
+    {{ settings.is_dark_theme ? 'mdi-weather-night' : 'mdi-white-balance-sunny' }}
+  </v-icon>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import settings from '@/libs/settings'
+
+export default Vue.extend({
+  name: 'ThemeTrayMenu',
+  data: () => ({
+    settings,
+  }),
+  methods: {
+    toggleDarkTheme(): void {
+      settings.is_dark_theme = !settings.is_dark_theme
+    },
+  },
+})
+</script>
