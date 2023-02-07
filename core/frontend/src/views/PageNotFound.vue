57ae6d3072453e21ac863ae564c380a147466412
Patrick José Pereira <patrickelectric@gmail.com>
2023-02-07 16:48:00 -0300

    core: frontend: views: Add PageNotFound
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/views/PageNotFound.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="d-flex flex-column align-center">
+    <v-icon
+      style="height: 400px"
+      size="256"
+      v-text="'mdi-sail-boat-sink'"
+    />
+    <div
+      class="text-h6"
+      :v-text="'Vehicle not found.'"
+    />
+    <p class="text-h6 text-center ma-4">
+      Page not found!
+    </p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'NoneViewer',
+})
+</script>
