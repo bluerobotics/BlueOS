4f2b6bb43d1f8e93b5c4d42ecff7f342d6ae98c6
Patrick José Pereira <patrickelectric@gmail.com>
2023-02-08 14:16:00 -0300

    core: frontend: utils: Use new mavlink2rest submodule
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/utils/mavlink_prettifier.ts
+++ core/frontend/src/utils/mavlink_prettifier.ts
@@ -1,25 +1,27 @@
+import { Message } from '@/libs/MAVLink2Rest/mavlink2rest-ts/messages/mavlink2rest'
+import { Message as M2R } from '@/libs/MAVLink2Rest/mavlink2rest-ts/messages/mavlink2rest-message'
 import { Dictionary } from '@/types/common'
 
 const formatters = {
-  AHRS2(message: any) {
+  AHRS2(message: M2R.Ahrs2) {
     return `Roll: ${message.roll.toFixed(2)} rad, Pitch: `
     + `${message.pitch.toFixed(2)} rad, Yaw: ${message.yaw.toFixed(2)} rad`
   },
-  ATTITUDE(message: any): string {
+  ATTITUDE(message: M2R.Attitude): string {
     return `Roll: ${message.roll.toFixed(2)} rad, Pitch: `
     + `${message.pitch.toFixed(2)} rad, Yaw: ${message.yaw.toFixed(2)} rad`
   },
-  BATTERY_STATUS(message: any) {
+  BATTERY_STATUS(message: M2R.BatteryStatus) {
     return `${message.voltages[0] / 1000} V ${message.current_consumed} mAh consumed`
   },
-  CAMERA_INFORMATION(message: any) {
-    function byteArrayToString(array: [number]): string {
+  CAMERA_INFORMATION(message: M2R.CameraInformation) {
+    function byteArrayToString(array: number[]): string {
       return array
         .filter((value: number) => value !== 0)
         .map((value: number) => String.fromCharCode(value))
         .join('')
     }
-    function removeNullFromCharArray(array: [string]): string {
+    function removeNullFromCharArray(array: string[]): string {
       return array
         .filter((value) => value !== '\x00')
         .join('')
@@ -28,11 +30,11 @@ const formatters = {
     const definition_url = removeNullFromCharArray(message.cam_definition_uri)
     return `vendor: ${vendor_name}, definition_url: ${definition_url}`
   },
-  COMMAND_ACK(message: any) {
+  COMMAND_ACK(message: M2R.CommandAck) {
     return `${message.command.type.replace('MAV_CMD_', '')}`
       + ` - ${message.result.type.replace('MAV_RESULT_', '')}`
   },
-  COMMAND_LONG(message: any) {
+  COMMAND_LONG(message: M2R.CommandLong) {
     function getParameters(): string {
       return Object.getOwnPropertyNames(message)
         .filter((name) => name.includes('param'))
@@ -42,27 +44,27 @@ const formatters = {
     return `${message.command.type.replace('MAV_CMD_', '')}`
       + ` - ${getParameters()}`
   },
-  GLOBAL_POSITION_INT(message: any) {
+  GLOBAL_POSITION_INT(message: M2R.GlobalPositionInt) {
     return `Lat: ${message.lat.toFixed(5)} Lon: ${message.lon.toFixed(5)}`
   },
-  HEARTBEAT(message: any) {
+  HEARTBEAT(message: M2R.Heartbeat) {
     return `${message.mavtype.type}`
   },
-  NAMED_VALUE_FLOAT(message: any) {
+  NAMED_VALUE_FLOAT(message: M2R.NamedValueFloat) {
     return `${message.name.join('')} = ${message.value.toFixed(2)}`
   },
-  PARAM_VALUE(message: any) {
+  PARAM_VALUE(message: M2R.ParamValue) {
     return `${message.param_id.join('')}: ${message.param_value}`
   },
-  STATUSTEXT(message: any): string {
+  STATUSTEXT(message: M2R.Statustext): string {
     return `${message.severity.type.replace('MAV_SEVERITY_', '')}: ${message.text.join('')}`
   },
-  SYS_STATUS(message: any) {
+  SYS_STATUS(message: M2R.SysStatus) {
     return `Batt: (${message.current_battery / 100} A, ${message.voltage_battery / 1000} V)`
   },
-} as Dictionary<(message: any) => string>
+} as Dictionary<(message: unknown) => string>
 
-export default function prettify(message: any): string {
+export default function prettify(message: Message): string {
   if (message.type === undefined) {
     return 'N/A'
   }
