977d93946145abbe473b34e9417a61cbb9e08efa
Patrick José Pereira <patrickelectric@gmail.com>
2023-02-09 19:59:38 -0300

    core: frontend: Update project to use latest eslint libraries
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
   },
   plugins: [
     'html',
+    'import',
     'simple-import-sort',
     'vue',
   ],
@@ -49,6 +50,7 @@ module.exports = {
     ],
     'no-param-reassign': 'off',
     'no-shadow': 'off',
+    'no-use-before-define': 'off',
     'no-useless-constructor': 'off',
     semi: ['error', 'never'],
     'simple-import-sort/exports': 'error',
@@ -57,12 +59,43 @@ module.exports = {
     '@typescript-eslint/no-useless-constructor': ['error'],
     '@typescript-eslint/no-shadow': ['error'],
     '@typescript-eslint/explicit-function-return-type': ['error', { allowExpressions: true }],
+    // Need to disable base rule to apply pattern of unused variables
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    // Disable to avoid conflicts with eslint max-len
+    'vue/max-len': 'off',
+    'vue/multi-word-component-names': 'off',
     'vue/no-unused-properties': ['error', {
       groups: ['props', 'data', 'computed', 'methods', 'setup'],
       deepData: true,
       ignorePublicMembers: false,
     }],
+    // We don't care about `noopener noreferrer`
+    'vue/no-template-target-blank': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
     'vue/valid-v-slot': ['error', { allowModifiers: true }],
+    // Disable accessibility checks
+    'vuejs-accessibility/alt-text': 'off',
+    'vuejs-accessibility/anchor-has-content': 'off',
+    'vuejs-accessibility/mouse-events-have-key-events': 'off',
     'no-await-in-loop': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: './tsconfig.json',
+      },
+      node: {
+        extensions: ['.js', '.json', '.jsx', '.ts', '.tsx', '.vue'],
+      },
+    },
   },
 }
