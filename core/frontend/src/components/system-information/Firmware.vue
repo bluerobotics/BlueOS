5f415f04fa5067576e46c90fc65bda2b967805bb
Patrick José Pereira <patrickelectric@gmail.com>
2023-03-02 14:39:26 -0300

    core: frontend: components: system-information: Firmware: Add 'Up to date' and 'latest' information
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/system-information/Firmware.vue
+++ core/frontend/src/components/system-information/Firmware.vue
@@ -6,7 +6,7 @@
   >
     <v-card
       class="ma-2 pa-2 card_style"
-      :loading="vcgencmd === undefined"
+      :loading="loading_vcgencmd"
     >
       <v-card-title class="align-center">
         Raspberry Firmware
@@ -15,7 +15,7 @@
         <div v-if="raspberry_firmware_error">
           <span><b> {{ raspberry_firmware_error }} </b> </span>
         </div>
-        <div v-else>
+        <div v-else-if="!loading_vcgencmd">
           <span class="d-block"><b>From:</b> {{ raspiberry_firmware_data }} </span>
           <span class="d-block"><b>Version:</b> {{ raspberry_firmware_version }} </span>
         </div>
@@ -24,35 +24,36 @@
 
     <v-card
       class="ma-2 pa-2 card_style"
-      :loading="vcgencmd === undefined"
+      :loading="loading"
     >
       <v-card-title class="align-center">
         Bootloader Version
       </v-card-title>
       <v-card-text>
-        <div v-if="raspberry_bootloader_error">
-          <span><b> {{ raspberry_bootloader_error }} </b> </span>
+        <div v-if="eeprom_update">
+          <div class="d-flex justify-space-between">
+            <span><b>From:</b> {{ raspberry_eeprom_data.current_bootloader }} </span>
+            <span v-if="raspberry_eeprom_data.current_bootloader !== raspberry_eeprom_data.latest_bootloader">
+              <b>Latest:</b> {{ raspberry_eeprom_data.latest_bootloader }}
+            </span>
+            <span v-else>
+              <b>Up to date</b>
+            </span>
+          </div>
+          <span v-if="raspberry_bootloader_version" class="d-block">
+            <b>Version:</b> {{ raspberry_bootloader_version }}
+          </span>
         </div>
-        <div v-else>
-          <span class="d-block"><b>From:</b> {{ raspiberry_bootloader_data }} </span>
-          <span class="d-block"><b>Version:</b> {{ raspberry_bootloader_version }} </span>
-        </div>
-      </v-card-text>
-    </v-card>
-
-    <v-card
-      class="ma-2 pa-2 card_style"
-      :loading="vcgencmd === undefined"
-    >
-      <v-card-title class="align-center">
-        VL085 Version (USB Controller)
-      </v-card-title>
-      <v-card-text>
-        <div v-if="raspberry_vl085_error">
-          <span><b> {{ raspberry_vl085_error }} </b> </span>
-        </div>
-        <div v-else>
-          <span class="d-block"><b>Version:</b> {{ raspberry_vl085_version }} </span>
+        <div v-if="eeprom_update">
+          <div class="d-flex justify-space-between mb-3">
+            <span><b>VL085 Firmware (USB Controller):</b> {{ raspberry_eeprom_data.current_vl085 }} </span>
+            <span v-if="raspberry_eeprom_data.current_vl085 !== raspberry_eeprom_data.latest_vl085">
+              <b>Latest:</b> {{ raspberry_eeprom_data.latest_vl085 }}
+            </span>
+            <span v-else>
+              <b>Up to date</b>
+            </span>
+          </div>
         </div>
       </v-card-text>
     </v-card>
@@ -70,14 +71,28 @@ interface ReturnStruct {
   return_code: number
 }
 
+interface EepromUpdateStruct {
+  latest_bootloader: string | undefined
+  current_bootloader: string | undefined
+  latest_vl085: string | undefined
+  current_vl085: string | undefined
+}
+
 export default Vue.extend({
   name: 'Processes',
   data() {
     return {
+      eeprom_update: undefined as undefined | ReturnStruct,
       vcgencmd: undefined as undefined | Record<string, ReturnStruct>,
     }
   },
   computed: {
+    loading_vcgencmd(): boolean {
+      return this.vcgencmd === undefined
+    },
+    loading(): boolean {
+      return this.eeprom_update === undefined || this.vcgencmd === undefined
+    },
     raspiberry_firmware(): string | undefined {
       return this.cleanUpString(this.vcgencmd?.firmware?.stdout)
     },
@@ -99,11 +114,6 @@ export default Vue.extend({
     raspiberry_bootloader(): string | undefined {
       return this.cleanUpString(this.vcgencmd?.bootloader?.stdout)
     },
-    raspiberry_bootloader_data(): string | undefined {
-      return this.raspiberry_bootloader
-        ?.split('\n')
-        ?.[0]
-    },
     raspberry_bootloader_version(): string | undefined {
       return this.raspiberry_bootloader
         ?.split('\n')
@@ -111,21 +121,43 @@ export default Vue.extend({
         ?.split('version ')
         ?.[1]
     },
-    raspberry_bootloader_error(): string | undefined {
-      return this.formatError(this.vcgencmd?.bootloader)
-    },
-    raspberry_vl085_version(): string | undefined {
-      return this.cleanUpString(this.vcgencmd?.vl085?.stdout)
-        ?.split('\n')
-        ?.find((x) => x.includes('28:'))
-        ?.substring(3)
-    },
-    raspberry_vl085_error(): string | undefined {
-      return this.formatError(this.vcgencmd?.vl085)
+    raspberry_eeprom_data(): EepromUpdateStruct {
+      const lines = this.cleanUpString(this.eeprom_update?.stdout)?.split('\n') ?? []
+
+      const current = []
+      const latest = []
+
+      for (const line of lines) {
+        if (!line.includes(':')) {
+          continue
+        }
+
+        let [key, value] = line.split(':')
+        key = key.trim()
+        value = value.trim()
+
+        switch (key) {
+          case 'CURRENT':
+            current.push(value)
+            break
+          case 'LATEST':
+            latest.push(value)
+            break
+          default:
+            continue
+        }
+      }
+
+      return {
+        latest_bootloader: latest?.[0],
+        current_bootloader: current?.[0],
+        latest_vl085: latest?.[1],
+        current_vl085: current?.[1],
+      }
     },
   },
   async mounted() {
-    await this.getVcgencmd()
+    await this.getData()
   },
   methods: {
     cleanUpString(text: string | undefined): string | undefined {
@@ -141,14 +173,14 @@ export default Vue.extend({
       }
       return this.cleanUpString(output?.stdout)
     },
-    async getVcgencmd(): Promise<void> {
-      await back_axios({
-        url: '/commander/v1.0/raspi/vcgencmd?',
+    async getData(): Promise<void> {
+      const vcgencmd = back_axios({
+        url: '/commander/v1.0/raspi/vcgencmd',
         method: 'get',
         params: {
           i_know_what_i_am_doing: true,
         },
-        timeout: 10000,
+        timeout: 20000,
       })
         .then((response) => {
           this.vcgencmd = response.data
@@ -162,6 +194,29 @@ export default Vue.extend({
           console.warn('Failed to fetch vcgencmd info:')
           console.warn(error)
         })
+
+      const eeprom = back_axios({
+        url: '/commander/v1.0/raspi/eeprom_update',
+        method: 'get',
+        params: {
+          i_know_what_i_am_doing: true,
+        },
+        timeout: 20000,
+      })
+        .then((response) => {
+          this.eeprom_update = response.data
+        })
+        .catch((error) => {
+          // Connection lost/timeout, normal when we are turning off/rebooting
+          if (error.code === 'ECONNABORTED') {
+            return
+          }
+
+          console.warn('Failed to fetch eeprom info:')
+          console.warn(error)
+        })
+
+      Promise.all([vcgencmd, eeprom])
     },
   },
 })
