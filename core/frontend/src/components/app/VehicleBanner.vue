78c98ad9b61318e55e5db769a452f7bc3ff7cc66
Willian Galvani <williangalvani@gmail.com>
2023-04-07 18:28:58 -0300

    core: frontend: create and use imagepicker

--- core/frontend/src/components/app/VehicleBanner.vue
+++ core/frontend/src/components/app/VehicleBanner.vue
@@ -1,29 +1,33 @@
 <template>
   <div>
-    <SpinningLogo v-if="!loading_done" size="5%" />
-    <div v-if="loading_done" id="vehicle-banner" class="d-flex justify-space-between mb-6">
-      <!-- TODO: create an imagePicker widget -->
-      <v-img src="/userdata/images/vehicle.png" contain class="banner pa-1" />
+    <div id="vehicle-banner" class="d-flex align-center justify-space-between mb-6">
+      <image-picker
+        directory="/userdata/images/vehicle"
+        readonly-directory="/vehicles/images"
+        :default-image="require('@/assets/vehicles/images/unknown.svg')"
+        :image="vehicle_image"
+        @image-selected="save_vehicle_image"
+      />
       <p id="vehicle-name" class="pa-4">
         {{ vehicle_name }}
+        <v-btn
+          class="mx-2 edit-icon"
+          fab
+          dark
+          x-small
+          @click="openDialog"
+        >
+          <v-icon>
+            mdi-pencil
+          </v-icon>
+        </v-btn>
       </p>
-      <!-- TODO: create an imagePicker widget -->
-      <v-img
-        src="../../assets/img/blue-robotics-logo.svg"
-        contain
-        class="banner pa-1"
+      <image-picker
+        directory="/userdata/images/logo"
+        :default-image="require('@/assets/img/blue-robotics-logo.svg')"
+        :image="logo_image"
+        @image-selected="save_logo"
       />
-      <v-btn
-        class="mx-2 edit-icon"
-        fab
-        dark
-        x-small
-        @click="openDialog"
-      >
-        <v-icon>
-          mdi-pencil
-        </v-icon>
-      </v-btn>
     </div>
     <v-dialog v-if="dialog" v-model="dialog" max-width="500px">
       <v-card>
@@ -58,63 +62,66 @@ import Vue from 'vue'
 import bag from '@/store/bag'
 import beacon from '@/store/beacon'
 
-import SpinningLogo from '../common/SpinningLogo.vue'
-
-const API_URL = '/beacon/v1.0'
+import ImagePicker from './ImagePicker.vue'
 
 export default Vue.extend({
   name: 'VehicleBanner',
   components: {
-    SpinningLogo,
+    ImagePicker,
   },
   data() {
     return {
-      vehicle_name: '',
-      loading_done: false,
       vehicle_name_input: '',
+      vehicle_image: undefined as string | undefined,
+      logo_image: null as string | null,
       mdns_hostname_input: '',
       dialog: false,
     }
   },
   computed: {
     vehicle_name() {
-      return beacon.vehicle_name || 'My Vehicle'
+      return beacon.vehicle_name
     },
     mdns_hostname() {
       return beacon.hostname || 'blueos'
     },
   },
   mounted() {
-    this.load_name()
+    beacon.registerBeaconListener(this)
+    this.load_vehicle_image()
+    this.vehicle_name_input = this.vehicle_name || 'My Vehicle'
+    this.mdns_hostname_input = this.mdns_hostname
+    this.load_company_logo()
   },
   methods: {
-    load_name() {
-      back_axios({
-        method: 'get',
-        url: `${API_URL}/vehicle_name`,
-      }).then((response) => {
-        this.vehicle_name = response.data
-        this.vehicle_name_input = this.vehicle_name
-        this.loading_done = true
-      })
+    async load_vehicle_image() {
+      this.vehicle_image = (await bag.getData('vehicle.image_path'))?.url as string
+    },
+    async load_company_logo() {
+      this.logo_image = (await bag.getData('vehicle.logo_image_path'))?.url as string
     },
     save() {
       this.save_name()
       this.save_mdns()
-      this.loading_done = false
       this.dialog = false
+    },
+    save_logo(image: string) {
+      this.logo_image = image
+      bag.setData('vehicle.logo_image_path', {
+        url: image,
+      })
+    },
+    save_vehicle_image(image: string) {
+      this.vehicle_image = image
+      bag.setData('vehicle.image_path', {
+        url: image,
+      })
     },
     save_name() {
       beacon.setVehicleName(this.vehicle_name_input)
     },
     save_mdns() {
-      if (this.mdns_hostname_input === '') {
-        return
-      }
-      back_axios({
-        method: 'post',
-        url: `${API_URL}/hostname?hostname=${this.mdns_hostname_input}`,
-      })
+      beacon.setHostname(this.mdns_hostname_input)
     },
     openDialog() {
       this.dialog = true
@@ -123,30 +130,23 @@ export default Vue.extend({
 })
 </script>
 <style scoped>
-  .banner {
-    display: inline-flex;
-    height: 50px;
-    width: 50px;
-    margin: 0px;
-    object-fit: contain;
-  }
-  #vehicle-banner:hover .edit-icon{
+  #vehicle-name:hover .edit-icon{
     display: inline-flex;
   }
 
   #vehicle-banner {
-    position: relative;
     margin-bottom: 0 !important;
     margin-top: 3px;
   }
 
   #vehicle-name {
     margin-bottom: 0 !important;
+    position: relative;
   }
   .edit-icon {
     display: none;
-    position: absolute;
     right: 0;
     bottom: 0;
-}
+    position: absolute;
+  }
 </style>
