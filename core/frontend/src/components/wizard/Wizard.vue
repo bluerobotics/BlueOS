40c4523b3b0d65d33d16e5f5e028c6ebe6f09984
Patrick José Pereira <patrickelectric@gmail.com>
2023-04-08 15:58:45 -0300

    core: frontend: components: Wizard: Add first version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/components/wizard/Wizard.vue
@@ -0,0 +1,207 @@
+<template>
+  <v-dialog
+    v-model="should_open"
+    transition="dialog-top-transition"
+    width="600"
+    height="400"
+    persistent
+  >
+    <v-card>
+      <v-stepper v-model="step_number">
+        <v-stepper-header>
+          <v-stepper-step :complete="step_number > 1" step="1">
+            Choose Vehicle
+          </v-stepper-step>
+
+          <v-divider />
+
+          <v-stepper-step :complete="step_number > 2" step="2">
+            Configuration
+          </v-stepper-step>
+
+          <v-divider />
+
+          <v-stepper-step step="3" :complete="is_ok">
+            Done
+          </v-stepper-step>
+        </v-stepper-header>
+
+        <v-stepper-items>
+          <v-stepper-content step="1">
+            <div class="d-flex justify-space-between">
+              <model-viewer
+                :src="sub_model"
+                :auto-rotate="false"
+                camera-orbit="45deg 75deg 1.5m"
+                shadow-intensity="0.3"
+                interaction-prompt="none"
+                class="model-button ma-2"
+                @click="setupROV"
+              />
+
+              <model-viewer
+                :src="boat_model"
+                :auto-rotate="false"
+                camera-orbit="-45deg 75deg 2.5m"
+                shadow-intensity="0.3"
+                interaction-prompt="none"
+                class="model-button ma-2"
+                @click="setupBoat"
+              />
+            </div>
+          </v-stepper-content>
+
+          <v-stepper-content step="2">
+            <div class="d-flex flex-column align-center">
+              <v-text-field v-model="vehicle_name" label="Vehicle Name" />
+              <v-text-field v-model="mdns_name" label="MDNS Name" />
+            </div>
+
+            <v-alert :value="configuration_failed" type="error">
+              {{ error_message }}
+            </v-alert>
+
+            <v-row class="pa-5">
+              <v-btn
+                color="warning"
+                :loading="wait_configuration"
+                @click="step_number = 1"
+              >
+                Return
+              </v-btn>
+              <v-spacer />
+              <v-btn
+                color="primary"
+                :loading="wait_configuration"
+                @click="setupConfiguration"
+              >
+                Continue
+              </v-btn>
+            </v-row>
+          </v-stepper-content>
+
+          <v-stepper-content step="3">
+            <v-alert :value="true" type="success">
+              Your vehicle is ready to use!
+            </v-alert>
+            <v-row class="pa-5">
+              <v-spacer />
+              <v-btn color="primary" @click="should_open = false">
+                Close
+              </v-btn>
+            </v-row>
+          </v-stepper-content>
+        </v-stepper-items>
+      </v-stepper>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+import '@google/model-viewer/dist/model-viewer'
+
+import beacon from '@/store/beacon'
+import back_axios, { backend_offline_error } from '@/utils/api'
+
+const API_URL = '/bag/v1.0'
+const WIZARD_VERSION = 1
+
+const models = require.context(
+  '/src/assets/vehicles/models/',
+  true,
+  /\.(glb|json)$/,
+)
+
+export default {
+  name: 'Wizard',
+  data() {
+    return {
+      boat_model: models('./boat/UNDEFINED.glb'),
+      configuration_failed: false,
+      error_message: 'The operation failed!',
+      is_ok: false,
+      mdns_name: 'blueos',
+      should_open: false,
+      step_number: 1,
+      sub_model: models('./bluerov.glb'),
+      vehicle_name: 'blueos',
+      wait_configuration: false,
+    }
+  },
+  mounted() {
+    back_axios({
+      method: 'get',
+      url: `${API_URL}/get/wizard`,
+      timeout: 10000,
+    })
+      .then((response) => {
+        const wizard = response.data
+        if (wizard?.version !== WIZARD_VERSION) {
+          this.should_open = true
+        }
+      })
+      .catch((error) => {
+        if (error === backend_offline_error) {
+          return
+        }
+
+        this.should_open = true
+      })
+  },
+  methods: {
+    setupBoat() {
+      this.vehicle_name = 'BlueBoat'
+      this.step_number += 1
+    },
+    async setupConfiguration() {
+      this.wait_configuration = true
+      if (await beacon.setHostname(this.mdns_name) && await beacon.setVehicleName(this.vehicle_name)) {
+        back_axios({
+          method: 'post',
+          url: `${API_URL}/set/wizard`,
+          timeout: 5000,
+          data: {
+            version: WIZARD_VERSION,
+          },
+        })
+          .then(() => {
+            this.wait_configuration = false
+            this.step_number = 3
+            this.is_ok = true
+          })
+          .catch((error) => {
+            this.configuration_failed = true
+            this.wait_configuration = false
+            this.error_message = 'Configuration done, but failed to set wizard version: '
+              + `${error.message ?? error.response?.data}.`
+          })
+
+        return
+      }
+      this.configuration_failed = true
+      this.wait_configuration = false
+    },
+    setupROV() {
+      this.vehicle_name = 'BlueROV'
+      this.step_number += 1
+    },
+  },
+}
+</script>
+
+<style scoped>
+.model-button {
+  display: inline-block;
+  padding: 10px 20px;
+  border: 2px solid #888;
+  cursor: pointer;
+  text-align: center;
+  user-select: none;
+  transition: background-color 0.3s, color 0.3s;
+}
+
+.model-button:hover {
+  background-color: #00000040;
+  color: #fff;
+}
+</style>
