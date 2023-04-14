b8f0c6e67d0f3d76ebadc841eccd48feef3a4f29
Patrick José Pereira <patrickelectric@gmail.com>
2023-04-14 11:11:04 -0300

    core: tools: nginx: bootstrap: Create ssl certificate
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/nginx/bootstrap.sh
+++ core/tools/nginx/bootstrap.sh
@@ -7,3 +7,11 @@ set -e
 echo "Installing nginx."
 apt update
 apt install --no-install-recommends --no-install-suggests -y nginx
+
+echo "Configure HTTPS"
+SSL_PATH=/etc/nginx/ssl
+mkdir -p "$SSL_PATH"
+openssl req -x509 -nodes -days 3650 -newkey rsa:2048 \
+    -keyout $SSL_PATH/nginx.key \
+    -out $SSL_PATH/nginx.crt \
+    -subj "/C=US/ST=California/L=Torrance/O=BlueRobotics/CN=blueos.local"
