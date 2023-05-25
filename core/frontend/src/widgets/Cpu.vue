6c29996b0823d9346a04dd115bae40460f08fee8
Patrick José Pereira <patrickelectric@gmail.com>
2023-05-25 12:50:57 -0300

    core: frontend: widgets: Add first version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/widgets/Cpu.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-card class="d-flex align-center justify-center" height="40">
+    <v-card-title>
+      CPU: {{ cpu_usage }} %
+    </v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import system_information from '@/store/system-information'
+
+export default Vue.extend({
+  name: 'HealthTrayMenu',
+  computed: {
+    cpu_usage(): string {
+      const cpus = system_information.system?.cpu
+      if (!cpus) {
+        return '-'
+      }
+      return (cpus.map((cpu) => cpu.usage).reduce((a, b) => a + b) / cpus.length).toFixed()
+    },
+  },
+})
+</script>
