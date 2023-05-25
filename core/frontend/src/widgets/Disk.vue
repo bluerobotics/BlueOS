159e17b3b0119db99a4988c1a55ee79f167a86bf
Patrick José Pereira <patrickelectric@gmail.com>
2023-05-25 12:50:57 -0300

    core: frontend: widgets: Add first version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/widgets/Disk.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card class="d-flex align-center justify-center" height="40">
+    <v-card-title>
+      Disk: {{ disk_usage }}%
+    </v-card-title>
+  </v-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import system_information from '@/store/system-information'
+import { Disk } from '@/types/system-information/system'
+
+export default Vue.extend({
+  name: 'HealthTrayMenu',
+  computed: {
+    main_disk(): undefined | Disk {
+      const disks = system_information.system?.disk
+      return disks?.find((sensor) => sensor.mount_point === '/')
+    },
+    disk_usage(): string {
+      return (this.main_disk ? 100 - this.main_disk.available_space_B / this.main_disk.total_space_B / 0.01 : undefined)
+        ?.toFixed() || '-'
+    },
+  },
+})
+</script>
