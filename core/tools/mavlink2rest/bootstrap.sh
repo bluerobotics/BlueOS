ebbd7c79fae50801793bcdb560191a3cc49811e8
Willian Galvani <williangalvani@gmail.com>
2023-05-25 08:20:17 -0300

    core: tools: update mavlink2rest repo url

--- core/tools/mavlink2rest/bootstrap.sh
+++ core/tools/mavlink2rest/bootstrap.sh
@@ -8,9 +8,9 @@ LOCAL_BINARY_PATH="/usr/bin/mavlink2rest"
 VERSION=t0.11.13
 
 # By default we install armv7
-REMOTE_BINARY_URL="https://github.com/patrickelectric/mavlink2rest/releases/download/${VERSION}/mavlink2rest-armv7-unknown-linux-musleabihf"
+REMOTE_BINARY_URL="https://github.com/mavlink/mavlink2rest/releases/download/${VERSION}/mavlink2rest-armv7-unknown-linux-musleabihf"
 if [[ "$(uname -m)" == "x86_64"* ]]; then
-    REMOTE_BINARY_URL="https://github.com/patrickelectric/mavlink2rest/releases/download/${VERSION}/mavlink2rest-x86_64-unknown-linux-musl"
+    REMOTE_BINARY_URL="https://github.com/mavlink/mavlink2rest/releases/download/${VERSION}/mavlink2rest-x86_64-unknown-linux-musl"
 fi
 
 # Download and install mavlink2rest under user binary folder with the correct permissions
