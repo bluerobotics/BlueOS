9ae6b1166eee4ea0461dfe28213e0ad46df3d336
Patrick José Pereira <patrickelectric@gmail.com>
2023-06-28 14:39:47 -0300

    core: frontend: types: parameter_repository: Update URL to Blue Robotics
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/types/parameter_repository.d.ts
+++ core/frontend/src/types/parameter_repository.d.ts
@@ -1,4 +1,4 @@
-declare module 'https://williangalvani.github.io/Blueos-Parameter-Repository/parameters.js' {
+declare module 'https://bluerobotics.github.io/Blueos-Parameter-Repository/params_v1.json' {
   const parameters: Dictionary<Dictionary<number>>
   export default parameters
 }
