b9abc8992bc9984ab2668eaac95b197d9dac867d
Patrick José Pereira <patrickelectric@gmail.com>
2023-07-05 16:48:16 -0300

    core: frontend: store: helper: Add speedtest functions
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/store/helper.ts
+++ core/frontend/src/store/helper.ts
@@ -6,6 +6,7 @@ import {
 import Notifier from '@/libs/notifier'
 import store from '@/store'
 import { helper_service } from '@/types/frontend_services'
+import { SpeedTestResult } from '@/types/helper'
 import back_axios from '@/utils/api'
 import { callPeriodically } from '@/utils/helper_functions'
 
@@ -51,6 +52,58 @@ class PingStore extends VuexModule {
       })
       .catch((error) => {
         notifier.pushBackError('INTERNET_CHECK_FAIL', error)
+      })
+  }
+
+  @Action
+  async checkInternetBestServer(): Promise<SpeedTestResult | void> {
+    return back_axios({
+      method: 'get',
+      url: `${this.API_URL}/internet_best_server`,
+      timeout: 15000,
+    })
+      .then((response) => response.data as SpeedTestResult)
+      .catch((error) => {
+        notifier.pushBackError('INTERNET_BEST_SERVER_FAIL', error)
+      })
+  }
+
+  @Action
+  async checkInternetDownloadSpeed(): Promise<SpeedTestResult | void> {
+    return back_axios({
+      method: 'get',
+      url: `${this.API_URL}/internet_download_speed`,
+      timeout: 15000,
+    })
+      .then((response) => response.data as SpeedTestResult)
+      .catch((error) => {
+        notifier.pushBackError('INTERNET_DOWNLOAD_SPEED_FAIL', error)
+      })
+  }
+
+  @Action
+  async checkInternetUploadSpeed(): Promise<SpeedTestResult | void> {
+    return back_axios({
+      method: 'get',
+      url: `${this.API_URL}/internet_upload_speed`,
+      timeout: 15000,
+    })
+      .then((response) => response.data as SpeedTestResult)
+      .catch((error) => {
+        notifier.pushBackError('INTERNET_UPLOAD_SPEED_FAIL', error)
+      })
+  }
+
+  @Action
+  async checkPreviousInternetTestResult(): Promise<SpeedTestResult | void> {
+    return back_axios({
+      method: 'get',
+      url: `${this.API_URL}/internet_test_previous_result`,
+      timeout: 15000,
+    })
+      .then((response) => response.data as SpeedTestResult)
+      .catch((error) => {
+        notifier.pushBackError('INTERNET_RESULT_SPEED_FAIL', error)
       })
   }
 }
