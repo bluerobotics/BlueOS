40baeff260c2ee63debdf4ffb35fb3cf92ab47c2
Patrick José Pereira <patrickelectric@gmail.com>
2023-07-05 16:47:55 -0300

    core: frontend: types: helper: Add types for speedtest
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/types/helper.ts
+++ core/frontend/src/types/helper.ts
@@ -19,3 +19,42 @@ export interface Service {
     port: number
     metadata?: ServiceMetadata
 }
+
+export interface SpeedtestServer {
+    url: string
+    lat: string
+    lon: string
+    name: string
+    country: string
+    cc: string
+    sponsor: string
+    id: string
+    host: string
+    d: number
+    latency: number
+}
+
+export interface SpeedtestClient {
+    ip: string
+    lat: string
+    lon: string
+    isp: string
+    isprating: string
+    rating: string
+    ispdlavg: string
+    ispulavg: string
+    loggedin: string
+    country: string
+}
+
+export interface SpeedTestResult {
+    download: number
+    upload: number
+    ping: number
+    server: SpeedtestServer
+    timestamp: Date
+    bytes_sent: number
+    bytes_received: number
+    share: string | null
+    client: SpeedtestClient
+}
