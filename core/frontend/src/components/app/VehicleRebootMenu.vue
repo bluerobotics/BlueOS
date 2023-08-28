6bc2d239f293e25d776be3c94084a5888b1c3940
Patrick José Pereira <patrickelectric@gmail.com>
2023-08-28 13:33:25 -0300

    core: frontend: components: VehicleRebootMenu: Use new AutopitlotManager.restart function
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/app/VehicleRebootMenu.vue
+++ core/frontend/src/components/app/VehicleRebootMenu.vue
@@ -23,32 +23,13 @@
 <script lang="ts">
 import Vue from 'vue'
 
-import Notifier from '@/libs/notifier'
-import autopilot_data from '@/store/autopilot'
-import autopilot from '@/store/autopilot_manager'
-import { parameters_service } from '@/types/frontend_services'
-import back_axios from '@/utils/api'
-
-const notifier = new Notifier(parameters_service)
+import * as AutopilotManager from '@/components/autopilot/AutopilotManagerUpdater'
 
 export default Vue.extend({
   name: 'VehicleRebootMenu',
   methods: {
     async rebootVehicle(): Promise<void> {
-      autopilot_data.reset()
-      autopilot.setRestarting(true)
-      await back_axios({
-        method: 'post',
-        url: `${autopilot.API_URL}/restart`,
-        timeout: 10000,
-      })
-        .catch((error) => {
-          notifier.pushBackError('AUTOPILOT_RESTART_FAIL', error)
-        })
-        .finally(() => {
-          autopilot.setRestarting(false)
-          autopilot_data.setRebootRequired(false)
-        })
+      await AutopilotManager.restart()
     },
   },
 })
