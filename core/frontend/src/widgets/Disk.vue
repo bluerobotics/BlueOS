b81137bc37b3f72e243bf17f0e9a4c050b01da24
Patrick José Pereira <patrickelectric@gmail.com>
2023-09-03 16:27:52 -0300

    core: frontend: widgets: Disk: Update value periodic
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/widgets/Disk.vue
+++ core/frontend/src/widgets/Disk.vue
@@ -9,11 +9,16 @@
 <script lang="ts">
 import Vue from 'vue'
 
-import system_information from '@/store/system-information'
+import system_information, { FetchType } from '@/store/system-information'
 import { Disk } from '@/types/system-information/system'
 
 export default Vue.extend({
   name: 'DiskWidget',
+  data() {
+    return {
+      timer: 0,
+    }
+  },
   computed: {
     main_disk(): undefined | Disk {
       const disks = system_information.system?.disk
@@ -23,6 +28,12 @@ export default Vue.extend({
       return (this.main_disk ? 100 - this.main_disk.available_space_B / this.main_disk.total_space_B / 0.01 : undefined)
         ?.toFixed() || '-'
     },
+  },
+  mounted() {
+    this.timer = setInterval(() => system_information.fetchSystemInformation(FetchType.SystemDiskType), 30000)
+  },
+  beforeDestroy() {
+    clearInterval(this.timer)
   },
 })
 </script>
