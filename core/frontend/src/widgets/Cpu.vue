26b9c49a1952194fe0790b8c387b1bb969585e68
Patrick José Pereira <patrickelectric@gmail.com>
2023-09-03 16:26:42 -0300

    core: frontend: widgets: CPU: Update value periodically
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/widgets/Cpu.vue
+++ core/frontend/src/widgets/Cpu.vue
@@ -9,10 +9,15 @@
 <script lang="ts">
 import Vue from 'vue'
 
-import system_information from '@/store/system-information'
+import system_information, { FetchType } from '@/store/system-information'
 
 export default Vue.extend({
   name: 'CpuWidget',
+  data() {
+    return {
+      timer: 0,
+    }
+  },
   computed: {
     cpu_usage(): string {
       const cpus = system_information.system?.cpu
@@ -21,6 +26,12 @@ export default Vue.extend({
       }
       return (cpus.map((cpu) => cpu.usage).reduce((a, b) => a + b) / cpus.length).toFixed()
     },
+  },
+  mounted() {
+    this.timer = setInterval(() => system_information.fetchSystemInformation(FetchType.SystemCpuType), 5000)
+  },
+  beforeDestroy() {
+    clearInterval(this.timer)
   },
 })
 </script>
