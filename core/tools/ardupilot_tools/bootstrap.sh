ad9ac27990cf94a5bc582230ef80af8af28a2792
Willian Galvani <williangalvani@gmail.com>
2023-09-12 13:43:27 -0300

    core: tools: handle pipenv when installing tools

--- core/tools/ardupilot_tools/bootstrap.sh
+++ core/tools/ardupilot_tools/bootstrap.sh
@@ -7,7 +7,8 @@ set -e
 
 ### Ardupilot's uploader is used to upload firmwares to serial boards
 COMMIT_HASH=f6544ca25ab232407ec102b7a5adf0adca0f2062
-LOCAL_PATH_UPLOADER="/usr/bin/ardupilot_fw_uploader.py"
+LOCAL_PATH_UPLOADER="$(python -m site --user-base)/bin/ardupilot_fw_uploader.py" || true
+mkdir -p "$(python -m site --user-base)/bin"
 REMOTE_URL_UPLOADER="https://raw.githubusercontent.com/ArduPilot/ardupilot/${COMMIT_HASH}/Tools/scripts/uploader.py"
 
 # Sudo command is not available on docker and the script is also used in different environments
@@ -19,19 +20,26 @@ $SUDO chmod +x "$LOCAL_PATH_UPLOADER"
 
 ## Download and install necessary modules to user-site folder
 
-USER_SITE_PACKAGES="$(python -m site --user-site)"
+# Check if pipenv is installed
+if command -v pipenv &> /dev/null; then
+    VERSION="$(python -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')"
+    USER_SITE_PACKAGES="$(pipenv --venv)/lib/python${VERSION}/site-packages/"
+else
+    # Fall back to the global Python site-packages directory if pipenv is not installed
+    USER_SITE_PACKAGES="$(python -m site --user-site)"
+fi
 mkdir -p $USER_SITE_PACKAGES
 
 ### Ardupilot's decoder is used to parse and validate firmware ELF files
 COMMIT_HASH=b839ddcc00f4cb89d89aaa8cf0cb03298d2f00b4
-LOCAL_PATH_DECODER="${USER_SITE_PACKAGES}/ardupilot_fw_decoder.py"
+LOCAL_PATH_DECODER="${USER_SITE_PACKAGES}/ardupilot_fw_decoder.py" || true
 REMOTE_URL_DECODER="https://raw.githubusercontent.com/ArduPilot/ardupilot/${COMMIT_HASH}/Tools/scripts/firmware_version_decoder.py"
 wget "$REMOTE_URL_DECODER" -O "$LOCAL_PATH_DECODER"
 
 
 ### Ardupilot's apj_tool is used to embed params into apj files
 COMMIT_HASH=b839ddcc00f4cb89d89aaa8cf0cb03298d2f00b4
-LOCAL_PATH_APJ_TOOL="/usr/bin/apj_tool.py"
+LOCAL_PATH_APJ_TOOL="$(python -m site --user-base)/bin/apj_tool.py" || true
 REMOTE_URL_APJ_TOOL="https://raw.githubusercontent.com/ArduPilot/ardupilot/${COMMIT_HASH}/Tools/scripts/apj_tool.py"
 
 $SUDO wget "$REMOTE_URL_APJ_TOOL" -O "$LOCAL_PATH_APJ_TOOL"
