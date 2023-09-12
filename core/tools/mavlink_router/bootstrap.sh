aa200c4c6c5ad17a42408fdefca326d44f0d89f6
Willian Galvani <williangalvani@gmail.com>
2023-09-12 13:43:27 -0300

    core: tools: handle pipenv when installing tools

--- core/tools/mavlink_router/bootstrap.sh
+++ core/tools/mavlink_router/bootstrap.sh
@@ -3,7 +3,13 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-LOCAL_BINARY_PATH="/usr/bin/mavlink-routerd"
+echo "checking sudo"
+SUDO="$(command -v sudo || echo '')"
+echo "making sudo readonly"
+readonly SUDO
+
+LOCAL_BINARY_PATH="/usr/bin/mavlink-routerd" || true
+echo "Installing mavlink-routerd to: ${LOCAL_BINARY_PATH}"
 VERSION="companion-core-development-0.1"
 GITHUB_REMOTE="https://github.com/patrickelectric/mavlink-router"
 
@@ -13,5 +19,5 @@ if [[ "$(uname -m)" == "x86_64"* ]]; then
     REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-libc-x86-64"
 fi
 
-wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
-chmod +x "$LOCAL_BINARY_PATH"
+$SUDO wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
+$SUDO chmod +x "$LOCAL_BINARY_PATH"
