28dfc67c74449a293775415b564dcf2ee459abda
Willian Galvani <williangalvani@gmail.com>
2023-09-12 17:27:12 -0300

    core: tools: sudo-less mavlink-router install

--- core/tools/mavlink_router/bootstrap.sh
+++ core/tools/mavlink_router/bootstrap.sh
@@ -3,12 +3,12 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-echo "checking sudo"
-SUDO="$(command -v sudo || echo '')"
-echo "making sudo readonly"
-readonly SUDO
+LOCAL_BINARY_PATH="$(python -m site --user-base)/bin/mavlink-routerd" || true
+if [ "$RUNNING_IN_CI" ]; then
+    LOCAL_BINARY_PATH="/usr/bin/mavlink-routerd" || true
+fi
+mkdir -p "$(dirname "$LOCAL_BINARY_PATH")"
 
-LOCAL_BINARY_PATH="/usr/bin/mavlink-routerd" || true
 echo "Installing mavlink-routerd to: ${LOCAL_BINARY_PATH}"
 VERSION="companion-core-development-0.1"
 GITHUB_REMOTE="https://github.com/patrickelectric/mavlink-router"
@@ -18,6 +18,10 @@ REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd
 if [[ "$(uname -m)" == "x86_64"* ]]; then
     REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-libc-x86-64"
 fi
+if [ -f "$LOCAL_BINARY_PATH" ]; then
+    echo "File $LOCAL_BINARY_PATH already exists. Skipping download."
+    exit 0
+fi
 
-$SUDO wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
-$SUDO chmod +x "$LOCAL_BINARY_PATH"
+wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
+chmod +x "$LOCAL_BINARY_PATH"
