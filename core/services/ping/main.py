e496518f1373cf6407697cc1974ee9ff4ce4ff4e
Patrick José Pereira <patrickelectric@gmail.com>
2023-09-14 13:31:49 -0300

    core: services: ping: Add ram_limit_usage
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/ping/main.py
+++ core/services/ping/main.py
@@ -5,6 +5,7 @@ import logging
 from typing import Any, List
 
 from commonwealth.utils.apis import GenericErrorHandlingRoute, PrettyJSONResponse
+from commonwealth.utils.general import limit_ram_usage
 from commonwealth.utils.logs import InterceptHandler, init_logger
 from fastapi import FastAPI, status
 from fastapi.responses import HTMLResponse
@@ -18,6 +19,8 @@ from portwatcher import PortWatcher
 from typedefs import PingDeviceDescriptorModel
 
 SERVICE_NAME = "ping"
+
+limit_ram_usage()
 
 logging.basicConfig(handlers=[InterceptHandler()], level=0)
 init_logger(SERVICE_NAME)
