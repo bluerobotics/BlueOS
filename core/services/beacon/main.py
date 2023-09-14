adc95432cf466c4dcf7907d968a68c89b6651409
Patrick José Pereira <patrickelectric@gmail.com>
2023-09-14 13:29:46 -0300

    core: services: beacon: Add limit_ram_usage
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/beacon/main.py
+++ core/services/beacon/main.py
@@ -10,6 +10,7 @@ from typing import Any, Dict, List, Optional
 import psutil
 from commonwealth.settings.manager import Manager
 from commonwealth.utils.apis import PrettyJSONResponse
+from commonwealth.utils.general import limit_ram_usage
 from commonwealth.utils.logs import init_logger
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -23,6 +24,8 @@ from settings import ServiceTypes, SettingsV4
 from typedefs import InterfaceType, IpInfo, MdnsEntry
 
 SERVICE_NAME = "beacon"
+
+limit_ram_usage()
 
 
 class AsyncRunner:
