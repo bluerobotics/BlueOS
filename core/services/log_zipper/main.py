065150c105a7df2eb9ae972010f14fb0383e72ba
Patrick José Pereira <patrickelectric@gmail.com>
2023-10-12 15:48:38 -0700

    core: services: log_zipper: Use gzip over zip
    This make it easier to process files
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/log_zipper/main.py
+++ core/services/log_zipper/main.py
@@ -3,11 +3,11 @@
 import argparse
 import datetime
 import glob
+import gzip
 import logging
 import os
 import pathlib
 import time
-import zipfile
 from typing import List
 
 from commonwealth.utils.general import delete_everything, limit_ram_usage
@@ -20,10 +20,10 @@ limit_ram_usage()
 
 
 def zip_files(files: List[str], output_path: str) -> None:
-    with zipfile.ZipFile(output_path, "w", zipfile.ZIP_DEFLATED) as zipper:
-        for file in files:
-            logger.debug(f"Zipping {file}...")
-            zipper.write(file)
+    for file in files:
+        logger.debug(f"Zipping {file}...")
+        with open(file, "rb") as f_in, gzip.open(output_path, "wb") as f_out:
+            f_out.writelines(f_in)
 
     for file in files:
         try:
@@ -66,7 +66,7 @@ def main() -> None:
                 continue
             folder_name = os.path.basename(folder)
             timestamp = time.strftime("%Y%m%d-%H%M%S")
-            zipped_file = f"{folder}/{folder_name}-{timestamp}.zip"
+            zipped_file = f"{folder}/{folder_name}-{timestamp}.gz"
             zip_files(local_files, zipped_file)
             logger.info(f"Created zip archive {zipped_file} with {len(local_files)} files.")
 
