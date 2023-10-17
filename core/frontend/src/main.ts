563859d53ed569a1b45fe0760bbdb36c164e9966
Patrick José Pereira <patrickelectric@gmail.com>
2023-10-17 16:15:38 -0700

    core: frontend: main: Update VTour registry
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/main.ts
+++ core/frontend/src/main.ts
@@ -1,18 +1,19 @@
 import './cosmos'
 import '@/style/css/vuetify-global.css'
 import '@/style/css/animations.css'
-import 'vue-tour/dist/vue-tour.css'
+import './components/vue-tour/dist/vue-tour.css'
 
 import Vue from 'vue'
 import VueApexCharts from 'vue-apexcharts'
 import JsonViewer from 'vue-json-viewer'
 import VueTooltipDirective from 'vue-tooltip-directive'
-import VueTour from 'vue-tour'
 import VueDraggable from 'vuedraggable'
 import Vuetify from 'vuetify/lib'
 
 import App from './App.vue'
 import DefaultTooltip from './components/common/DefaultTooltip.vue'
+import VStep from './components/vue-tour/src/components/VStep.vue'
+import VTour from './components/vue-tour/src/components/VTour.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 import store from './store'
@@ -22,11 +23,15 @@ Vue.use(VueTooltipDirective, {
 })
 Vue.use(VueApexCharts)
 Vue.use(Vuetify)
-Vue.use(VueTour)
 Vue.use(JsonViewer)
 
 Vue.component('Apexchart', VueApexCharts)
 Vue.component('Draggable', VueDraggable)
+
+// Do Vue-Tour registration manually
+Vue.component('VTour', VTour)
+Vue.component('VStep', VStep)
+Vue.prototype.$tours = {}
 
 new Vue({
   router,
