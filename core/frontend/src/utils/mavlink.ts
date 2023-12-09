1af7344bab78cd1fb0f9fa8cf0f133e3024a9b77
Willian Galvani <williangalvani@gmail.com>
2023-12-09 14:15:09 -0300

    core: frontend: mavlink store: create identified messages dict

--- core/frontend/src/utils/mavlink.ts
+++ core/frontend/src/utils/mavlink.ts
@@ -1,5 +1,8 @@
 import { get } from 'lodash'
 
-export default function mavlink_store_get<T>(storage: T, path: string): unknown {
+export default function mavlink_store_get<T>(storage: T, path: string, system_id? :number, component_id?: number): unknown {
+  if (system_id !== undefined && component_id !== undefined) {
+    return get(storage, `available_identified_messages.${system_id}_${component_id}.${path}`)
+  }
   return get(storage, `available_messages.${path}`)
 }
