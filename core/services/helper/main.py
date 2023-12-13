a41ee5f37bb27f20755b2eba9d38a8605bc2c5b8
Willian Galvani <williangalvani@gmail.com>
2023-12-13 15:31:26 -0300

    core: helper: bump memory use limit to 200mb

--- core/services/helper/main.py
+++ core/services/helper/main.py
@@ -35,7 +35,7 @@ from nginx_parser import parse_nginx_file
 SERVICE_NAME = "helper"
 SPEED_TEST: Optional[Speedtest] = None
 
-limit_ram_usage()
+limit_ram_usage(200)
 
 logging.basicConfig(handlers=[InterceptHandler()], level=logging.DEBUG)
 try:
