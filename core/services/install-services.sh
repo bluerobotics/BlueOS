048a8e4e5721494ea1c0ad1ef3c081e7a2607b5c
Patrick José Pereira <patrickelectric@gmail.com>
2023-12-19 08:12:09 -0300

    core: services: install-services: libs are already installed by install-libs
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/install-services.sh
+++ core/services/install-services.sh
@@ -10,13 +10,6 @@ BUILD_PACKAGES=(
 apt update
 apt install -y --no-install-recommends ${BUILD_PACKAGES[*]}
 
-# Pre-Build dependencies:
-# For convenience, we build ourselves the .wheel packages for dependencies
-# which have no armv7 wheel in pypi. This saves a lot of build time in docker
-if [[ "$(uname -m)" == "armv7l"* ]]; then
-    pip install https://s3.amazonaws.com/downloads.bluerobotics.com/companion-docker/wheels/aiohttp-3.7.4-cp39-cp39-linux_armv7l.whl
-fi
-
 # Wifi service:
 ## Bind path for wpa
 mkdir -p /var/run/wpa_supplicant/
