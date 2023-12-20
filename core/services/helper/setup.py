f309b91b7f9954c55751f1f1e58a3a9231a0b1f7
Willian Galvani <williangalvani@gmail.com>
2023-12-20 19:36:14 -0300

    core: bump fastapi and starlette versions

--- core/services/helper/setup.py
+++ core/services/helper/setup.py
@@ -67,13 +67,15 @@ setup(
         "aiofiles == 0.6.0",
         "beautifulsoup4 == 4.9.3",
         "commonwealth == 0.1.0",
-        "fastapi == 0.63.0",
+        "fastapi == 0.105.0",
+        # Enforce anyio fastapi subdependency to avoid conflict with starlette
+        "anyio == 3.7.1",
         "fastapi-versioning == 0.9.1",
         "loguru == 0.5.3",
         "psutil == 5.7.2",
         "requests == 2.25.1",
         "speedtest-cli == 2.1.3",
-        "starlette == 0.13.6",
+        "starlette == 0.27.0",
         "uvicorn == 0.13.4",
     ],
 )
