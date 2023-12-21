4806e1ad499a2bf07b353954e876707056bb904e
Willian Galvani <williangalvani@gmail.com>
2023-12-20 21:02:24 -0300

    core: do not compile unnecessary libs/services

--- core/libs/install-libs.sh
+++ core/libs/install-libs.sh
@@ -7,23 +7,30 @@ BUILD_PACKAGES=(
     g++
 )
 
-apt update
-apt install -y --no-install-recommends ${BUILD_PACKAGES[*]}
+echo "Target architecture: $TARGETARCH"
+echo "Target variant: $TARGETVARIANT"
+# Install build packages if not on armv7 or amd64, which are the only platforms that have pre-built wheels
+if ! { [ "$TARGETARCH" == "arm" ] && [ "$TARGETVARIANT" == "v7" ]; } && [ "$TARGETARCH" != "amd64" ]; then
+    apt update
+    apt install -y --no-install-recommends ${BUILD_PACKAGES[*]}
+fi
 
 # Piwheels is a Python package repository providing Arm platform wheels (pre-compiled binary Python packages)
 # specifically for the Raspberry Pi, making pip installations much faster.
 # Packages are natively compiled on Raspberry Pi 3 and 4 hardware
-if [[ "$(uname -m)" == "arm"* ]]; then
-    echo "Configuring pip to use piwheels"
-    echo "[global]" >> /etc/pip.conf
-    echo "extra-index-url=https://www.piwheels.org/simple" >> /etc/pip.conf
-fi
+# Add it regardless of the platform, as it is a fallback index, and will only be used if the package is not found on the main index.
+echo "Configuring pip to use piwheels"
+echo "[global]" >> /etc/pip.conf
+echo "extra-index-url=https://www.piwheels.org/simple" >> /etc/pip.conf
 
 CURRENT_PATH=$(dirname "$0")
 
 # Install libraries
-python3 $CURRENT_PATH/bridges/setup.py install
-python3 $CURRENT_PATH/commonwealth/setup.py install
+
+# install pykson = {git = "https://github.com/patrickelectric/pykson.git", rev = "fcab71c1eadd6c6b730ca21a5eecb3bf9c374507"}
+pip3 install https://codeload.github.com/patrickelectric/pykson/zip/fcab71c1eadd6c6b730ca21a5eecb3bf9c374507
+pip3 install -e $CURRENT_PATH/bridges
+pip3 install -e $CURRENT_PATH/commonwealth
 
 apt -y remove ${BUILD_PACKAGES[*]}
 apt -y autoremove
