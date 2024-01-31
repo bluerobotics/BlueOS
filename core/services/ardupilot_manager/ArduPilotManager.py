ba32a4a2effd9224e5e682e04927a1a777f02809
Patrick José Pereira <patrickelectric@gmail.com>
2024-01-31 02:30:39 -0300

    core: services: ardupilot_manager: ArduPilotManager: Fix should_be_running value in kill_ardupilot
    This operation should be done asap, the internal functions will take some time,
    in paralle, if should_be_running is not false, the `auto_restart_ardupilot` will run.
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/ardupilot_manager/ArduPilotManager.py
+++ core/services/ardupilot_manager/ArduPilotManager.py
@@ -478,6 +478,7 @@ class ArduPilotManager(metaclass=Singleton):
                 raise ArdupilotProcessKillFail(f"Could not kill {process.name()}::{process.pid}.") from error
 
     async def kill_ardupilot(self) -> None:
+        self.should_be_running = False
         if not self.current_board or self.current_board.platform != Platform.SITL:
             try:
                 logger.info("Disarming vehicle.")
@@ -486,21 +487,18 @@ class ArduPilotManager(metaclass=Singleton):
             except Exception as error:
                 logger.warning(f"Could not disarm vehicle: {error}. Proceeding with kill.")
 
-        try:
-            # TODO: Add shutdown command on HAL_SITL and HAL_LINUX, changing terminate/prune
-            # logic with a simple "self.vehicle_manager.shutdown_vehicle()"
-            logger.info("Terminating Ardupilot subprocess.")
-            await self.terminate_ardupilot_subprocess()
-            logger.info("Ardupilot subprocess terminated.")
-            logger.info("Pruning Ardupilot's system processes.")
-            await self.prune_ardupilot_processes()
-            logger.info("Ardupilot's system processes pruned.")
+        # TODO: Add shutdown command on HAL_SITL and HAL_LINUX, changing terminate/prune
+        # logic with a simple "self.vehicle_manager.shutdown_vehicle()"
+        logger.info("Terminating Ardupilot subprocess.")
+        await self.terminate_ardupilot_subprocess()
+        logger.info("Ardupilot subprocess terminated.")
+        logger.info("Pruning Ardupilot's system processes.")
+        await self.prune_ardupilot_processes()
+        logger.info("Ardupilot's system processes pruned.")
 
-            logger.info("Stopping Mavlink manager.")
-            await self.mavlink_manager.stop()
-            logger.info("Mavlink manager stopped.")
-        finally:
-            self.should_be_running = False
+        logger.info("Stopping Mavlink manager.")
+        await self.mavlink_manager.stop()
+        logger.info("Mavlink manager stopped.")
 
     async def start_ardupilot(self) -> None:
         await self.setup()
