e1fcb2d6e52ac1bf64f91c19ec0e3e8f6cf712a3
Patrick José Pereira <patrickelectric@gmail.com>
2024-02-07 11:59:01 -0300

    core: components: vehiclesetup: Add back clasic mode and add wip ui under dev mode
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/vehiclesetup/Configure.vue
+++ core/frontend/src/components/vehiclesetup/Configure.vue
@@ -1,36 +1,73 @@
 <template>
-  <v-container fluid>
-    <v-tabs
-      v-model="page_selected"
-      centered
-      show-arrows
-    >
-      <v-tabs-slider />
-      <v-tab
-        v-for="page in filtered_pages"
-        :key="`title-${page.title}`"
+  <div>
+    <v-container fluid v-if="is_dev_mode">
+      <v-tabs
+        v-model="page_selected"
+        centered
+        show-arrows
       >
-        {{ page.title }}
-      </v-tab>
-    </v-tabs>
-    <v-tabs-items v-model="page_selected">
-      <v-tab-item
-        v-for="page in filtered_pages"
-        :key="`item-${page.title}`"
-      >
-        <component :is="page.component" />
-      </v-tab-item>
-    </v-tabs-items>
-  </v-container>
+        <v-tabs-slider />
+        <v-tab
+          v-for="page in filtered_pages"
+          :key="`title-${page.title}`"
+        >
+          {{ page.title }}
+        </v-tab>
+      </v-tabs>
+      <v-tabs-items v-model="page_selected">
+        <v-tab-item
+          v-for="page in filtered_pages"
+          :key="`item-${page.title}`"
+        >
+          <component :is="page.component" />
+        </v-tab-item>
+      </v-tabs-items>
+    </v-container>
+    <div v-else>
+      <v-row class="mt-5">
+        <v-col
+          cols="12"
+          sm="7"
+        >
+          <v-card class="pa-5 ma-2">
+            <vehicle-viewer
+              noannotations
+              :autorotate="true"
+            />
+          </v-card>
+        </v-col>
+        <v-col
+          cols="12"
+          sm="5"
+        >
+          <vehicle-info :autorotate="true" />
+          <param-sets />
+        </v-col>
+      </v-row>
+      <v-sheet class="d-flex">
+        <lights-info />
+        <gripper-info />
+        <leak-info />
+        <power-info />
+      </v-sheet>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
+import VehicleInfo from '@/components/vehiclesetup/overview/VehicleInfo.vue'
+import VehicleViewer from '@/components/vehiclesetup/viewers/VehicleViewer.vue'
 import autopilot from '@/store/autopilot_manager'
+import settings from '@/store/settings'
 
 import LightsConfigration from './configuration/lights.vue'
+import GripperInfo from './overview/gripper.vue'
+import LeakInfo from './overview/LeakInfo.vue'
+import LightsInfo from './overview/LightsInfo.vue'
 import ParamSets from './overview/ParamSets.vue'
+import PowerInfo from './overview/PowerInfo.vue'
 
 export interface Item {
   title: string,
@@ -42,6 +79,12 @@ export interface Item {
 export default Vue.extend({
   name: 'Configure',
   components: {
+    VehicleViewer,
+    VehicleInfo,
+    GripperInfo,
+    PowerInfo,
+    LightsInfo,
+    LeakInfo,
     ParamSets,
     LightsConfigration,
   },
@@ -63,6 +106,9 @@ export default Vue.extend({
     filtered_pages() {
       // eslint-disable-next-line no-extra-parens
       return this.pages.filter((page) => (page.filter ? page.filter() : true))
+    },
+    is_dev_mode() {
+      return settings.is_dev_mode
     },
   },
 })
