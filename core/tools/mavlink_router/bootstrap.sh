8b1926441656c4e8e209c546265fb08951433bdb
Patrick José Pereira <patrickelectric@gmail.com>
2024-02-13 14:26:04 -0300

    core: tools: mavlink-router: Use latest version of official repository
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/mavlink_router/bootstrap.sh
+++ core/tools/mavlink_router/bootstrap.sh
@@ -10,13 +10,13 @@ fi
 mkdir -p "$(dirname "$LOCAL_BINARY_PATH")"
 
 echo "Installing mavlink-routerd to: ${LOCAL_BINARY_PATH}"
-VERSION="companion-core-development-0.1"
-GITHUB_REMOTE="https://github.com/patrickelectric/mavlink-router"
+VERSION="v4"
+GITHUB_REMOTE="https://github.com/mavlink-router/mavlink-router"
 
-# By default we install armv6
-REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-libc-armv6"
+# By default we install armhf
+REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-glibc-armhf"
 if [[ "$(uname -m)" == "x86_64"* ]]; then
-    REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-libc-x86-64"
+    REMOTE_BINARY_URL="${GITHUB_REMOTE}/releases/download/${VERSION}/mavlink-routerd-glibc-x86_64"
 fi
 if [ -f "$LOCAL_BINARY_PATH" ]; then
     echo "File $LOCAL_BINARY_PATH already exists. Skipping download."
