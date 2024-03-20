830677907fbe4487aab51eff58f25d59162d2162
Patrick José Pereira <patrickelectric@gmail.com>
2024-03-20 12:53:15 -0300

    bootstrap: Add message with version
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- bootstrap/main.py
+++ bootstrap/main.py
@@ -9,7 +9,9 @@ from bootstrap.bootstrap import Bootstrapper
 
 
 if __name__ == "__main__":
+    version = os.environ.get("GIT_DESCRIBE_TAGS", None)
     logger.add("/var/logs/blueos/services/bootstrap/bootstrap_{time}.log", enqueue=True, rotation="30 minutes")
+    logger.info(f"Running BlueOS Bootstrap {version}")
     if os.environ.get("BLUEOS_CONFIG_PATH", None) is None:
         logger.info("Please supply the host path for the config files as the BLUEOS_CONFIG_PATH environment variable.")
         logger.info("Example docker command line:")
