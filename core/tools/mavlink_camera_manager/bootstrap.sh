6f09be2c48b642c1d08af601a47dfd9a78f3fd7e
Patrick José Pereira <patrickelectric@gmail.com>
2024-04-25 12:52:23 -0300

    core: tools: mavlink-camera-manager: Update to t3.12.12
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/mavlink_camera_manager/bootstrap.sh
+++ core/tools/mavlink_camera_manager/bootstrap.sh
@@ -5,7 +5,7 @@ set -e
 
 LOCAL_BINARY_PATH="/usr/bin/mavlink-camera-manager"
 ARTIFACT_PREFIX="mavlink-camera-manager"
-VERSION="t3.12.11"
+VERSION="t3.12.12"
 
 # By default we install armv7
 REMOTE_BINARY_URL="https://github.com/mavlink/mavlink-camera-manager/releases/download/${VERSION}/${ARTIFACT_PREFIX}-armv7.zip"
