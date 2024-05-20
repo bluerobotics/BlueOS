eff3efa70b13c5e28fe2bdeae55dc400ff0d9438
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-05-20 11:37:04 -0300

    kraken: Connect Manifest module V2 with API

--- core/services/kraken/api/v2/routers/manifest.py
+++ core/services/kraken/api/v2/routers/manifest.py
@@ -1,6 +1,24 @@
-from fastapi import APIRouter, status
+from functools import wraps
+from typing import Any, Callable, Tuple
+
+from fastapi import APIRouter, HTTPException, status
 from fastapi.responses import Response
 from fastapi_versioning import versioned_api_route
+
+from manifest import ManifestManager
+from manifest.exceptions import (
+    ManifestDataFetchFailed,
+    ManifestDataParseFailed,
+    ManifestInvalidURL,
+    ManifestNotFound,
+    ManifestOperationNotAllowed,
+)
+from manifest.models import (
+    Manifest,
+    ManifestSource,
+    RepositoryEntry,
+    UpdateManifestSource,
+)
 
 manifest_router_v2 = APIRouter(
     prefix="/manifest",
@@ -9,67 +27,113 @@ manifest_router_v2 = APIRouter(
     responses={status.HTTP_404_NOT_FOUND: {"description": "Not found"}},
 )
 
+manifest_manager = ManifestManager.instance()
+
+
+def manifest_to_http_exception(endpoint: Callable[..., Any]) -> Callable[..., Any]:
+    @wraps(endpoint)
+    async def wrapper(*args: Tuple[Any], **kwargs: dict[str, Any]) -> Any:
+        try:
+            return await endpoint(*args, **kwargs)
+        except (ManifestDataFetchFailed, ManifestDataParseFailed, ManifestInvalidURL) as error:
+            raise HTTPException(status_code=status.HTTP_502_BAD_GATEWAY, detail=str(error)) from error
+        except ManifestNotFound as error:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(error)) from error
+        except ManifestOperationNotAllowed as error:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail=str(error)) from error
+        except Exception as error:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(error)) from error
+
+    return wrapper
+
 
 @manifest_router_v2.get("/", status_code=status.HTTP_200_OK)
-async def fetch(_data: bool = True) -> Response:
+@manifest_to_http_exception
+async def fetch(data: bool = True, enabled: bool = False) -> list[Manifest]:
     """
     List all available manifests sorted by its priority. If data is set to false, only the manifest settings will be
-    returned.
+    returned. If enabled is set to true, only enabled manifests will be returned.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    return await manifest_manager.fetch(data, enabled)
 
 
 @manifest_router_v2.get("/{identifier}/details", status_code=status.HTTP_200_OK)
-async def fetch_by_identifier(_identifier: str, _data: bool = True) -> Response:
+@manifest_to_http_exception
+async def fetch_by_identifier(identifier: str, data: bool = True) -> Manifest:
     """
     Get details of a given manifest. If data is set to false, only the manifest settings will be returned.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    return await manifest_manager.fetch_by_identifier(identifier, data)
+
+
+@manifest_router_v2.get("/consolidated", status_code=status.HTTP_200_OK)
+@manifest_to_http_exception
+async def fetch_consolidated() -> list[RepositoryEntry]:
+    """
+    List a consolidation of all repository entries from all manifest sources merged by its sorted priority, if a
+    repository entry is duplicated, the one with the highest priority will be kept.
+    """
+    return await manifest_manager.fetch_consolidated()
 
 
 @manifest_router_v2.post("/", status_code=status.HTTP_201_CREATED)
-async def create() -> Response:
+@manifest_to_http_exception
+async def create(body: ManifestSource, validate_url: bool = True) -> Manifest:
     """
     Creates a new manifest source.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    return await manifest_manager.add_source(body, validate_url)
 
 
 @manifest_router_v2.post("/{identifier}/enable", status_code=status.HTTP_204_NO_CONTENT)
-async def enable(_identifier: str) -> Response:
+@manifest_to_http_exception
+async def enable(identifier: str) -> None:
     """
     Enables a manifest source.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    await manifest_manager.enable_source(identifier)
 
 
 @manifest_router_v2.post("/{identifier}/disable", status_code=status.HTTP_204_NO_CONTENT)
-async def disable(_identifier: str) -> Response:
+@manifest_to_http_exception
+async def disable(identifier: str) -> Response:
     """
     Disables a manifest source.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    await manifest_manager.disable_source(identifier)
 
 
-@manifest_router_v2.put("/{identifier}", status_code=status.HTTP_204_NO_CONTENT)
-async def update(_identifier: str) -> Response:
+@manifest_router_v2.put("/{identifier}/details", status_code=status.HTTP_204_NO_CONTENT)
+@manifest_to_http_exception
+async def update(identifier: str, body: UpdateManifestSource, validate_url: bool = True) -> None:
     """
     Updates a manifest source.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    await manifest_manager.update_source(identifier, body, validate_url)
+
+
+@manifest_router_v2.put("/orders", status_code=status.HTTP_204_NO_CONTENT)
+@manifest_to_http_exception
+async def reorder_by_identifiers(body: list[str]) -> None:
+    """
+    Reorders all manifests based on the order of given list of identifiers.
+    """
+    await manifest_manager.order_sources(body)
 
 
 @manifest_router_v2.put("/{identifier}/order/{order}", status_code=status.HTTP_204_NO_CONTENT)
-async def reorder_by_identifier(_identifier: str, _order: int) -> Response:
+@manifest_to_http_exception
+async def reorder_by_identifier(identifier: str, order: int) -> None:
     """
     Reorders a given manifest source priority.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    await manifest_manager.order_source(identifier, order)
 
 
 @manifest_router_v2.delete("/{identifier}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete(_identifier: str) -> Response:
+@manifest_to_http_exception
+async def delete(identifier: str) -> Response:
     """
     Deletes a manifest source.
     """
-    return Response(status_code=status.HTTP_501_NOT_IMPLEMENTED)
+    await manifest_manager.remove_source(identifier)
