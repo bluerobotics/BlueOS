ecc802b8ab41886c8f4a5b780bb30c1ee636d7ee
Willian Galvani <williangalvani@gmail.com>
2024-05-23 11:34:35 -0300

    core: tools: remove unnecessary apt calls

--- core/tools/linux2rest/bootstrap.sh
+++ core/tools/linux2rest/bootstrap.sh
@@ -12,7 +12,5 @@ if [[ "$(uname -m)" == "x86_64"* ]]; then
     REMOTE_BINARY_URL="https://github.com/patrickelectric/linux2rest/releases/download/${VERSION}/linux2rest-x86_64"
 fi
 
-# Download and install necessary libraries for linux2rest
-DEBIAN_FRONTEND=noninteractive apt update && apt install -y libudev-dev
 wget "$REMOTE_BINARY_URL" -O "$LOCAL_BINARY_PATH"
 chmod +x "$LOCAL_BINARY_PATH"
--- core/tools/sudo/bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Exit if something goes wrong
-set -e
-
-echo "Installing sudo."
-apt update
-apt install --no-install-recommends --no-install-suggests -y sudo
