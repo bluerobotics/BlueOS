36142005ae2658813427bade6f10450eb854fbd2
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-05-30 11:11:23 -0300

    kraken: Add docker wrapper module
    * Add "harbor" a docekr wrapper module, cannot be named same as docker
      to avoid conflicts, that manage parts related to litings containers
      and stats.
    * Add DockerCtx to avoid keeping open operations happening

---
+++ core/services/kraken/harbor/__init__.py
@@ -0,0 +1,5 @@
+# pylint: disable=W0406
+from harbor.container import ContainerManager
+from harbor.contexts import DockerCtx
+
+__all__ = ["ContainerManager", "DockerCtx"]
---
+++ core/services/kraken/harbor/exceptions.py
@@ -0,0 +1,2 @@
+class ContainerNotFound(Exception):
+    pass
