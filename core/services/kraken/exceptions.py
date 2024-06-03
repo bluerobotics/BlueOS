8b171f8aa45bf7c60d02c60a776d4a5cada4c795
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-06-03 10:27:18 -0300

    kraken: Connect new extension module with main app

--- core/services/kraken/exceptions.py
+++
@@ -1,6 +0,0 @@
-class ContainerDoesNotExist(RuntimeError):
-    """Attempted to use a non-existing container"""
-
-
-class ExtensionNotFound(RuntimeError):
-    """Attempted to operate on a non-existing extension"""
