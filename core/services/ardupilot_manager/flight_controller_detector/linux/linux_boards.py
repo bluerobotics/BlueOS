28edf4240fda9bf4540d5a13a1d0903b5a770219
Willian Galvani <williangalvani@gmail.com>
2024-06-05 11:56:28 -0300

    ardupilot_manager: refactor linux boards detection

---
+++ core/services/ardupilot_manager/flight_controller_detector/linux/linux_boards.py
@@ -0,0 +1,41 @@
+from typing import List, Type
+
+from smbus2 import SMBus
+
+from typedefs import FlightController, PlatformType, Serial
+
+
+class LinuxFlightController(FlightController):
+    """Linux-based Flight-controller board."""
+
+    @property
+    def type(self) -> PlatformType:
+        return PlatformType.Linux
+
+    def detect(self) -> bool:
+        raise NotImplementedError
+
+    def get_serial_cmdlines(self) -> str:
+        cmdlines = [f"-{entry.port} {entry.endpoint}" for entry in self.get_serials()]
+        return " ".join(cmdlines)
+
+    def get_serials(self) -> List[Serial]:
+        raise NotImplementedError
+
+    def check_for_i2c_device(self, bus_number: int, address: int) -> bool:
+        try:
+            bus = SMBus(bus_number)
+            bus.read_byte_data(address, 0)
+            return True
+        except OSError:
+            return False
+
+    @classmethod
+    def get_all_boards(cls) -> List[Type["LinuxFlightController"]]:
+        all_subclasses = []
+
+        for subclass in cls.__subclasses__():
+            all_subclasses.append(subclass)
+            all_subclasses.extend(subclass.get_all_boards())
+
+        return all_subclasses
