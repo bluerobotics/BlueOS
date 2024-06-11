c1b5424fe6422551b0ac66558e479dc6ac329572
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-06-11 16:24:24 -0300

    kraken: Add job queue module for hit and close

---
+++ core/services/kraken/jobs/__init__.py
@@ -0,0 +1,4 @@
+from jobs.jobs import JobsManager
+from jobs.models import Job
+
+__all__ = ["Job", "JobsManager"]
--- core/services/kraken/main.py
+++ core/services/kraken/main.py
@@ -13,9 +13,11 @@ logging.basicConfig(handlers=[InterceptHandler()], level=0)
 init_logger(SERVICE_NAME)
 
 from api import application
+from jobs import JobsManager
 from kraken import Kraken
 
 kraken = Kraken()
+jobs = JobsManager()
 
 if __name__ == "__main__":
     args = CommandLineArgs.from_args()
@@ -24,12 +26,14 @@ if __name__ == "__main__":
         logging.getLogger(SERVICE_NAME).setLevel(logging.DEBUG)
 
     logger.info("Releasing the Kraken service.")
-
     loop = asyncio.new_event_loop()
 
     config = Config(app=application, loop=loop, host=args.host, port=args.port, log_config=None)
     server = Server(config)
+    jobs.set_base_host(f"http://{args.host}:{args.port}")
 
     loop.create_task(kraken.start())
+    loop.create_task(jobs.start())
     loop.run_until_complete(server.serve())
+    loop.run_until_complete(jobs.stop())
     loop.run_until_complete(kraken.stop())
