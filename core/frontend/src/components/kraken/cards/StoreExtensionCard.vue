c327f26e392f63ac0c30081811338df89504f58a
Joao Mario Lago <joao.mario.lago@hotmail.com>
2024-06-23 10:00:07 -0300

    WIP UI 2

--- core/frontend/src/components/kraken/cards/StoreExtensionCard.vue
+++ core/frontend/src/components/kraken/cards/StoreExtensionCard.vue
@@ -9,6 +9,20 @@
     class="store-extension-card"
   >
     <div
+      :style="card_dominant_color ? { backgroundColor: card_dominant_color } : {}"
+      class="img-background"
+    />
+    <v-img
+      v-once
+      ref="extension_logo"
+      contain
+      :src="extension.extension_logo"
+      class="background-image"
+      eager="true"
+      @load="setDominantColor"
+    />
+
+    <div
       :style="architecture_list_style"
       class="pt-1 mb-2 pl-2 pr-3 architectures-list"
     >
@@ -24,15 +38,7 @@
           @click="$emit('selected')"
           @keydown.left="() => {}"
         >
-          <v-img
-            v-once
-            ref="extension_logo"
-            contain
-            :src="extension.extension_logo"
-            height="150px"
-            class="mx-3 my-2"
-            @load="setDominantColor"
-          />
+          <div class="spacer" />
 
           <v-card-subtitle class="px-3 py-2">
             <div
@@ -50,8 +56,10 @@
       </template>
       <span>This extension is not compatible with current machine architecture running BlueOS.</span>
     </v-tooltip>
-    <v-card-actions class="px-3 py-2 d-flex justify-space-between align-center">
-      <v-avatar size="32">
+    <div class="bottom-gradient-fade" />
+    <div class="bottom-gradient" />
+    <v-card-actions class="px-3 py-2 d-flex justify-space-between align-center card-actions">
+      <v-avatar size="32" rounded="0">
         <v-img
           contain
           :src="extension.company_logo"
@@ -86,7 +94,7 @@
 </template>
 
 <script lang="ts">
-import ColorThief from 'color-thief'
+import ColorThief from 'colorthief'
 import Vue, { PropType } from 'vue'
 
 import { getLatestVersion, isStable, updateAvailableTag } from '@/components/kraken/Utils'
@@ -173,7 +181,7 @@ export default Vue.extend({
         return 'Unknown'
       }
 
-      const names = authors.slice(0, 2).map((author) => author.name).join(', ')
+      const names = authors.map((author) => author.name).join(', ')
 
       return authors.length > 2 ? `${names} ...` : names
     },
@@ -223,6 +231,57 @@ export default Vue.extend({
   -moz-box-sizing: border-box;
   -webkit-box-sizing: border-box;
   border: 2px solid transparent;
+  position: relative;
+  overflow: hidden;
+}
+
+.store-extension-card:hover {
+  transform: translateY(-5px);
+}
+
+.img-background {
+  position: absolute;
+  top: -1px;
+  left: -1px;
+  width: calc(100% + 2px);
+  height: 90%;
+  z-index: 0;
+  border-radius: 0px !important;
+}
+
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  border-radius: 8px;
+  height: auto;
+  z-index: 1;
+}
+
+.spacer {
+  flex-grow: 1;
+  min-height: 150px;
+}
+
+.bottom-gradient {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 55px;
+  background-color: var(--v-oyster-darken1);
+  z-index: 3 !important;
+}
+.bottom-gradient-fade {
+  position: absolute;
+  bottom: 55px;
+  left: 0;
+  width: 100%;
+  height: 80px;
+  background: linear-gradient(0deg, var(--v-oyster-darken1), rgba(30, 30, 30, 0.6));
+  backdrop-filter: blur(10px);
+  z-index: 3 !important;
 }
 
 .content-wrapper {
@@ -231,10 +290,11 @@ export default Vue.extend({
   flex-grow: 1;
   justify-content: space-between;
   cursor: pointer;
+  z-index: 4 !important;
 }
 
-.store-extension-card:hover {
-  transform: translateY(-5px);
+.card-actions {
+  z-index: 4 !important;
 }
 
 .architectures-list {
@@ -244,6 +304,7 @@ export default Vue.extend({
   font-size: 12px;
   box-shadow: 0px 0px 10px 0px rgba(0, 0, 0, 0.2);
   border-radius: 0px 0px 10px 0px !important;
+  z-index: 4 !important;
 }
 
 .extension-name {
@@ -259,6 +320,7 @@ export default Vue.extend({
 .extension-creators {
   flex-grow: 1;
   margin-left: 8px;
+  min-width: 0; /* Ensure flexbox doesn't force a minimum width */
 }
 
 .extension-company {
@@ -269,6 +331,10 @@ export default Vue.extend({
 .extension-authors {
   color: gray;
   font-size: 12px;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: calc(100% - 10px);
 }
 
 .v-card-actions {
