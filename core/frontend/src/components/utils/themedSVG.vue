eeb0dcfff686bde811e9e1bf815520b134cf1e3c
Willian Galvani <williangalvani@gmail.com>
2024-06-28 23:56:04 -0300

    frontend: create class for themed svg imagess

---
+++ core/frontend/src/components/utils/themedSVG.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="ma-4 d-flex justify-center">
+    <!-- this is theoretically not safe, but we have a command that gives users root access, so... -->
+    <!-- eslint-disable vue/no-v-html -->
+    <i :class="`${svg_outside_style} svg-icon`" v-html="image" />
+  </div>
+</template>
+
+<script lang="ts">
+import axios from 'axios'
+import Vue from 'vue'
+
+import settings from '@/store/settings'
+
+export default Vue.extend({
+  name: 'ThemedSVG',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+
+  },
+  data() {
+    return {
+      image: '',
+    }
+  },
+  computed: {
+    svg_outside_style(): string {
+      return `mr-0 ${settings.is_dark_theme ? 'svg-outline-dark' : 'svg-outline-light'}`
+    },
+  },
+  mounted() {
+    axios.get(this.src).then((response) => {
+      this.image = response.data
+    }).catch((error) => {
+      console.error(error)
+    })
+  },
+})
+
+</script>
+<style>
+i.svg-icon svg {
+  height: 100% !important;
+  min-width: 180px;
+}
+
+i.svg-outline-dark path {
+  fill: white;
+}
+
+i.svg-outline-light path {
+  fill: black;
+}
+</style>
