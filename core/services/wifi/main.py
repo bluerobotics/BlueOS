e1c7f5aad6ad8954d04a58b452cdc9fec27393e0
Patrick José Pereira <patrickelectric@gmail.com>
2024-07-04 08:31:59 -0300

    core: services: wifi: Add temporary_cache for scan endpoint
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/wifi/main.py
+++ core/services/wifi/main.py
@@ -8,6 +8,7 @@ import sys
 from pathlib import Path
 from typing import Any, List, Optional
 
+from aiocache import cached
 from commonwealth.utils.apis import (
     GenericErrorHandlingRoute,
     PrettyJSONResponse,
@@ -55,6 +56,7 @@ async def network_status() -> Any:
 
 @app.get("/scan", response_model=List[ScannedWifiNetwork], summary="Retrieve available wifi networks.")
 @version(1, 0)
+@cached(ttl=15, namespace="scan")
 async def scan() -> Any:
     logger.info("Trying to perform network scan.")
     try:
