5d9c9990af346e82919a8cfb1d74963112bc8a9d
Willian Galvani <williangalvani@gmail.com>
2024-07-09 10:38:45 -0300

    frontend: fix off-by-8 issue with pixhawk channels

--- core/frontend/src/components/vehiclesetup/overview/common.ts
+++ core/frontend/src/components/vehiclesetup/overview/common.ts
@@ -4,7 +4,7 @@ export default function toBoardFriendlyChannel(board: string | undefined, servo:
   const servo_number = parseInt(servo.replace('SERVO', '').replace('_FUNCTION', ''), 10)
   if (board === Platform.Pixhawk1) {
     if (servo_number >= 9) {
-      return `Aux ${servo_number}`
+      return `Aux ${servo_number - 8}`
     }
   }
   return `Channel ${servo_number}`
