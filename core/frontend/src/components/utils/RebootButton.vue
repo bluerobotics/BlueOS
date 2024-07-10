84e3aff99eb3a9991ba549f223a9bde91258eb10
Willian Galvani <williangalvani@gmail.com>
2024-07-09 23:52:20 -0300

    frontend: create component RebootButton

---
+++ core/frontend/src/components/utils/RebootButton.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-btn
+    v-if="rebootRequired"
+    color="warning"
+    @click="rebootVehicle"
+  >
+    Reboot Autopilot
+  </v-btn>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import * as AutopilotManager from '@/components/autopilot/AutopilotManagerUpdater'
+import autopilot_data from '@/store/autopilot'
+
+export default Vue.extend({
+  name: 'RebootButton',
+  computed: {
+    rebootRequired(): boolean {
+      return autopilot_data.reboot_required
+    },
+  },
+  methods: {
+    async rebootVehicle(): Promise<void> {
+      await AutopilotManager.restart()
+    },
+  },
+})
+</script>
