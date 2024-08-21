7d1e7df54623cda2c7f9812067513b5e0bc462dc
Patrick José Pereira <patrickelectric@gmail.com>
2024-08-21 16:58:42 -0300

    core: tools: linux2rest: Update to 0.6.1
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/linux2rest/bootstrap.sh
+++ core/tools/linux2rest/bootstrap.sh
@@ -3,7 +3,7 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-VERSION="v0.6.0"
+VERSION="v0.6.1"
 REPOSITORY_ORG="patrickelectric"
 REPOSITORY_NAME="linux2rest"
 PROJECT_NAME="$REPOSITORY_NAME"
