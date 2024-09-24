5d31a4415d7dfae5d3059c3dc16a8ff30172bf13
Dan Rose <danoftheroses@gmail.com>
2024-09-24 18:53:49 -0500

    Fix another broken bit of nginx config

--- core/services/helper/main.py
+++ core/services/helper/main.py
@@ -486,7 +486,7 @@ class Helper:
         name = metadata.sanitized_name
         text = f"""
         location /extensionv2/{name}/ {{
-        proxy_pass :{port}/;
+        proxy_pass http://127.0.0.1:{port}/;
         proxy_set_header Host $host;
         proxy_set_header X-Real-IP $remote_addr;
         proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
