b98cd01b2fe0179b513b70961862c69977ffd45a
ES-Alexander <sandman.esalexander@gmail.com>
2024-12-09 01:12:12 +1100

    diagrams: refactor to use theme colors

--- core/frontend/src/components/utils/themedSVG.vue
+++ core/frontend/src/components/utils/themedSVG.vue
@@ -2,15 +2,13 @@
   <div class="ma-4 d-flex justify-center">
     <!-- this is theoretically not safe, but we have a command that gives users root access, so... -->
     <!-- eslint-disable vue/no-v-html -->
-    <i :class="`${svg_outside_style} svg-icon`" v-html="image" />
+    <i class="svg-icon" v-html="image" />
   </div>
 </template>
 
 <script lang="ts">
 import axios from 'axios'
 import Vue from 'vue'
-
-import settings from '@/store/settings'
 
 export default Vue.extend({
   name: 'ThemedSVG',
@@ -25,11 +23,6 @@ export default Vue.extend({
     return {
       image: '',
     }
-  },
-  computed: {
-    svg_outside_style(): string {
-      return `mr-0 ${settings.is_dark_theme ? 'svg-outline-dark' : 'svg-outline-light'}`
-    },
   },
   watch: {
     src() {
@@ -54,21 +47,5 @@ export default Vue.extend({
 i.svg-icon svg {
   height: 100% !important;
   min-width: 180px;
-}
-
-i.svg-outline-dark path {
-  fill: #D1EAF1;
-}
-
-i.svg-outline-light path {
-  fill: #002F45;
-}
-
-i.svg-outline-dark text {
-  fill: #D1EAF1 !important;
-}
-
-i.svg-outline-light text {
-  fill: #002F45 !important;
 }
 </style>
