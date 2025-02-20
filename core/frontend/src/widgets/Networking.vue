9537c7cd8b440eadbbfbf1b659cfde58d8ff8243
Willian Galvani <williangalvani@gmail.com>
2025-02-20 12:27:04 -0300

    Frontend: update networking bandwidth widget style Frontend: allow showing bandwidth for wifi frontend: update networking widgets for using the a blueos-on-sbc  with css colors

--- core/frontend/src/widgets/Networking.vue
+++ core/frontend/src/widgets/Networking.vue
@@ -1,65 +1,157 @@
 <template>
-  <v-card class="d-flex align-center justify-center" height="40">
-    <v-card-title>
-      <v-icon>mdi-arrow-up</v-icon>
-      {{ formatBandwidth(upload) }}
-      <v-icon>mdi-arrow-down</v-icon>
-      {{ formatBandwidth(download) }}
+  <v-card class="d-flex align-center justify-center" height="40" min-width="180">
+    <v-card-title class="d-flex align-center pa-0 fill-width">
+      <!-- eslint-disable vue/no-v-html -->
+      <div class="left-content ml-2">
+        <i class="pi-icon" v-html="image" />
+        <span class="vertical-text">{{ interface }}</span>
+      </div>
+      <v-spacer />
+      <div class="d-flex flex-column stacked-text mr-2">
+        <div class="d-flex align-center text-caption bandwidth-row" style="margin-bottom: -8px">
+          <div class="icon-label-group">
+            <i class="arrow-icon-rx" v-html="arrowLeft" />
+          </div>
+          {{ formatBandwidth(download) }}
+        </div>
+        <div class="d-flex align-center text-caption bandwidth-row">
+          <div class="icon-label-group">
+            <i class="arrow-icon-tx" v-html="arrowRight" />
+          </div>
+          {{ formatBandwidth(upload) }}
+        </div>
+      </div>
     </v-card-title>
   </v-card>
 </template>
 
 <script lang="ts">
+import axios from 'axios'
 import Vue from 'vue'
 
-import { OneMoreTime } from '@/one-more-time'
-import system_information, { FetchType } from '@/store/system-information'
+import system_information from '@/store/system-information'
+import { formatBandwidth } from '@/utils/networking'
 
 export default Vue.extend({
-  name: 'CpuWidget',
+  name: 'ETh0Widget',
+  props: {
+    interface: {
+      type: String,
+      required: true,
+    },
+  },
   data() {
     return {
       timer: 0,
-      check_backend_status_task: new OneMoreTime({ delay: 2000, disposeWith: this }),
+      image: '',
+      arrowLeft: '',
+      arrowRight: '',
     }
   },
   computed: {
     upload(): number {
-      const eth0 = system_information.system?.network?.find((iface) => iface.name === 'eth0')
-      return eth0?.upload_speed ?? 0
+      const data = system_information.system?.network?.find((iface) => iface.name === this.interface)
+      return data?.upload_speed ?? 0
     },
     download(): number {
-      const eth0 = system_information.system?.network?.find((iface) => iface.name === 'eth0')
-      return eth0?.download_speed ?? 0
+      const data = system_information.system?.network?.find((iface) => iface.name === this.interface)
+      return data?.download_speed ?? 0
     },
   },
-  mounted() {
-    this.check_backend_status_task.setAction(() => {
-      system_information.fetchSystemInformation(FetchType.SystemNetworkType)
-    })
+  async mounted() {
+    this.loadImages()
   },
   beforeDestroy() {
     clearInterval(this.timer)
   },
   methods: {
     formatBandwidth(bytesPerSecond: number): string {
-      const units = ['bps', 'kbps', 'Mbps', 'Gbps']
-      const base = 1000
-
-      if (bytesPerSecond <= 0) return '0 bps'
-
-      const bitsPerSecond = bytesPerSecond * 8
-
-      const unitIndex = Math.max(0, Math.min(
-        Math.floor(Math.log(bitsPerSecond) / Math.log(base)),
-        units.length - 1,
-      ))
-
-      const exp = Math.min(Math.floor(Math.log(bytesPerSecond * 8) / Math.log(base)), units.length - 1)
-      const value = bytesPerSecond * 8 / base ** exp
-
-      return `${value.toFixed(2)} ${units[unitIndex]}`
+      return formatBandwidth(bytesPerSecond)
+    },
+    async loadImages() {
+      this.loadImage()
+      this.loadArrowLeft()
+      this.loadArrowRight()
+    },
+    async loadImage() {
+      const url = (await import('@/assets/img/icons/pi.svg')).default as string
+      const response = await axios.get(url)
+      this.image = response.data
+    },
+    async loadArrowLeft() {
+      const url = (await import('@/assets/img/icons/RX.svg')).default as string
+      const response = await axios.get(url)
+      this.arrowLeft = response.data
+    },
+    async loadArrowRight() {
+      const url = (await import('@/assets/img/icons/TX.svg')).default as string
+      const response = await axios.get(url)
+      this.arrowRight = response.data
     },
   },
 })
 </script>
+
+<style scoped>
+.fill-width {
+  width: 100%;
+}
+
+.left-content {
+  display: flex;
+  align-items: center;
+}
+
+.vertical-text {
+  writing-mode: vertical-rl;
+  text-orientation: mixed;
+  transform: rotate(180deg);
+  font-size: 0.8rem;
+  width: 20px;
+  margin-left: -8px;
+  margin-right: 6px;
+  max-height: 40px;
+  overflow: hidden;
+  text-overflow: clip ;
+  white-space: nowrap;
+}
+
+.pi-icon :deep(svg) {
+  width: 35px;
+  margin-bottom: -10px;
+}
+
+.stacked-text {
+  text-align: right;
+}
+
+.bandwidth-row {
+  min-width: 110px;
+}
+
+.icon-label-group {
+  display: flex;
+  align-items: center;
+  width: 45px;
+}
+
+.bandwidth-label {
+  font-size: 0.7rem;
+  margin-left: 2px;
+  opacity: 0.7;
+}
+
+.arrow-icon-tx :deep(svg){
+  width: 50px;
+  height: 16px;
+  margin-top: 2px;
+  margin-left: -5px;
+}
+
+.arrow-icon-rx :deep(svg){
+  width: 50px;
+  height: 16px;
+  margin-top: 5px;
+  margin-left: -6px;
+}
+</style>
