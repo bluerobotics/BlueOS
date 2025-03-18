b60dc2bff303ab8343bf15e12de9b36474c2ff89
Joao Mario Lago <joao.mario.lago@hotmail.com>
2025-03-18 13:54:32 -0300

    core:services:autopilot_manager: Update FirmwareInstall test

--- core/services/ardupilot_manager/firmware/test_FirmwareInstall.py
+++ core/services/ardupilot_manager/firmware/test_FirmwareInstall.py
@@ -1,3 +1,4 @@
+import asyncio
 import pathlib
 import platform
 
@@ -10,30 +11,33 @@ from typedefs import FlightController, Platform, Vehicle
 
 
 def test_firmware_validation() -> None:
-    downloader = FirmwareDownloader()
-    installer = FirmwareInstaller()
+    async def firmware_validation_wrapper() -> None:
+        downloader = FirmwareDownloader()
+        installer = FirmwareInstaller()
 
-    # Pixhawk1 and Pixhawk4 APJ firmwares should always work
-    temporary_file = downloader.download(Vehicle.Sub, Platform.Pixhawk1)
-    installer.validate_firmware(temporary_file, Platform.Pixhawk1)
+        # Pixhawk1 and Pixhawk4 APJ firmwares should always work
+        temporary_file = downloader.download(Vehicle.Sub, Platform.Pixhawk1)
+        installer.validate_firmware(temporary_file, Platform.Pixhawk1)
 
-    temporary_file = downloader.download(Vehicle.Sub, Platform.Pixhawk4)
-    installer.validate_firmware(temporary_file, Platform.Pixhawk4)
+        temporary_file = downloader.download(Vehicle.Sub, Platform.Pixhawk4)
+        installer.validate_firmware(temporary_file, Platform.Pixhawk4)
 
-    # New SITL firmwares should always work, except for MacOS
-    # there are no SITL builds for MacOS
-    if platform.system() != "Darwin":
-        temporary_file = downloader.download(Vehicle.Sub, Platform.SITL, version="DEV")
-        installer.validate_firmware(temporary_file, Platform.SITL)
-
-    # Raise when validating Navigator firmwares (as test platform is x86)
-    temporary_file = downloader.download(Vehicle.Sub, Platform.Navigator)
-    with pytest.raises(InvalidFirmwareFile):
-        installer.validate_firmware(temporary_file, Platform.Navigator)
-
-    # Install SITL firmware
-    if platform.system() != "Darwin":
+        # New SITL firmwares should always work, except for MacOS
         # there are no SITL builds for MacOS
-        temporary_file = downloader.download(Vehicle.Sub, Platform.SITL, version="DEV")
-        board = FlightController(name="SITL", manufacturer="ArduPilot Team", platform=Platform.SITL)
-        installer.install_firmware(temporary_file, board, pathlib.Path(f"{temporary_file}_dest"))
+        if platform.system() != "Darwin":
+            temporary_file = downloader.download(Vehicle.Sub, Platform.SITL, version="DEV")
+            installer.validate_firmware(temporary_file, Platform.SITL)
+
+        # Raise when validating Navigator firmwares (as test platform is x86)
+        temporary_file = downloader.download(Vehicle.Sub, Platform.Navigator)
+        with pytest.raises(InvalidFirmwareFile):
+            installer.validate_firmware(temporary_file, Platform.Navigator)
+
+        # Install SITL firmware
+        if platform.system() != "Darwin":
+            # there are no SITL builds for MacOS
+            temporary_file = downloader.download(Vehicle.Sub, Platform.SITL, version="DEV")
+            board = FlightController(name="SITL", manufacturer="ArduPilot Team", platform=Platform.SITL)
+            await installer.install_firmware(temporary_file, board, pathlib.Path(f"{temporary_file}_dest"))
+
+    asyncio.run(firmware_validation_wrapper())
