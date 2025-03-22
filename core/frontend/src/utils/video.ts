de7a27633d030de9d3fec4a49071d74750109869
João Antônio Cardoso <joao.maker@gmail.com>
2025-03-22 12:25:06 -0300

    core: frontend: src: Move VideoDevice::device_stream method to utils

--- core/frontend/src/utils/video.ts
+++ core/frontend/src/utils/video.ts
@@ -1,4 +1,7 @@
-import { FrameInterval } from '@/types/video'
+import {
+  Device, FrameInterval, StreamStatus,
+} from '@/types/video'
+
 
 export function video_dimension_text(height?: number, width?: number): string {
   if (height === undefined || width === undefined) {
@@ -21,4 +24,22 @@ export function video_dimension_framerate_text(
   frame_interval?: FrameInterval,
 ): string {
   return `${video_dimension_text(height, width)} @ ${video_framerate_text(frame_interval)}`
+}
+
+export function available_streams_from_device(available_streams: StreamStatus[], device: Device): StreamStatus[] {
+  return available_streams.filter((stream) => {
+    if ('Gst' in stream.video_and_stream.video_source) {
+      return stream.video_and_stream.video_source.Gst.source.Fake === device.source
+    }
+    if ('Local' in stream.video_and_stream.video_source) {
+      return stream.video_and_stream.video_source.Local.device_path === device.source
+    }
+    if ('Redirect' in stream.video_and_stream.video_source) {
+      return stream.video_and_stream.video_source.Redirect.source.Redirect === device.source
+    }
+    if ('Onvif' in stream.video_and_stream.video_source) {
+      return stream.video_and_stream.video_source.Onvif.source.Onvif === device.source
+    }
+    return false
+  })
 }
