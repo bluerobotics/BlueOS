b529b48e032f0240174a1cc732c32ffc4e0e8050
João Antônio Cardoso <joao.maker@gmail.com>
2025-04-23 11:33:51 -0300

    core: service: cable_guy: Introduce Route model

--- core/services/cable_guy/typedefs.py
+++ core/services/cable_guy/typedefs.py
@@ -1,7 +1,11 @@
 from enum import Enum
+from ipaddress import ip_network
 from typing import List, Optional
 
 from pydantic import BaseModel
+
+# TODO: Replace this by `from pydantic import IPvAnyAddress, IPvAnyNetwork` once we update to pydantic v2
+from typedefs_pydantic_network_shin import IPvAnyAddress, IPvAnyNetwork
 
 
 class AddressMode(str, Enum):
@@ -31,7 +35,57 @@ class InterfaceInfo(BaseModel):
     priority: int
 
 
-class NetworkInterface(BaseModel):
+class Route(BaseModel):
+    destination: str  # TODO: change this to IPvAnyNetwork from pydantic v2
+    gateway: Optional[str] = None  # TODO: change this to IPvAnyAddress from pydantic v2
+    priority: Optional[int] = None
+    managed: bool = False
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Route):
+            return NotImplemented
+        return self.destination == other.destination and self.gateway == other.gateway
+
+    def __hash__(self) -> int:
+        return hash(
+            (
+                self.destination,
+                self.gateway,
+            )
+        )
+
+    # TODO: Remove this once we update self.destination type
+    @property
+    def destination_parsed(self) -> IPvAnyNetwork:
+        return IPvAnyNetwork(self.destination)
+
+    # TODO: Remove this once we update self.destination type
+    @destination_parsed.setter
+    def destination_parsed(self, ip: IPvAnyNetwork) -> None:
+        self.destination = str(ip)
+
+    # TODO: Remove this once we update self.next_hop type
+    @property
+    def next_hop_parsed(self) -> Optional[IPvAnyAddress]:
+        if self.gateway is None:
+            return None
+        return IPvAnyAddress(self.gateway)
+
+    # TODO: Remove this once we update self.next_hop type
+    @next_hop_parsed.setter
+    def next_hop_parsed(self, ip: Optional[IPvAnyAddress]) -> None:
+        self.gateway = str(ip) if ip else None
+
+    @property
+    def is_default(self) -> bool:
+        return ip_network(self.destination).is_unspecified
+
+    @property
+    def is_multicast(self) -> bool:
+        return ip_network(self.destination).is_multicast
+
+
+class NetworkInterfaceV1(BaseModel):
     name: str
     addresses: List[InterfaceAddress]
     info: Optional[InterfaceInfo] = None
@@ -39,6 +93,10 @@ class NetworkInterface(BaseModel):
 
     def __hash__(self) -> int:
         return hash(self.name) + sum(hash(address) for address in self.addresses)
+
+
+class NetworkInterface(NetworkInterfaceV1):
+    routes: List[Route]
 
 
 class NetworkInterfaceMetric(BaseModel):
