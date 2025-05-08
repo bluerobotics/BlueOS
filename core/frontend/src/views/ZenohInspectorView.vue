27b7081b1bc047cfc33959cd873af51366449e2a
Patrick José Pereira <patrickelectric@gmail.com>
2025-05-08 11:00:05 -0300

    core: frontend: views: ZenohInspectorView: First commit
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

---
+++ core/frontend/src/views/ZenohInspectorView.vue
@@ -0,0 +1,16 @@
+<template>
+  <zenoh-inspector />
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+import ZenohInspector from '@/components/zenoh-inspector/ZenohInspector.vue'
+
+export default Vue.extend({
+  name: 'ZenohInspectorView',
+  components: {
+    'zenoh-inspector': ZenohInspector,
+  },
+})
+</script>
