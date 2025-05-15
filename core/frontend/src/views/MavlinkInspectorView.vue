528c1639b785ee45b347b09a570744dca7487531
Patrick José Pereira <patrickelectric@gmail.com>
2025-05-15 08:55:17 -0300

    core: frontend: views: MavlinkInspector: Use MAVLink-Server to visualize it
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/views/MavlinkInspectorView.vue
+++ core/frontend/src/views/MavlinkInspectorView.vue
@@ -1,16 +1,23 @@
 <template>
-  <mavlink-inspector />
+  <BrIframe
+    :source="service_path"
+  />
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import MavlinkInspector from '@/components/mavlink-inspector/MavlinkInspector.vue'
+import BrIframe from '@/components/utils/BrIframe.vue'
 
 export default Vue.extend({
   name: 'MavlinkInspectorView',
   components: {
-    'mavlink-inspector': MavlinkInspector,
+    BrIframe,
+  },
+  data() {
+    return {
+      service_path: `${window.location.protocol}//${window.location.hostname}:8080`,
+    }
   },
 })
 </script>
