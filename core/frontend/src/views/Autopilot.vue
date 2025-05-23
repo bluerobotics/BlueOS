bluerobotics/BlueOS-docker

frontend: autopilot manager: only show buttons when it makes sense to do so

core/frontend/src/views/Autopilot.vue
@@ -73,7 +73,7 @@
           Change board
         </v-btn>
         <v-btn
-          v-if="settings.is_pirate_mode"
+          v-if="settings.is_pirate_mode && board_supports_start_stop"
           class="ma-1"
           :block="$vuetify.breakpoint.xs"
           color="secondary"
@@ -83,7 +83,7 @@
           Start autopilot
         </v-btn>
         <v-btn
-          v-if="settings.is_pirate_mode"
+          v-if="settings.is_pirate_mode && board_supports_start_stop"
           class="ma-1"
           :block="$vuetify.breakpoint.xs"
           color="secondary"
@@ -93,6 +93,7 @@
           Stop autopilot
         </v-btn>
         <v-btn
+          v-if="settings.is_pirate_mode && board_supports_restart"
           color="primary"
           class="ma-1"
           :block="$vuetify.breakpoint.xs"
@@ -156,6 +157,13 @@ export default Vue.extend({
     }
   },
   computed: {
+    board_supports_start_stop(): boolean {
+      return this.current_board?.name !== 'Unmanaged'
+    },
+    board_supports_restart(): boolean {
+      // this is a mavlink command, all boards should support it
+      return true
+    },
     autopilot_info(): Record<string, string> {
       let version = 'Unknown'
       if (this.firmware_info) {

bluerobotics/BlueOS

core/frontend/src/views/Autopilot.vue
@@ -88,7 +88,7 @@
           Change board
         </v-btn>
         <v-btn
-          v-if="settings.is_pirate_mode"
+          v-if="settings.is_pirate_mode && board_supports_start_stop"
           class="ma-1"
           :block="$vuetify.breakpoint.xs"
           color="secondary"
@@ -98,7 +98,7 @@
           Start autopilot
         </v-btn>
         <v-btn
-          v-if="settings.is_pirate_mode"
+          v-if="settings.is_pirate_mode && board_supports_start_stop"
           class="ma-1"
           :block="$vuetify.breakpoint.xs"
           color="secondary"
@@ -108,6 +108,7 @@
           Stop autopilot
         </v-btn>
         <v-btn
+          v-if="settings.is_pirate_mode && board_supports_restart"
           color="primary"
           class="ma-1"
           :block="$vuetify.breakpoint.xs"
@@ -173,6 +174,13 @@ export default Vue.extend({
     }
   },
   computed: {
+    board_supports_start_stop(): boolean {
+      return this.current_board?.name !== 'Manual'
+    },
+    board_supports_restart(): boolean {
+      // this is a mavlink command, all boards should support it
+      return true
+    },
     autopilot_info(): Record<string, string> {
       let version = 'Unknown'
       if (this.firmware_info) {