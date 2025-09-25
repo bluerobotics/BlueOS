910e592b5c4a9707891b1cc0727f77bd33d6e9a6
Joao Mario Lago <joao.mario.lago@hotmail.com>
2025-09-25 16:22:46 -0300

    core:frontend:components: InterfaceCard/Types
    * Add DHCPServerLease type to `types/ethernet.ts`
    * Add simple DHCP Leases panel to `ethernet/InterfaceCard.vue`

--- core/frontend/src/types/ethernet.ts
+++ core/frontend/src/types/ethernet.ts
@@ -22,3 +22,24 @@ export interface EthernetInterface {
     info?: InterfaceInfo,
     priority?: number,
 }
+
+export interface DHCPServerLease {
+    mac: string,
+    ip: string,
+    expires_epoch?: number,
+    expires_at?: Date,
+    hostname?: string,
+    client_id?: string,
+    is_active?: boolean,
+}
+
+export interface DHCPServerDetails {
+  interface: string,
+  ipv4_gateway: string,
+  lease_range: [number, number],
+  lease_time: string,
+  is_backup: boolean,
+  is_running: boolean,
+  leases: DHCPServerLease[],
+  subnet_mask: string | null,
+}
