0b3dddbcda1a81a73781f4289ab384cb1e8b5ead
Patrick José Pereira <patrickelectric@gmail.com>
2025-10-03 12:22:30 -0300

    bootstrap: bootstrap: Use 127.0.0.1 over localhost
    localhost may not be available depending of the OS, 127.0.0.1 is safer
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- bootstrap/bootstrap/bootstrap.py
+++ bootstrap/bootstrap/bootstrap.py
@@ -261,7 +261,7 @@ class Bootstrapper:
             bool: True if version chooser is online, False otherwise.
         """
         try:
-            response = requests.get("http://localhost/version-chooser/v1.0/version/current", timeout=10)
+            response = requests.get("http://127.0.0.1/version-chooser/v1.0/version/current", timeout=10)
             if Bootstrapper.SETTINGS_NAME_CORE in response.json()["repository"]:
                 if not self.version_chooser_is_online:
                     self.version_chooser_is_online = True
