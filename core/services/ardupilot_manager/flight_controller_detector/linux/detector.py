28daa19f4bf1e92e442d4af4612fb1b91baad0d0
Joao Mario Lago <joao.mario.lago@hotmail.com>
2025-12-11 13:46:41 -0300

    core: detector: fix isort
    Signed-off-by: Joao Mario Lago <joao.mario.lago@hotmail.com>

--- core/services/ardupilot_manager/flight_controller_detector/linux/detector.py
+++ core/services/ardupilot_manager/flight_controller_detector/linux/detector.py
@@ -1,11 +1,10 @@
 # pylint: disable=unused-import
 from typing import List, Optional, Type
 
-from loguru import logger
-
 from flight_controller_detector.linux.argonot import Argonot
 from flight_controller_detector.linux.linux_boards import LinuxFlightController
 from flight_controller_detector.linux.navigator import NavigatorPi4, NavigatorPi5
+from loguru import logger
 
 
 class LinuxFlightControllerDetector:
