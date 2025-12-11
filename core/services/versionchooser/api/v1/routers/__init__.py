44a5bd76c6588cf2cf27c6601c64b58c52000ef5
Joao Mario Lago <joao.mario.lago@hotmail.com>
2025-12-11 13:46:41 -0300

    core: __init__: fix isort
    Signed-off-by: Joao Mario Lago <joao.mario.lago@hotmail.com>

--- core/services/versionchooser/api/v1/routers/__init__.py
+++ core/services/versionchooser/api/v1/routers/__init__.py
@@ -1,7 +1,7 @@
 # pylint: disable=W0406
 from .bootstrap import bootstrap_router_v1
-from .index import index_router_v1
 from .docker import docker_router_v1
+from .index import index_router_v1
 from .version import version_router_v1
 
 __all__ = ["bootstrap_router_v1", "index_router_v1", "docker_router_v1", "version_router_v1"]
