1b5800dcbdf590353e91e5b49427b3baa5051d14
Patrick José Pereira <patrickelectric@gmail.com>
2026-01-09 16:57:36 -0300

    core: services: ping: ping360_ethernet_prober: Use REUSEADDR with 30303
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/ping/ping360_ethernet_prober.py
+++ core/services/ping/ping360_ethernet_prober.py
@@ -50,6 +50,7 @@ async def find_ping360_ethernet() -> List[PingDeviceDescriptor]:
     for ip in list_ips():
         server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
         server.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+        server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         # Set a timeout so the socket does not block
         # indefinitely when trying to receive data.
         server.settimeout(0.2)
