78d088f0facb40950c26a3756a08e65071c0bc08
Patrick José Pereira <patrickelectric@gmail.com>
2026-01-21 13:53:51 -0300

    core: services: ardupilot_manager: firmware: test: Update to use async firmware download
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/ardupilot_manager/firmware/test_FirmwareDownload.py
+++ core/services/ardupilot_manager/firmware/test_FirmwareDownload.py
@@ -1,3 +1,4 @@
+import asyncio
 import os
 import platform
 
@@ -7,53 +8,65 @@ from typedefs import Platform, Vehicle
 
 
 def test_static() -> None:
-    downloaded_file = FirmwareDownloader._download(FirmwareDownloader._manifest_remote)
-    assert downloaded_file, "Failed to download file."
-    assert downloaded_file.exists(), "Download file does not exist."
+    async def static_wrapper() -> None:
+        downloaded_file = await FirmwareDownloader._download(FirmwareDownloader._manifest_remote)
+        assert downloaded_file, "Failed to download file."
+        assert downloaded_file.exists(), "Download file does not exist."
 
-    smaller_valid_size_bytes = 180 * 1024
-    assert downloaded_file.stat().st_size > smaller_valid_size_bytes, "Download file size is not big enough."
+        smaller_valid_size_bytes = 180 * 1024
+        assert downloaded_file.stat().st_size > smaller_valid_size_bytes, "Download file size is not big enough."
+
+    asyncio.run(static_wrapper())
 
 
 def test_firmware_download() -> None:
-    firmware_download = FirmwareDownloader()
-    assert firmware_download.download_manifest(), "Failed to download/validate manifest file."
+    async def firmware_download_wrapper() -> None:
+        firmware_download = FirmwareDownloader()
+        assert await firmware_download.download_manifest(), "Failed to download/validate manifest file."
 
-    versions = firmware_download._find_version_item(
-        vehicletype="Sub", format="apj", mav_firmware_version_type="STABLE-4.0.1", platform=Platform.Pixhawk1
-    )
-    assert len(versions) == 1, "Failed to find a single firmware."
+        versions = await firmware_download._find_version_item(
+            vehicletype="Sub", format="apj", mav_firmware_version_type="STABLE-4.0.1", platform=Platform.Pixhawk1
+        )
+        assert len(versions) == 1, "Failed to find a single firmware."
 
-    versions = firmware_download._find_version_item(
-        vehicletype="Sub", mav_firmware_version_type="STABLE-4.0.1", platform=Platform.Pixhawk1
-    )
-    # There are two versions, one for the firmware and one with the bootloader
-    assert len(versions) == 2, "Failed to find multiple versions."
+        versions = await firmware_download._find_version_item(
+            vehicletype="Sub", mav_firmware_version_type="STABLE-4.0.1", platform=Platform.Pixhawk1
+        )
+        # There are two versions, one for the firmware and one with the bootloader
+        assert len(versions) == 2, "Failed to find multiple versions."
 
-    available_versions = firmware_download.get_available_versions(Vehicle.Sub, Platform.Pixhawk1)
-    assert len(available_versions) == len(set(available_versions)), "Available versions are not unique."
+        available_versions = await firmware_download.get_available_versions(Vehicle.Sub, Platform.Pixhawk1)
+        assert len(available_versions) == len(set(available_versions)), "Available versions are not unique."
 
-    test_available_versions = ["STABLE-4.0.1", "STABLE-4.0.0", "OFFICIAL", "DEV", "BETA"]
-    assert len(set(available_versions)) >= len(
-        set(test_available_versions)
-    ), "Available versions are missing know versions."
+        test_available_versions = ["STABLE-4.0.1", "STABLE-4.0.0", "OFFICIAL", "DEV", "BETA"]
+        assert len(set(available_versions)) >= len(
+            set(test_available_versions)
+        ), "Available versions are missing know versions."
 
-    assert firmware_download.download(
-        Vehicle.Sub, Platform.Pixhawk1, "STABLE-4.0.1"
-    ), "Failed to download a valid firmware file."
+        assert await firmware_download.download(
+            Vehicle.Sub, Platform.Pixhawk1, "STABLE-4.0.1"
+        ), "Failed to download a valid firmware file."
 
-    assert firmware_download.download(Vehicle.Sub, Platform.Pixhawk1), "Failed to download latest valid firmware file."
+        assert await firmware_download.download(
+            Vehicle.Sub, Platform.Pixhawk1
+        ), "Failed to download latest valid firmware file."
 
-    assert firmware_download.download(Vehicle.Sub, Platform.Pixhawk4), "Failed to download latest valid firmware file."
+        assert await firmware_download.download(
+            Vehicle.Sub, Platform.Pixhawk4
+        ), "Failed to download latest valid firmware file."
 
-    assert firmware_download.download(Vehicle.Sub, Platform.SITL), "Failed to download SITL."
+        assert await firmware_download.download(Vehicle.Sub, Platform.SITL), "Failed to download SITL."
 
-    # skipt these tests for MacOS
-    if platform.system() == "Darwin":
-        pytest.skip("Skipping test for MacOS")
-    # It'll fail if running in an arch different of ARM
-    if "x86" in os.uname().machine:
-        assert firmware_download.download(Vehicle.Sub, Platform.Navigator), "Failed to download navigator binary."
-    else:
-        with pytest.raises(Exception):
-            firmware_download.download(Vehicle.Sub, Platform.Navigator)
+        # skipt these tests for MacOS
+        if platform.system() == "Darwin":
+            pytest.skip("Skipping test for MacOS")
+        # It'll fail if running in an arch different of ARM
+        if "x86" in os.uname().machine:
+            assert await firmware_download.download(
+                Vehicle.Sub, Platform.Navigator
+            ), "Failed to download navigator binary."
+        else:
+            with pytest.raises(Exception):
+                await firmware_download.download(Vehicle.Sub, Platform.Navigator)
+
+    asyncio.run(firmware_download_wrapper())
