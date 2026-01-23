9580eb446a95c1b58f4da07719575f769649b84f
Patrick José Pereira <patrickelectric@gmail.com>
2026-01-23 13:18:08 -0300

    core: tools: mcap-extractor: Update to 0.1.2
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/tools/mcap-extractor/bootstrap.sh
+++ core/tools/mcap-extractor/bootstrap.sh
@@ -5,7 +5,7 @@ set -e
 PROJECT_NAME="mcap-foxglove-video-extract"
 REPOSITORY_ORG="bluerobotics"
 REPOSITORY_NAME="mcap-foxglove-video-extract"
-VERSION="0.1.1"
+VERSION="0.1.2"
 
 echo "Installing project $PROJECT_NAME version $VERSION"
 
