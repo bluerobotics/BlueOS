b64f29ff8f75721f29c1635e0b054998b519d355
Willian Galvani <williangalvani@gmail.com>
2026-02-05 12:29:30 -0300

    Frontend: compress most files to .gz
    This saves about 75mb of image size

--- core/frontend/vite.config.js
+++ core/frontend/vite.config.js
@@ -4,6 +4,7 @@ import Components from 'unplugin-vue-components/vite'
 import { defineConfig, loadEnv } from 'vite'
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { VitePWA } from 'vite-plugin-pwa'
+import viteCompression from 'vite-plugin-compression'
 import wasm from "vite-plugin-wasm"
 const { name } = require('./package.json')
 
@@ -100,6 +101,14 @@ export default defineConfig(({ command, mode }) => {
           removeNonJson(repoPath)
         }
       },
+      // Pre-compress assets with gzip for nginx to serve via gzip_static always
+      viteCompression({
+        algorithm: 'gzip',
+        ext: '.gz',
+        threshold: 1024,
+        deleteOriginFile: true,
+        filter: /\.(js|css|json|svg|txt|xml|wasm|glb)$/i,
+      }),
     ],
     assetsInclude: ['**/*.gif', '**/*.glb', '**/*.png', '**/*.svg', '**/*.msg', "**/three/examples/jsm/libs/draco/*"],
     resolve: {
