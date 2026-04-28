d871a9055eb652fb5ef7931a2f8af9b620e52b7e
Willian Galvani <williangalvani@gmail.com>
2026-04-28 13:36:14 -0300

    Core: fix xml module to use less memory

--- core/frontend/src/types/parameter_repository.d.ts
+++ core/frontend/src/types/parameter_repository.d.ts
@@ -2,3 +2,8 @@ declare module 'https://bluerobotics.github.io/Blueos-Parameter-Repository/param
   const parameters: Dictionary<Dictionary<number>>
   export default parameters
 }
+
+declare module 'virtual:ardupilot-param-index' {
+  const paths: string[]
+  export default paths
+}
