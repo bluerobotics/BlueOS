dea4a84a290aca33fec0d3d68ac8c4e1270e1f70
Patrick José Pereira <patrickelectric@gmail.com>
2026-04-30 10:22:31 -0300

    frontend: vehiclesetup: PowerInfo: Add icon in title
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/vehiclesetup/overview/PowerInfo.vue
+++ core/frontend/src/components/vehiclesetup/overview/PowerInfo.vue
@@ -1,6 +1,9 @@
 <template>
   <v-card class="pa-2">
     <v-card-title class="align-center">
+      <v-icon left>
+        mdi-battery
+      </v-icon>
       Battery Monitor
     </v-card-title>
     <v-card-text>
