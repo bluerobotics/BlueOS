c07dad1bbaf1ec8fa712bfdd7a01d586bc3f1b2d
João Antônio Cardoso <joao.maker@gmail.com>
2026-07-24 23:56:22 -0300

    frontend: widgets: Subscribe to store FetchTypes instead of local timers
    Cpu, Disk, and CpuPie register only the system-information endpoints they display.

--- core/frontend/src/widgets/Disk.vue
+++ core/frontend/src/widgets/Disk.vue
@@ -12,13 +12,10 @@ import Vue from 'vue'
 import system_information, { FetchType } from '@/store/system-information'
 import { Disk } from '@/types/system-information/system'
 
+const FETCH_TYPES = [FetchType.SystemDiskType]
+
 export default Vue.extend({
   name: 'DiskWidget',
-  data() {
-    return {
-      timer: 0,
-    }
-  },
   computed: {
     main_disk(): undefined | Disk {
       const disks = system_information.system?.disk
@@ -30,10 +27,10 @@ export default Vue.extend({
     },
   },
   mounted() {
-    this.timer = setInterval(() => system_information.fetchSystemInformation(FetchType.SystemDiskType), 30000)
+    system_information.subscribeSystemInformation(FETCH_TYPES)
   },
   beforeDestroy() {
-    clearInterval(this.timer)
+    system_information.unsubscribeSystemInformation(FETCH_TYPES)
   },
 })
 </script>
