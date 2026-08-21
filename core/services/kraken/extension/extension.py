8348657d9cdf55c38a25b64d679d3709e442d61e
João Antônio Cardoso <joao.maker@gmail.com>
2026-08-21 15:54:36 -0300

    Revert "kraken: raise ExtensionNotFound when restarting uninstalled extension"
    This reverts commit 8ab48d37ba36a17bf5bc4c49056c3e9701640a77.

--- core/services/kraken/extension/extension.py
+++ core/services/kraken/extension/extension.py
@@ -389,8 +389,6 @@ class Extension:
     @classmethod
     async def from_running(cls, identifier: str) -> "Extension":
         installed: List[Extension] = cast(List[Extension], await cls.from_settings(identifier))
-        if not installed:
-            raise ExtensionNotFound(f"Extension {identifier} not found")
 
         enabled = [ext for ext in installed if ext.source.enabled]
         if not enabled:
