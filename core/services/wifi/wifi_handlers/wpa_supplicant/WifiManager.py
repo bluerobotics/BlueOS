8f1f0d0740eb6659600943bccbe3f7637d3d91c4
Patrick José Pereira <patrickelectric@gmail.com>
2026-08-21 14:57:04 +0000

    core: services: wifi: WifiManager: Start without a wlan interface
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/wifi/wifi_handlers/wpa_supplicant/WifiManager.py
+++ core/services/wifi/wifi_handlers/wpa_supplicant/WifiManager.py
@@ -193,6 +193,8 @@ class WifiManager(AbstractWifiManager):
 
     async def get_wifi_available(self) -> List[ScannedWifiNetwork]:
         """Get a dict from the wifi signals available"""
+        if self.wpa_path is None:
+            return []
 
         async def perform_new_scan() -> None:
             try:
@@ -232,6 +234,8 @@ class WifiManager(AbstractWifiManager):
 
     async def get_saved_wifi_network(self) -> List[SavedWifiNetwork]:
         """Get a list of saved wifi networks"""
+        if self.wpa_path is None:
+            return []
         try:
             data = await self.wpa.send_command_list_networks()
             networks_list = WifiManager.__dict_from_table(data)
@@ -332,6 +336,8 @@ class WifiManager(AbstractWifiManager):
 
     async def status(self) -> WifiStatus:
         """Check wpa_supplicant status"""
+        if self.wpa_path is None:
+            return WifiStatus(state="unavailable")
         try:
             data = await self.wpa.send_command_status()
             # pylint: disable=too-many-function-args
@@ -475,6 +481,9 @@ class WifiManager(AbstractWifiManager):
         self._settings_manager.settings.hotspot_password = credentials.password
         self._settings_manager.save()
 
+        if self.wpa_path is None:
+            return
+
         self.hotspot.set_credentials(credentials)
 
         if self.hotspot.is_running():
@@ -483,6 +492,9 @@ class WifiManager(AbstractWifiManager):
             await self.enable_hotspot(save_settings=False)
 
     def hotspot_credentials(self) -> WifiCredentials:
+        if self.wpa_path is None:
+            settings = self._settings_manager.settings
+            return WifiCredentials(ssid=settings.hotspot_ssid or "", password=settings.hotspot_password or "")
         credentials: WifiCredentials = self.hotspot.credentials
         return credentials
 
@@ -569,14 +581,19 @@ class WifiManager(AbstractWifiManager):
             try:
                 await self.connect(("127.0.0.1", 6664))
             except Exception as udp_connection_error:
-                logger.error(f"Could not connect with internet socket: {udp_connection_error}. Exiting.")
-                raise udp_connection_error
+                logger.error(f"Could not connect with internet socket: {udp_connection_error}.")
+                self.wpa_path = None
+                return
         loop = asyncio.get_event_loop()
         loop.create_task(self.auto_reconnect(60))
         loop.create_task(self.start_hotspot_watchdog())
 
     async def supports_hotspot(self) -> bool:
+        if self.wpa_path is None:
+            return False
         return self.hotspot.supports_hotspot
 
     async def hotspot_is_running(self) -> bool:
+        if self.wpa_path is None:
+            return False
         return self.hotspot.is_running()
