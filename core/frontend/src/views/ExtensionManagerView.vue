98a902ae1c926c6d646c4a7909793a6bc301051a
João Antônio Cardoso <joao.maker@gmail.com>
2026-08-26 13:16:23 -0300

    core: frontend: views: ExtensionManagerView: Key installed cards by identifier

--- core/frontend/src/views/ExtensionManagerView.vue
+++ core/frontend/src/views/ExtensionManagerView.vue
@@ -290,7 +290,7 @@
         <div class="installed-extensions-container">
           <InstalledExtensionCard
             v-for="extension in installed_extensions"
-            :key="extension.docker"
+            :key="extension.identifier"
             :extension="extension"
             :loading="extension.loading"
             :metrics="metricsFor(extension)"
