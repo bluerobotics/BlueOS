02facc1c0b022ad2848414ef481b5830a73cc416
Patrick José Pereira <patrickelectric@gmail.com>
2026-08-27 19:30:22 +0000

    core: services: versionchooser: chooser: Add DNS error message
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/services/versionchooser/utils/chooser.py
+++ core/services/versionchooser/utils/chooser.py
@@ -11,7 +11,7 @@ import docker
 from aiohttp import web
 from loguru import logger
 
-from utils.dockerhub import TagFetcher
+from utils.dockerhub import TagFetcher, remote_tags_error_message
 
 DOCKER_CONFIG_PATH = pathlib.Path(appdirs.user_config_dir("bootstrap"), "startup.json")
 
@@ -353,7 +353,7 @@ class VersionChooser:
         except Exception as error:
             logger.critical(f"error fetching online tags: {error}")
             online_tags = []
-            output["error"] = f"error fetching online tags: {error}"
+            output["error"] = remote_tags_error_message(error)
         assert isinstance(output["remote"], list)
         output["remote"].extend([asdict(tag) for tag in online_tags])
 
