6dd69617b30cf21cdd005d99f6e5ac4d52ac6f8e
Patrick José Pereira <patrickelectric@gmail.com>
2026-09-09 20:54:07 -0300

    core: frontend: kraken: BackAlleyTab: Fix search field on small screens
    The filter and sort buttons take the whole toolbar width, the search
    field shrinks to nothing since it has no minimum width.
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/kraken/BackAlleyTab.vue
+++ core/frontend/src/components/kraken/BackAlleyTab.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-toolbar height="0" extension-height="70" elevation="0">
+    <v-toolbar height="0" :extension-height="$vuetify.breakpoint.xs ? 130 : 70" elevation="0">
       <template #extension>
         <div class="toolbar-extension">
           <v-text-field
@@ -445,6 +445,7 @@ export default Vue.extend({
 <style scoped>
 .query-text-field {
   max-width: 620px;
+  min-width: 200px;
 }
 
 .store-tab {
@@ -460,6 +461,7 @@ export default Vue.extend({
 
 .toolbar-extension {
   display: flex;
+  flex-wrap: wrap;
   justify-content: center;
   align-items: start;
   width: 100%;
