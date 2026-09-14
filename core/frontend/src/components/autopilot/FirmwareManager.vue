fa81904d4a93fe8c45fbc2ba31b0993a91f5e28e
Patrick José Pereira <patrickelectric@gmail.com>
2026-09-14 17:50:51 +0000

    core: frontend: FirmwareManager: Add vehicle type change warning
    Changing Sub to Rover has no warning.
    
    Signed-off-by: Patrick José Pereira <patrickelectric@gmail.com>

--- core/frontend/src/components/autopilot/FirmwareManager.vue
+++ core/frontend/src/components/autopilot/FirmwareManager.vue
@@ -83,10 +83,21 @@
             v-model="chosen_vehicle"
             :items="vehicle_types"
             label="Vehicle type"
+            :hint="vehicle_changed ? vehicle_change_warning : ''"
+            :persistent-hint="vehicle_changed"
             required
             class="ma-1 pa-0"
             @change="updateAvailableFirmwares"
-          />
+          >
+            <template
+              v-if="vehicle_changed"
+              #append-outer
+            >
+              <v-icon color="warning">
+                mdi-alert
+              </v-icon>
+            </template>
+          </v-select>
           <div class="d-flex">
             <v-select
               v-model="chosen_firmware_url"
@@ -274,6 +285,14 @@ export default Vue.extend({
         (vehicle) => firmwareVehicleTypeFromVehicle(vehicle) === firmware,
       )
       return match ?? null
+    },
+    vehicle_changed(): boolean {
+      return this.chosen_vehicle !== null
+        && this.current_vehicle !== null
+        && this.chosen_vehicle !== this.current_vehicle
+    },
+    vehicle_change_warning(): string {
+      return `Installing this firmware will change the vehicle from ${this.current_vehicle} to ${this.chosen_vehicle}.`
     },
     available_boards(): {value: FlightController, text: string}[] {
       return autopilot.available_boards.map(
